aaf66a3a3fa44d8ccd4380711e6d6aa756fe0d26
AMP\psahoo:5 <AMP\psahoo:5>
2010-07-01 19:06:13 +0530

    Advisor RM wise search Customer Done for FP Report.....

--- WealthERPSolution/UI/WealthERP/Reports/FPReports.ascx.cs
+++ WealthERPSolution/UI/WealthERP/Reports/FPReports.ascx.cs
@@ -22,19 +22,55 @@ namespace WealthERP.Reports
         CustomerVo customerVo = new CustomerVo();
         UserVo userVo = new UserVo();
         //string path = string.Empty;
-
+        int AdvisorRMId=0;
         protected void Page_Load(object sender, EventArgs e)
         {
-            //path = Server.MapPath(ConfigurationManager.AppSettings["xmllookuppath"].ToString());
-            rmVo = (RMVo)Session[SessionContents.RmVo];
-            advisorVo = (AdvisorVo)Session[SessionContents.AdvisorVo];
+            ////path = Server.MapPath(ConfigurationManager.AppSettings["xmllookuppath"].ToString());
+            //rmVo = (RMVo)Session[SessionContents.RmVo];
+            //advisorVo = (AdvisorVo)Session[SessionContents.AdvisorVo];
             
-            txtCustomer_autoCompleteExtender.ContextKey = advisorVo.advisorId.ToString();
-            if ((Session["FP_UserID"] != null) && (Session["FP_UserName"] != null))
+            //txtCustomer_autoCompleteExtender.ContextKey = advisorVo.advisorId.ToString();
+            //if ((Session["FP_UserID"] != null) && (Session["FP_UserName"] != null))
+            //{
+            //    txtCustomer.Text = Session["FP_UserName"].ToString();
+            //    txtCustomerId.Value = Session["FP_UserID"].ToString();
+            //}
+
+            SessionBo.CheckSession();
+            if (!IsPostBack)
             {
-                txtCustomer.Text = Session["FP_UserName"].ToString();
-                txtCustomerId.Value = Session["FP_UserID"].ToString();
+                if (Session[SessionContents.CurrentUserRole].ToString() == "RM")
+                {
+                    rmVo = (RMVo)Session[SessionContents.RmVo];
+                    AdvisorRMId = rmVo.RMId;
+                    txtCustomer_autoCompleteExtender.ServiceMethod = "GetCustomerName";
+                }
+                else if (Session[SessionContents.CurrentUserRole].ToString() == "Admin")
+                {
+                    advisorVo = (AdvisorVo)Session[SessionContents.AdvisorVo];
+                    AdvisorRMId = advisorVo.advisorId;
+                    txtCustomer_autoCompleteExtender.ServiceMethod = "GetAdviserCustomerName";
+                }
+
+
+                if ((Session["FP_UserID"] == null) && (Session["FP_UserName"] == null))
+                {
+                    SessionBo.CheckSession();
+                    //rmVo = (RMVo)Session[SessionContents.RmVo];
+                    //txtPickCustomer_autoCompleteExtender.ContextKey = rmVo.RMId.ToString();
+                    txtCustomer_autoCompleteExtender.ContextKey = AdvisorRMId.ToString();
+
+                }
+                else
+                {
+
+                    txtCustomer.Text = Session["FP_UserName"].ToString();
+                    txtCustomerId.Value = Session["FP_UserID"].ToString();
+                    SessionBo.CheckSession();
+                    txtCustomer_autoCompleteExtender.ContextKey = AdvisorRMId.ToString();
+                }
             }
+
         }
 
         protected void txtCustomerId_ValueChanged(object sender, EventArgs e)
@@ -42,6 +78,10 @@ namespace WealthERP.Reports
             CustomerBo customerBo = new CustomerBo();
             if (txtCustomerId.Value != string.Empty)
             {
+                //Maintaining customer session throughout the financial planning Module //Pramoda//
+                Session["FP_UserID"] = txtCustomerId.Value;
+                Session["FP_UserName"] = txtCustomer.Text;
+
                 customerVo = customerBo.GetCustomer(int.Parse(txtCustomerId.Value));
                 Session["CusVo"] = customerVo;
 
