84ab15ff14017ae06698a346327a4fa13a537d52
AMP\vinothvs <AMP\vinothvs>
2010-07-29 17:48:11 +0530

    Function:GetLastUploadDate,Description:GetLastUploadDate of particular Customer

--- WealthERPSolution/Bo/Uploads/UploadCommonBo.cs
+++ WealthERPSolution/Bo/Uploads/UploadCommonBo.cs
@@ -56,7 +56,32 @@ namespace BoUploads
 
             return dsColumnNames;
         }
-
+        public string GetLastUploadDate(int advisorid)
+        {
+            string lastUploadDate="";
+            UploadsCommonDao uploadscommonDao = new UploadsCommonDao();
+            try
+            {
+                lastUploadDate = uploadscommonDao.GetLastUploadDate(advisorid);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "UploadCommonBo.cs:GetLastUploadDate()");
+                object[] objects = new object[1];
+                objects[0] = advisorid;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return lastUploadDate;
+        }
         public DataSet GetUploadWERPNameForExternalColumnNames(int xmlfiletypeId)
         {
             DataSet dsColumnNames = new DataSet();
