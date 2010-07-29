794e7d512d608b46eb469030408085f752d82afd
AMP\vinothvs <AMP\vinothvs>
2010-07-29 17:43:12 +0530

    Function:GetLastUploadDate,Description:GetLastUploadDate of particular Customer

--- WealthERPSolution/Dao/Uploads/UploadsCommonDao.cs
+++ WealthERPSolution/Dao/Uploads/UploadsCommonDao.cs
@@ -56,6 +56,38 @@ namespace DaoUploads
             return getColumnNamesds;
         }
 
+        public string GetLastUploadDate(int advisorid)
+        {
+            string lastUploadDate="";
+            Database db;
+            DbCommand getColumnNamesCmd;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getColumnNamesCmd = db.GetStoredProcCommand("SP_GetLastUploadDate");
+                db.AddInParameter(getColumnNamesCmd, "@advisorId", DbType.Int32, advisorid);
+                lastUploadDate = db.ExecuteDataSet(getColumnNamesCmd).Tables[0].Rows[0]["ADUL_StartTime"].ToString();                
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "CamsUploadsDao.cs:GetLastUploadDate()");
+                object[] objects = new object[1];
+                objects[0] = advisorid;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return lastUploadDate;
+        }
+
         public DataSet GetUploadWERPNameForExternalColumnNames(int xmlfiletypeId)
         {
             DataSet ds = new DataSet();
