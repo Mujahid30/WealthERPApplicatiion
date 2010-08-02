15c5f54638ff9acca68df76e8fd0cbfd7995b176
AMP\vinothvs <AMP\vinothvs>
2010-08-02 13:19:22 +0530

    MenuIssue(Fixed)

--- WealthERPSolution-MoneyTouch/UI/WealthERP/Customer/CustomerIndividualLeftPane.ascx.cs
+++ WealthERPSolution-MoneyTouch/UI/WealthERP/Customer/CustomerIndividualLeftPane.ascx.cs
@@ -42,6 +42,7 @@ namespace WealthERP.Customer
             if (!IsPostBack)
             {
                 TreeView1.CollapseAll();
+                ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "pageloadscript", "loadtopmenu('CustomerIndividualLeftPane');", true);
 
             }
         }
