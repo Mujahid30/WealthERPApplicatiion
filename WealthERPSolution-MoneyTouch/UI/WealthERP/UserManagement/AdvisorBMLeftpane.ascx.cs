b197c14fcc74084a4b507111d5935a658da5ed3f
AMP\vinothvs <AMP\vinothvs>
2010-08-02 18:00:30 +0530

    Tree View Changes(Enhancement)

--- WealthERPSolution-MoneyTouch/UI/WealthERP/UserManagement/AdvisorBMLeftpane.ascx.cs
+++ WealthERPSolution-MoneyTouch/UI/WealthERP/UserManagement/AdvisorBMLeftpane.ascx.cs
@@ -36,10 +36,17 @@ namespace WealthERP.UserManagement
 
                 ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "pageloadscript", "loadtopmenu('AdvisorBMLeftpane');", true);
             }
-            
+            if (Page.Request.Params.Get("__EVENTTARGET") != null && (Page.Request.Params.Get("__EVENTTARGET")).Contains("TreeView1"))
+            {
+                SetNode();
+            }
         }
 
         protected void TreeView1_SelectedNodeChanged(object sender, EventArgs e)
+        {
+            SetNode();
+        }
+        public void SetNode()
         {
             if (TreeView1.SelectedNode.Value == "Advisor")
             {
