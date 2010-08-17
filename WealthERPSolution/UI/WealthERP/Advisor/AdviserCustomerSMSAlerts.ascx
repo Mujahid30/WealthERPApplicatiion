c11b311c96bd6e7faaa7b37ae51218a3c7fc23a6
AMP\vinothvs <AMP\vinothvs>
2010-08-17 19:06:00 +0530

    Alert SMs Grid have been Completely Changed Modified Stored Procedure Dependencies:SP_GetAdviserCustomerSMSAlerts

--- WealthERPSolution/UI/WealthERP/Advisor/AdviserCustomerSMSAlerts.ascx
+++ WealthERPSolution/UI/WealthERP/Advisor/AdviserCustomerSMSAlerts.ascx
@@ -1,4 +1,13 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AdviserCustomerSMSAlerts.ascx.cs" Inherits="WealthERP.Advisor.AdviserCustomerSMSAlerts" %>
+
+<script src="../Scripts/jquery.alerts.js" type="text/javascript"></script>
+<link href="../CSS/jquery.alerts.css" rel="stylesheet" type="text/css" />
+<%--Used to create Popups and Alert Box--%>
+<script src="/Scripts/jquery.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+</script>
 <table width="100%">
 <tr><td><asp:Label ID="lblCustomerSMSAlerts" Text="Customer SMS Alerts" CssClass="HeaderTextSmall" runat="server"></asp:Label> </td></tr>
 <tr><td>
@@ -15,14 +24,19 @@
 <asp:Panel ID="pnlCustomerSMSAlerts" runat="server" Height="300px" 
                        Width="100%" ScrollBars="Vertical" Visible="false" HorizontalAlign="Left">
 <asp:Label ID="lblNoRecords" Text="No Alert Exists" runat="server" Visible="false" CssClass="FieldName"></asp:Label>
-<asp:GridView ID="gvCustomerSMSAlerts" DataKeyNames="CustomerId,AlertId" runat="server" AutoGenerateColumns="False" CellPadding="4"
-                        ForeColor="#333333" Width="624px" Height="78px" 
-                        Font-Size="Small" BackImageUrl="~/CSS/Images/HeaderGlassBlack.jpg" CssClass="GridViewStyle" 
-                        ShowFooter="true" OnRowDataBound="gvCustomerSMSAlerts_RowDataBound">
+<asp:GridView ID="gvCustomerSMSAlerts" DataKeyNames="CustomerId,AlertId" 
+        runat="server" AutoGenerateColumns="False" CellPadding="4"
+                        ForeColor="#333333" Width="100%" Height="100%" 
+                        Font-Size="Small" 
+        BackImageUrl="~/CSS/Images/HeaderGlassBlack.jpg" CssClass="GridViewStyle" 
+                        ShowFooter="true" 
+        OnRowDataBound="gvCustomerSMSAlerts_RowDataBound" AllowPaging="True">
+                        <PagerStyle BorderStyle="Solid" />
                         <SelectedRowStyle Font-Bold="True" CssClass="SelectedRowStyle" />
                         <HeaderStyle Font-Bold="True" Font-Size="Small" ForeColor="White" CssClass="HeaderStyle" />
                         <EditRowStyle Font-Size="X-Small" CssClass="EditRowStyle" />
                         <AlternatingRowStyle BorderStyle="None" CssClass="AltRowStyle" />
+                        <PagerSettings Mode="NextPreviousFirstLast" />
                         <RowStyle CssClass="RowStyle" />
                         <FooterStyle CssClass="FooterStyle" />
                         <Columns>                      
@@ -34,9 +48,13 @@
                             </asp:TemplateField>
                             
                             <asp:BoundField DataField="CustomerName" HeaderText="Customer"  />
-                            <asp:BoundField DataField="Mobile" HeaderText="Mobile"  />
-                            <asp:BoundField DataField="AlertMessage" HeaderText="Alert Message"  />
+                             <asp:BoundField DataField="Name" HeaderText="Details" />
+                             <asp:BoundField DataField="AlertMessage" HeaderText="Alert Message"  />
+                             <asp:BoundField DataField="TimesSMSSent" HeaderText="No of Times SMS Sent" />
+                             <asp:BoundField DataField="LastSMSDate" HeaderText="Last SMS Date" />
                             <asp:BoundField DataField="AlertDate" HeaderText="Alert Date"  />
+                            
+                           
 
                         </Columns>
                         
@@ -44,6 +62,10 @@
                     </asp:Panel>
 </td>
 </tr>
-<tr><td><asp:Button ID="btnSend" Text="Send SMS" runat="server" 
+<tr><td align="center"><asp:Button ID="btnSend" Text="Send SMS" runat="server" 
         CssClass="PCGButton" onclick="btnSend_Click" /> </td></tr>
 </table>
+<asp:HiddenField ID="hdnCustomerIdWithoutMobileNumber" runat="server" />
+<asp:HiddenField ID="hdnCustomerNameWithoutMobileNumber" runat="server" />
+
+
--- WealthERPSolution/UI/WealthERP/Advisor/AdviserCustomerSMSAlerts.ascx.cs
+++ WealthERPSolution/UI/WealthERP/Advisor/AdviserCustomerSMSAlerts.ascx.cs
@@ -15,6 +15,7 @@ namespace WealthERP.Advisor
 {
     public partial class AdviserCustomerSMSAlerts : System.Web.UI.UserControl
     {
+        DataSet dsAdviserCustomerAlerts;
         protected void Page_Load(object sender, EventArgs e)
         {
             setAdviserSMSLicenseInfo();
@@ -23,33 +24,102 @@ namespace WealthERP.Advisor
         public void GetAdviserCustomerAlerts()
         {
             int adviserId = 0;
-            DataSet dsAdviserCustomerAlerts;
-            DataTable dtAdviserCustomerAlerts;
             
-            if(Session["advisorVo"]!=null)
-                adviserId=((AdvisorVo)Session["advisorVo"]).advisorId;
+            
+            DataRow drAdviserCustomerAlert = null;
+            if (Session["advisorVo"] != null)
+                adviserId = ((AdvisorVo)Session["advisorVo"]).advisorId;
             AlertsBo alertsBo = new AlertsBo();
             dsAdviserCustomerAlerts = alertsBo.GetAdviserCustomerSMSAlerts(adviserId);
             if (dsAdviserCustomerAlerts != null)
             {
-                dtAdviserCustomerAlerts = dsAdviserCustomerAlerts.Tables[0];
-                if (dtAdviserCustomerAlerts.Rows.Count != 0)
+                DataTable dtAdviserCustomerAlerts = new DataTable();
+                dtAdviserCustomerAlerts.Columns.Add("CustomerId");
+                dtAdviserCustomerAlerts.Columns.Add("AlertId");
+                dtAdviserCustomerAlerts.Columns.Add("CustomerName");
+                dtAdviserCustomerAlerts.Columns.Add("Name");
+                dtAdviserCustomerAlerts.Columns.Add("AlertMessage");
+                dtAdviserCustomerAlerts.Columns.Add("TimesSMSSent");
+                dtAdviserCustomerAlerts.Columns.Add("LastSMSDate");
+                dtAdviserCustomerAlerts.Columns.Add("AlertDate");
+                for (int i = 0; i < dsAdviserCustomerAlerts.Tables[0].Rows.Count; i++)
                 {
-                    gvCustomerSMSAlerts.DataSource = dtAdviserCustomerAlerts;
-                    gvCustomerSMSAlerts.DataBind();
-                    gvCustomerSMSAlerts.Visible = true;
-                    pnlCustomerSMSAlerts.Visible = true;
-                    lblNoRecords.Visible = false;
-                    btnSend.Visible = true;
+                    drAdviserCustomerAlert = dtAdviserCustomerAlerts.NewRow();
+                    if (dsAdviserCustomerAlerts.Tables[0].Rows[i]["CustomerId"] != null && dsAdviserCustomerAlerts.Tables[0].Rows[i]["AlertId"]!=null)
+                    {
+                        drAdviserCustomerAlert["CustomerId"] = dsAdviserCustomerAlerts.Tables[0].Rows[i]["CustomerId"].ToString();
+                        drAdviserCustomerAlert["AlertId"] = dsAdviserCustomerAlerts.Tables[0].Rows[i]["AlertId"].ToString();
+                        if (dsAdviserCustomerAlerts.Tables[0].Rows[i]["CustomerName"] != null)
+                        {
+                            drAdviserCustomerAlert["CustomerName"] = dsAdviserCustomerAlerts.Tables[0].Rows[i]["CustomerName"].ToString();
+                        }
+                        else
+                        {
+                            drAdviserCustomerAlert["CustomerName"] = "";
+                        }
+                        if (dsAdviserCustomerAlerts.Tables[1].Rows[i]["Name"].ToString() != null)
+                        {
+                            drAdviserCustomerAlert["Name"] = dsAdviserCustomerAlerts.Tables[1].Rows[i]["Name"].ToString();
+                        }
+                        else
+                        {
+                            drAdviserCustomerAlert["Name"] = "";
+                        }
+                        if (dsAdviserCustomerAlerts.Tables[0].Rows[i]["AlertMessage"].ToString() != null)
+                        {
+                            drAdviserCustomerAlert["AlertMessage"] = dsAdviserCustomerAlerts.Tables[0].Rows[i]["AlertMessage"].ToString();
+                        }
+                        else
+                        {
+                            drAdviserCustomerAlert["AlertMessage"] = "";
+                        }
+                        if (dsAdviserCustomerAlerts.Tables[1].Rows[i]["TimesSMSSent"].ToString() != null && dsAdviserCustomerAlerts.Tables[1].Rows[i]["TimesSMSSent"].ToString() != "")
+                        {
+                            drAdviserCustomerAlert["TimesSMSSent"] = dsAdviserCustomerAlerts.Tables[1].Rows[i]["TimesSMSSent"].ToString();
+                        }
+                        else
+                        {
+                            drAdviserCustomerAlert["TimesSMSSent"] = "";
+                        }
+
+                        if (dsAdviserCustomerAlerts.Tables[1].Rows[i]["LastSMSDate"].ToString() != null && dsAdviserCustomerAlerts.Tables[1].Rows[i]["LastSMSDate"].ToString() !="")
+                        {
+                            DateTime lastSMSDate=DateTime.Parse(dsAdviserCustomerAlerts.Tables[1].Rows[i]["LastSMSDate"].ToString());
+                            drAdviserCustomerAlert["LastSMSDate"] = lastSMSDate.ToShortDateString();
+                        }
+                        else
+                        {
+                            drAdviserCustomerAlert["LastSMSDate"] = "";
+                        }
+                        if (dsAdviserCustomerAlerts.Tables[0].Rows[i]["AlertDate"].ToString() != null && dsAdviserCustomerAlerts.Tables[0].Rows[i]["AlertDate"].ToString() != "")
+                        {
+                            DateTime alertDate = DateTime.Parse(dsAdviserCustomerAlerts.Tables[0].Rows[i]["AlertDate"].ToString());
+                            drAdviserCustomerAlert["AlertDate"] = alertDate.ToShortDateString();
+                        }
+                        else
+                        {
+                            drAdviserCustomerAlert["AlertDate"] = "";
+                        }
+                        if (drAdviserCustomerAlert != null)
+                        {
+                            dtAdviserCustomerAlerts.Rows.Add(drAdviserCustomerAlert);
+                        }
+                        if (Int64.Parse(dsAdviserCustomerAlerts.Tables[0].Rows[i]["Mobile"].ToString()) == 0)
+                        {
+                            hdnCustomerIdWithoutMobileNumber.Value += dsAdviserCustomerAlerts.Tables[0].Rows[i]["CustomerId"].ToString() + ",";
+                            hdnCustomerNameWithoutMobileNumber.Value += dsAdviserCustomerAlerts.Tables[0].Rows[i]["CustomerName"].ToString() + ",";
+                        }
+                        
+                    }                   
+                    
                 }
-                else
-                {
-                    lblNoRecords.Visible = true;
-                    pnlCustomerSMSAlerts.Visible = true;
-                    gvCustomerSMSAlerts.Visible = false;
-                    btnSend.Visible = false;
-                   
-                }
+                gvCustomerSMSAlerts.DataSource = dtAdviserCustomerAlerts;
+                gvCustomerSMSAlerts.DataBind();
+                gvCustomerSMSAlerts.Visible = true;
+                pnlCustomerSMSAlerts.Visible = true;
+                lblNoRecords.Visible = false;
+                btnSend.Visible = true;
+
             }
             else
             {
@@ -58,6 +128,7 @@ namespace WealthERP.Advisor
                 pnlCustomerSMSAlerts.Visible = true;
                 btnSend.Visible = false;
             }
+
         }
         public void setAdviserSMSLicenseInfo()
         {
@@ -112,23 +183,31 @@ namespace WealthERP.Advisor
             List<SMSVo> smsVoList = new List<SMSVo>();
             AdvisorBo advisorBo = new AdvisorBo();
             AlertsBo alertsBo=new AlertsBo();
+            string mobileNo;
             int adviserId=0;
             if(Session["advisorVo"]!=null)
                 adviserId=((AdvisorVo)Session["advisorVo"]).advisorId;
             if(lblLincenceValue.Text!="No SMS Licence Left!!")
                 smsLicence=int.Parse(lblLincenceValue.Text.ToString());
-
+            int i = 0;
             foreach (GridViewRow gvRow in gvCustomerSMSAlerts.Rows)
-            {
+            {                
                 if (gvRow.RowType == DataControlRowType.DataRow)
                 {
                     if (((CheckBox)gvRow.FindControl("chkCustomerSMSAlert")).Checked)
                     {
                         smsVo = new SMSVo();
-                        if (gvRow.Cells[2].Text.Trim().Length <= 10)
-                            smsVo.Mobile = Int64.Parse("91" + gvRow.Cells[2].Text.Trim());
-                        else
-                            smsVo.Mobile = int.Parse(gvRow.Cells[2].Text.Trim());
+                        if (dsAdviserCustomerAlerts != null)
+                        {                            
+                            mobileNo = dsAdviserCustomerAlerts.Tables[0].Rows[i]["Mobile"].ToString();
+                            if (mobileNo != "0")
+                            {
+                                if (mobileNo.Length <= 10)
+                                    smsVo.Mobile = Int64.Parse("91" + dsAdviserCustomerAlerts.Tables[0].Rows[i]["Mobile"].ToString());
+                                else
+                                    smsVo.Mobile = Int64.Parse(mobileNo);
+                            }
+                        }
                         smsVo.Message = gvRow.Cells[3].Text.ToString();
                         smsVo.CustomerId = int.Parse(gvCustomerSMSAlerts.DataKeys[gvRow.RowIndex].Values["CustomerId"].ToString());
                         smsVo.IsSent = 0;
@@ -138,6 +217,7 @@ namespace WealthERP.Advisor
 
                     }
                 }
+                i++;
             }
             if (smsCount <= smsLicence && smsCount!=0)
             {
