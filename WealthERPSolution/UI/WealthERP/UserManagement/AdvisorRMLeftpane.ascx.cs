a56e7c04dfd6158b4d00fce7b58306971a91cbc3
AMP\psahoo:5 <AMP\psahoo:5>
2010-09-03 17:30:33 +0530

    RM and Admin Role stored in session

--- WealthERPSolution/UI/WealthERP/UserManagement/AdvisorRMLeftpane.ascx.cs
+++ WealthERPSolution/UI/WealthERP/UserManagement/AdvisorRMLeftpane.ascx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using BoCommon;
+using WealthERP.Base;
+
 
 namespace WealthERP.UserManagement
 {
@@ -23,10 +25,12 @@ namespace WealthERP.UserManagement
         {
             if (TreeView1.SelectedNode.Value == "Advisor")
             {
+                Session[SessionContents.CurrentUserRole] = "Admin";
                 Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "leftpane", "loadcontrol('IFAAdminMainDashboard','login');", true);
             }
             else if (TreeView1.SelectedNode.Value == "RM")
             {
+                Session[SessionContents.CurrentUserRole] = "RM";
                 Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "leftpane", "loadcontrol('RMDashBoard','login');", true);
             }
         }
