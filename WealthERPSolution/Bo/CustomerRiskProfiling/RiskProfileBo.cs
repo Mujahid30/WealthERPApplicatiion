d2026f9ba47444e21c396a8fb6dd42cb23a522ad
AMP\psahoo:5 <AMP\psahoo:5>
2010-09-03 17:09:43 +0530

    RiskProfiling Test Problem Solved

--- WealthERPSolution/Bo/CustomerRiskProfiling/RiskProfileBo.cs
+++ WealthERPSolution/Bo/CustomerRiskProfiling/RiskProfileBo.cs
@@ -113,53 +113,60 @@ namespace BoCustomerRiskProfiling
             DataSet DSAssets = new DataSet();
             string CashLessMore="";
             DSAssets = riskprofiledao.GetCustomerAssets(CustomerID);
-            if (DSAssets.Tables[1].Rows.Count > 0)
+            try
             {
-                if (DSAssets.Tables[0].Columns["Cash"].ToString() != "" || DSAssets.Tables[1].Columns["Cash"].ToString() != "")
+                if (DSAssets.Tables[1].Rows.Count > 0)
                 {
-                    if (double.Parse(DSAssets.Tables[0].Rows[0]["Cash"].ToString()) > double.Parse(DSAssets.Tables[1].Rows[0]["Cash"].ToString()))
+                    if (DSAssets.Tables[0].Columns["Cash"].ToString() != "" || DSAssets.Tables[1].Columns["Cash"].ToString() != "")
                     {
-                        CashLessMore = "more";
+                        if (double.Parse(DSAssets.Tables[0].Rows[0]["Cash"].ToString()) > double.Parse(DSAssets.Tables[1].Rows[0]["Cash"].ToString()))
+                        {
+                            CashLessMore = "more";
+                        }
+                        else
+                        {
+                            CashLessMore = "less";
+                        }
+                    }
+                    //DSAssets = riskprofiledao.GetCustomerAssets(CustomerID);
+                    if (double.Parse(DSAssets.Tables[0].Rows[0]["Equity"].ToString()) == double.Parse(DSAssets.Tables[1].Rows[0]["Equity"].ToString()) && double.Parse(DSAssets.Tables[0].Rows[0]["Debt"].ToString()) == double.Parse(DSAssets.Tables[1].Rows[0]["Debt"].ToString()))
+                    {
+
+                        AssetAllocationText = "Based on your current investments we have identified that your asset " +
+                        "allocation matches the asset allocation recommeded by us. The asset allocation recommended by " +
+                        "us is based on the your risk profile and other data pulled from your profile information." +
+                        "Your current equity allocation is " + Math.Round(double.Parse(DSAssets.Tables[0].Rows[0]["Equity"].ToString()), 2).ToString()+ " %" +
+                        " and debt allocation is " + Math.Round(double.Parse(DSAssets.Tables[0].Rows[0]["Debt"].ToString()), 2).ToString() + " %" + " Based on our analysis we recommend an equity allocation of "
+                        + Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Equity"].ToString()), 2).ToString() + " %" + " and debt allocation of " + Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Debt"].ToString()), 2) + " %" + 
+                        "As per our recommendion you keep <recommended cash> of your investment portfolio " +
+                        "in cash and cash equivalents to take care of liquidity in your portfolio." +
+                        "You have an appropriate asset allocation. Please contact your advisor to help you meet you financial goals";
+
+
                     }
                     else
-                    {
-                        CashLessMore = "less";
-                    }
-                }
-                //DSAssets = riskprofiledao.GetCustomerAssets(CustomerID);
-                if (double.Parse(DSAssets.Tables[0].Rows[0]["Equity"].ToString()) == double.Parse(DSAssets.Tables[1].Rows[0]["Equity"].ToString()) && double.Parse(DSAssets.Tables[0].Rows[0]["Debt"].ToString()) == double.Parse(DSAssets.Tables[1].Rows[0]["Debt"].ToString()))
-                {
-
-                    AssetAllocationText = "Based on your current investments we have identified that your asset " +
-                    "allocation matches the asset allocation recommeded by us. The asset allocation recommended by " +
-                    "us is based on the your risk profile and other data pulled from your profile information." +
-                    "Your current equity allocation is Rs " + Math.Round(double.Parse(DSAssets.Tables[0].Rows[0]["Equity"].ToString()), 2).ToString() +
-                    " and debt allocation is Rs " + Math.Round(double.Parse(DSAssets.Tables[0].Rows[0]["Debt"].ToString()), 2).ToString() + " Based on our analysis we recommend an equity allocation of Rs "
-                    + Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Equity"].ToString()), 2).ToString() + " and debt allocation of Rs " + Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Debt"].ToString()), 2) +
-                    "As per our recommendion you keep <recommended cash> of your investment portfolio " +
-                    "in cash and cash equivalents to take care of liquidity in your portfolio." +
-                    "You have an appropriate asset allocation. Please contact your advisor to help you meet you financial goals";
-
+                        AssetAllocationText = "Based on your current investments we have identified that your asset " +
+                       "allocation does not match the asset allocation recommeded by us. The asset allocation recommended " +
+                       "by us is based on the your risk profile and other data pulled from your profile information." +
+                       "Your current equity allocation is " + Math.Round(double.Parse(DSAssets.Tables[0].Rows[0]["Equity"].ToString()), 2).ToString() + " %" +
+                       " and debt allocation is " + Math.Round(double.Parse(DSAssets.Tables[0].Rows[0]["Debt"].ToString()), 2).ToString() + " %" + ". But based on " +
+                       "our analysis we recommend an equity allocation of Rs" + Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Equity"].ToString()), 2).ToString() +
+                       "and debt allocation of " + Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Debt"].ToString()), 2).ToString() + " %" + ".Please contact your advisor" +
+                       " to help you shift closer to the recommended asst allocation. This will keep you in sync with your " +
+                       "rsik taking capacity and your risk appetite both.Moreover we recommend you keep " +
+                       Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Cash"].ToString()), 2).ToString() + " %" + " of your investment portfolio in cash and cash equivalents to " +
+                       "take care of liquidity in your portfolio. Based on your current assset allocation we have identified that " +
+                       "cash allocation is " + CashLessMore + "than recommended.Please contact your advisor to remove the gap.";
 
                 }
                 else
-                    AssetAllocationText = "Based on your current investments we have identified that your asset " +
-                   "allocation does not match the asset allocation recommeded by us. The asset allocation recommended " +
-                   "by us is based on the your risk profile and other data pulled from your profile information." +
-                   "Your current equity allocation is Rs " + Math.Round(double.Parse(DSAssets.Tables[0].Rows[0]["Equity"].ToString()), 2).ToString() +
-                   " and debt allocation is Rs " + Math.Round(double.Parse(DSAssets.Tables[0].Rows[0]["Debt"].ToString()), 2).ToString() + ". But based on " +
-                   "our analysis we recommend an equity allocation of Rs" + Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Equity"].ToString()), 2).ToString() +
-                   "and debt allocation of Rs " + Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Debt"].ToString()), 2).ToString() + ".Please contact your advisor" +
-                   " to help you shift closer to the recommended asst allocation. This will keep you in sync with your Rs " +
-                   "rsik taking capacity and your risk appetite both.Moreover we recommend you keep " +
-                   Math.Round(double.Parse(DSAssets.Tables[1].Rows[0]["Cash"].ToString()), 2).ToString() + " of your investment portfolio in cash and cash equivalents to " +
-                   "take care of liquidity in your portfolio. Based on your current assset allocation we have identified that " +
-                   "cash allocation is " + CashLessMore + "than recommended.Please contact your advisor to remove the gap.";
-
+                {
+                    AssetAllocationText = "";
+                }
             }
-            else
+            catch(Exception ex)
             {
-                AssetAllocationText = "";
+                throw ex;
             }
             return AssetAllocationText;
         }
