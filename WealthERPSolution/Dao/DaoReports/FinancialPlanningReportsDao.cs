ffd8ce0d921714a2b9a698ccdba5cf04501a1d7e
AMP\psahoo:5 <AMP\psahoo:5>
2010-10-19 11:17:10 +0530

    RE:2.2,Bug Fixing:R4-customer list issues solved

--- WealthERPSolution/Dao/DaoReports/FinancialPlanningReportsDao.cs
+++ WealthERPSolution/Dao/DaoReports/FinancialPlanningReportsDao.cs
@@ -31,19 +31,24 @@ namespace DaoReports
             DataTable dtCustomerDetails = new DataTable();
             DataTable dtSpouseDetails = new DataTable();
             DataTable dtChildrenDetails = new DataTable();
-            
+            DataTable dtTempChildrenDetails = new DataTable();
             try
             {
                 db = DatabaseFactory.CreateDatabase("wealtherp");
                 cmd = db.GetStoredProcCommand("SP_RPT_GetFinancialPlanningReport");
                 db.AddInParameter(cmd, "@CustomerId", DbType.String, report.CustomerId);
-                
+
                 ds = db.ExecuteDataSet(cmd);
-                dtCustomerDetails.Columns.Add("Name",typeof(string));
+                dtCustomerDetails.Columns.Add("Name", typeof(string));
                 dtCustomerDetails.Columns.Add("Dob", typeof(string));
 
                 dtSpouseDetails.Columns.Add("Name", typeof(string));
                 dtSpouseDetails.Columns.Add("Dob", typeof(string));
+
+                dtTempChildrenDetails.Columns.Add("Name", typeof(string));
+                dtTempChildrenDetails.Columns.Add("Dob", typeof(string));
+                dtTempChildrenDetails.Columns.Add("YearEducation", typeof(string));
+                dtTempChildrenDetails.Columns.Add("YearMarriage", typeof(string));
 
                 dtChildrenDetails.Columns.Add("Name", typeof(string));
                 dtChildrenDetails.Columns.Add("Dob", typeof(string));
@@ -62,9 +67,9 @@ namespace DaoReports
                             if (!string.IsNullOrEmpty(dr["DOB"].ToString().Trim()))
                                 drCustomer["Dob"] = String.Format("{0:dd MMM yyyy}", DateTime.Parse(dr["DOB"].ToString().Trim()));
                             else
-                                drCustomer["Dob"] ="-";
+                                drCustomer["Dob"] = "-";
                             dtCustomerDetails.Rows.Add(drCustomer);
-                          
+
                         }
                         else if (dr["RelationShipCode"].ToString() == "SP")
                         {
@@ -78,15 +83,15 @@ namespace DaoReports
                             else
                                 drSpouse["Dob"] = "-";
                             dtSpouseDetails.Rows.Add(drSpouse);
- 
+
                         }
-                        else if (dr["RelationShipCode"].ToString() == "CH" && dr["IsGoalActive"].ToString()=="1")
+                        else if (dr["RelationShipCode"].ToString() == "CH" && (dr["IsGoalActive"].ToString() == "1" || string.IsNullOrEmpty(dr["IsGoalActive"].ToString())))
                         {
-                            DataRow drChild = dtChildrenDetails.NewRow();
+                            DataRow drChild = dtTempChildrenDetails.NewRow();
                             if (!string.IsNullOrEmpty(dr["Name"].ToString()))
-                            drChild["Name"] = dr["Name"].ToString();
+                                drChild["Name"] = dr["Name"].ToString();
                             else
-                                drChild["Name"] ="-";
+                                drChild["Name"] = "-";
                             if (!string.IsNullOrEmpty(dr["DOB"].ToString().Trim()))
                                 drChild["Dob"] = String.Format("{0:dd MMM yyyy}", DateTime.Parse(dr["DOB"].ToString().Trim()));
                             else
@@ -99,14 +104,77 @@ namespace DaoReports
                                 drChild["YearMarriage"] = dr["YearOfMarriage"].ToString();
                             else
                                 drChild["YearMarriage"] = "-";
-                            dtChildrenDetails.Rows.Add(drChild);
- 
+                            dtTempChildrenDetails.Rows.Add(drChild);
+
                         }
 
- 
+
                     }
 
                 }
+                if (dtTempChildrenDetails.Rows.Count > 0)
+                {
+                    for (int rowNo = 0; rowNo < dtTempChildrenDetails.Rows.Count; rowNo++)
+                    {
+                        DataRow drChild = dtChildrenDetails.NewRow();
+                        if (rowNo != dtTempChildrenDetails.Rows.Count)
+                        {
+                            if (rowNo == dtTempChildrenDetails.Rows.Count - 1)
+                            {
+                                drChild["Name"] = dtTempChildrenDetails.Rows[rowNo]["Name"].ToString();
+                                drChild["Dob"] = dtTempChildrenDetails.Rows[rowNo]["Dob"].ToString();
+                                drChild["YearEducation"] = dtTempChildrenDetails.Rows[rowNo]["YearEducation"].ToString();
+                                drChild["YearMarriage"] = dtTempChildrenDetails.Rows[rowNo]["YearMarriage"].ToString();
+                                dtChildrenDetails.Rows.Add(drChild);
+
+                            }
+                            else
+                            {
+                                if (dtTempChildrenDetails.Rows[rowNo]["Name"].ToString().Trim() == dtTempChildrenDetails.Rows[rowNo + 1]["Name"].ToString().Trim())
+                                {
+                                    drChild["Name"] = dtTempChildrenDetails.Rows[rowNo]["Name"].ToString();
+                                    drChild["Dob"] = dtTempChildrenDetails.Rows[rowNo]["Dob"].ToString();
+                                    if (dtTempChildrenDetails.Rows[rowNo]["YearEducation"].ToString()!="-")
+                                        drChild["YearEducation"] = dtTempChildrenDetails.Rows[rowNo]["YearEducation"].ToString();
+                                    else
+                                        drChild["YearEducation"] = dtTempChildrenDetails.Rows[rowNo + 1]["YearEducation"].ToString();
+                                    if (dtTempChildrenDetails.Rows[rowNo]["YearMarriage"].ToString()!="-")
+                                        drChild["YearMarriage"] = dtTempChildrenDetails.Rows[rowNo]["YearMarriage"].ToString();
+                                    else
+                                        drChild["YearMarriage"] = dtTempChildrenDetails.Rows[rowNo + 1]["YearMarriage"].ToString();
+
+                                    dtChildrenDetails.Rows.Add(drChild);
+                                    rowNo++;
+                                }
+                                else
+                                {
+                                    drChild["Name"] = dtTempChildrenDetails.Rows[rowNo]["Name"].ToString();
+                                    drChild["Dob"] = dtTempChildrenDetails.Rows[rowNo]["Dob"].ToString();
+                                    drChild["YearEducation"] = dtTempChildrenDetails.Rows[rowNo]["YearEducation"].ToString();
+                                    drChild["YearMarriage"] = dtTempChildrenDetails.Rows[rowNo]["YearMarriage"].ToString();
+                                    dtChildrenDetails.Rows.Add(drChild);
+
+
+                                }
+                            }
+                        }
+
+
+                    }
+
+                    //int rowCount = 0;
+                    //foreach (DataRow dr in dtTempChildrenDetails.Rows)
+                    //{
+                    //    if (rowCount == 0)
+                    //    {
+
+                    //    }
+                    //    DataRow drChild = dtChildrenDetails.NewRow();
+
+
+                    //}
+                }
+
                 ds.Tables.Add(dtCustomerDetails);
                 ds.Tables.Add(dtSpouseDetails);
                 ds.Tables.Add(dtChildrenDetails);
@@ -136,7 +204,7 @@ namespace DaoReports
                 //    ds.Tables[0].Rows.Add(drGoal);
 
                 //}
-                
+
             }
             catch (BaseApplicationException Ex)
             {
