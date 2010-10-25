cef1d94bc2bcb3d8b8aa3f572a1981a0194f1365
AMP\vsingh <AMP\vsingh>
2010-10-25 14:12:19 +0530

    Customer Stability: Is Prospect flag added.

--- WealthERPSolution/Dao/CustomerProfiling/CustomerDao.cs
+++ WealthERPSolution/Dao/CustomerProfiling/CustomerDao.cs
@@ -266,6 +266,17 @@ namespace DaoCustomerProfiling
                         customerVo.AlertViaEmail = int.Parse(dr["C_AlertViaEmail"].ToString());
 
                     }
+                    if (dr["C_IsProspect"] == null)
+                    {
+                        customerVo.IsProspect = 0;
+
+
+                    }
+                    else
+                    {
+                        customerVo.IsProspect = int.Parse(dr["C_IsProspect"].ToString());
+
+                    }
                     customerVo.AdviseNote = dr["C_Comments"].ToString();
                     if (!string.IsNullOrEmpty(dr["ACC_CustomerClassificationId"].ToString()))
                     {
@@ -782,6 +793,7 @@ namespace DaoCustomerProfiling
               
                
                 db.AddInParameter(editCustomerCmd, "@C_DummyPAN", DbType.String, customerVo.DummyPAN);
+                db.AddInParameter(editCustomerCmd, "@C_Prospect", DbType.String, customerVo.IsProspect);
                 db.AddInParameter(editCustomerCmd, "@C_mail", DbType.String, customerVo.AlertViaEmail);
                 db.AddInParameter(editCustomerCmd, "@C_sms", DbType.String, customerVo.ViaSMS);
                
@@ -1406,7 +1418,7 @@ namespace DaoCustomerProfiling
                 db.AddInParameter(createCustomerCmd, "@XQ_QualificationCode", DbType.String, customerVo.Qualification);
                 db.AddInParameter(createCustomerCmd, "@XMS_MaritalStatusCode", DbType.String, customerVo.MaritalStatus);
                 db.AddInParameter(createCustomerCmd, "@XN_NationalityCode", DbType.String, customerVo.Nationality);
-                db.AddInParameter(createCustomerCmd, "@C_IsProspect", DbType.Int32, customerVo.IsProspect);
+               // db.AddInParameter(createCustomerCmd, "@C_IsProspect", DbType.Int32, customerVo.IsProspect);
                 db.AddInParameter(createCustomerCmd, "@C_IsFPClient", DbType.Int32, customerVo.IsFPClient);  
                 //Customer Marriage Date
 
@@ -1485,6 +1497,7 @@ namespace DaoCustomerProfiling
                 db.AddInParameter(createCustomerCmd, "@C_CreatedBy", DbType.Int32, userId);
                 db.AddInParameter(createCustomerCmd, "@C_ModifiedBy", DbType.Int32, userId);
                 db.AddInParameter(createCustomerCmd, "@C_DummyPAN", DbType.Int32, customerVo.DummyPAN);
+                db.AddInParameter(createCustomerCmd, "@C_Prospect", DbType.Int32, customerVo.IsProspect);
 
                 db.AddOutParameter(createCustomerCmd, "@C_CustomerId", DbType.Int32, 10);
                 db.AddOutParameter(createCustomerCmd, "@U_UserId", DbType.Int32, 10);
