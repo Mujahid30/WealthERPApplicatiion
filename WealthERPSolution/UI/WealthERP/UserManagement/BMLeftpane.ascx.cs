a5297a5df83242c24b3180af31aeeb949c48dcdc
AMP\vpatil <AMP\vpatil>
2010-11-09 17:21:19 +0530

    Merging TB & WERP : BMLeftpane merging successfully done.

--- WealthERPSolution/UI/WealthERP/UserManagement/BMLeftpane.ascx.cs
+++ WealthERPSolution/UI/WealthERP/UserManagement/BMLeftpane.ascx.cs
@@ -38,6 +38,7 @@ namespace WealthERP.UserManagement
             if (!IsPostBack)
             {
                 ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "pageloadscript", "loadtopmenu('BMLeftpane');", true);
+                BMLeftTree.CollapseAll();
             }
         }
 
@@ -61,7 +62,7 @@ namespace WealthERP.UserManagement
                 count = roleList.Count;
                 if (count == 3)
                 {
-                    Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loginloadcontrol('AdvisorRMBMDashBoard','login');", true);
+                    Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loginloadcontrol('AdvisorRMBMDashBoard','login','" + UserName + "','" + sourcepath + "','" + branchLogoSourcePath + "');", true);
                 }
                 if (count == 2)
                 {
@@ -77,12 +78,12 @@ namespace WealthERP.UserManagement
                 }
                 if (count == 1 && userVo.UserType == "Branch Man")
                 {
-                    Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loginloadcontrol('BMDashBoard','login');", true);
+                    Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loginloadcontrol('BMDashBoard','login','" + UserName + "','" + sourcepath + "','" + branchLogoSourcePath + "');", true);
                 }
             }
             else if (BMLeftTree.SelectedNode.Value == "Dashboard")
             {
-                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loginloadcontrol('BMDashBoard','login');", true);
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loginloadcontrol('BMDashBoard','login','" + UserName + "','" + sourcepath + "','" + branchLogoSourcePath + "');", true);
             }
             //else if (BMLeftTree.SelectedNode.Value == "View Branch Details")
             //{
