5971a425d461a85f362729f81ed565bdf2945f5b
AMP\vinothvs <AMP\vinothvs>
2010-12-01 01:34:44 +0530

    FP Superlite -> Vo

---
+++ WealthERPSolution-MoneyTouch/Vo/VoFPSuperlite/CustomerProspectIncomeDetailsVo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VoFPSuperlite
+{
+    public class CustomerProspectIncomeDetailsVo
+    {
+        private int FP_IncomeDetailsId;
+
+        
+        private int FPS_CP_IncomeCategoryCode;
+
+        private double FPS_CP_IncomeValue;
+
+        public int IncomeDetailsId
+        {
+            get { return FP_IncomeDetailsId; }
+            set { FP_IncomeDetailsId = value; }
+        }
+        public int IncomeCategoryCode
+        {
+            get { return FPS_CP_IncomeCategoryCode; }
+            set { FPS_CP_IncomeCategoryCode = value; }
+        }
+        public double IncomeValue
+        {
+            get { return FPS_CP_IncomeValue; }
+            set { FPS_CP_IncomeValue = value; }
+        }
+
+    }
+}
