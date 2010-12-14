83d5bae6b6ff6e7b994703f0ae1193ff740b0777
AMP\vinothvs <AMP\vinothvs>
2010-12-14 18:55:16 +0530

    Logout is not working... Status: Fixed

--- WealthERPSolution-MoneyTouch/UI/WealthERP/General/UserLogout.ascx.cs
+++ WealthERPSolution-MoneyTouch/UI/WealthERP/General/UserLogout.ascx.cs
@@ -13,7 +13,7 @@ namespace WealthERP.General
         {
             if (Session["Theme"] != "PCG")
             {
-                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "window.parent.location.href = (window.parent.location.href).substring(0,window.parent.location.href.indexOf('&UserId'));", true);
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "window.parent.location.href = window.parent.location.href;", true);
             }
             Session.Clear();
             Session.Abandon();
