32cda65684a5c5170a69e68b82ebaf320149944f
AMP\vinothvs <AMP\vinothvs>
2011-02-08 10:00:49 +0530

    Few Modification made for SuperAdmin, REgisterAdviser and GetAdviserXMLCategory

--- WealthERPSolution/Bo/AdvisorProfiling/AdvisorBo.cs
+++ WealthERPSolution/Bo/AdvisorProfiling/AdvisorBo.cs
@@ -1013,5 +1013,82 @@ namespace BoAdvisorProfiling
             return dsUserPotentialHomepage;
 
         }
+
+        /// <summary>
+        /// Gets category that adviser belongs to
+        /// </summary>
+        /// <returns></returns>
+        public DataSet GetXMLAdvisorCategory()
+        {
+            DataSet dsGetXMLAdvisorCategory;
+            AdvisorDao advisorDao = new AdvisorDao();
+            try
+            {
+                dsGetXMLAdvisorCategory = advisorDao.GetXMLAdvisorCategory();
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdvisorBo.cs:GetAdviserSubscriptionDetails(int adviserId)");
+
+
+                object[] objects = new object[1];
+                objects[0] = "GetXMLAdvisorCategory";
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+
+            }
+            return dsGetXMLAdvisorCategory;
+        }
+
+        /// <summary>
+        /// Updates Complete Advisor which includes AdviserRM,Adviser and Adviser Table
+        /// Caution:Please use this only if it is needed.
+        /// </summary>
+        /// <param name="userVo"></param>
+        /// <param name="adviserVo"></param>
+        /// <param name="rmVo"></param>
+        public void UpdateCompleteAdviser(UserVo userVo, AdvisorVo adviserVo, RMVo rmVo)
+        {
+
+            AdvisorDao advisorDao = new AdvisorDao();
+
+
+            try
+            {
+                advisorDao.UpdateCompleteAdviser(userVo, adviserVo, rmVo);
+
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "AdvisorBo.cs:CreateCompleteAdviser()");
+                object[] objects = new object[3];
+                objects[0] = adviserVo;
+                objects[1] = rmVo;
+                objects[2] = userVo;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+
+
+
+            }
+        }
     }
 }
