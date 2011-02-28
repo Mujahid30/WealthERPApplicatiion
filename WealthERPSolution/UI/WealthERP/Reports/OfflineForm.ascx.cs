946a828be22dde2605428f605b89521a829625e0
AMP\Kirteeshree <AMP\Kirteeshree>
2011-02-28 11:56:06 +0530

    R 2.5 New FPOfflineform Crystal Report

---
+++ WealthERPSolution/UI/WealthERP/Reports/OfflineForm.ascx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BoCommon;
+using VoUser;
+using BoCustomerProfiling;
+using System.Configuration;
+using WealthERP.Base;
+using BoCustomerPortfolio;
+using VoCustomerPortfolio;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+using System.Text;
+using VoReports;
+using System.Web.UI;
+
+namespace WealthERP.Reports
+{
+    public partial class OfflineForm : System.Web.UI.Page
+    {
+        RMVo rmVo = new RMVo();
+        CustomerBo customerBo = new CustomerBo();
+        UserVo userVo = new UserVo();
+        AdvisorVo advisorVo = null;
+        string path = string.Empty;
+        FPOfflineFormVo fpofflineForm = new FPOfflineFormVo();
+        CustomerVo customerVo = new CustomerVo();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SessionBo.CheckSession();
+            if (Request.Form["ctrl_EquityReports$btnViewInPDF"] != "View Report")
+            {
+                rmVo = (RMVo)Session[SessionContents.RmVo];
+            }
+        }
+    }
+}
