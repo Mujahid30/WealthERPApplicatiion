aded2d0bdc513cd3fa06ef3f21c9ebb80a7c4065
AMP\psahoo:5 <AMP\psahoo:5>
2011-05-18 13:42:06 +0530

    RE:3.0:FP Goal Work in progress

--- WealthERPSolution/Bo/BoFPSuperlite/CustomerGoalPlanningBo.cs
+++ WealthERPSolution/Bo/BoFPSuperlite/CustomerGoalPlanningBo.cs
@@ -77,13 +77,20 @@ namespace BoFPSuperlite
         }
 
 
-        public void CreateCustomerGoalPlanning(CustomerGoalPlanningVo GoalPlanningVo, int UserId, int Flag)
+        public void CreateCustomerGoalPlanning(CustomerGoalPlanningVo goalPlanningVo, int UserId, int Flag)
         {
             try
             {
                 CustomerGoalPlanningVo customerGoalPlanningVo = new CustomerGoalPlanningVo();
-                customerGoalPlanningVo = CalculateGoalProfile(GoalPlanningVo);
+               
                 CustomerGoalPlanningDao customerGoalPlanningDao = new CustomerGoalPlanningDao();
+                CustomerAssumptionVo customerAssumptionVo = new CustomerAssumptionVo();
+                customerAssumptionVo = customerGoalPlanningDao.GetAllCustomerAssumption(goalPlanningVo.CustomerId, goalPlanningVo.GoalYear);
+                if (goalPlanningVo.Goalcode!="RT")
+                customerGoalPlanningVo = CalculateGoalProfile(goalPlanningVo, customerAssumptionVo);
+                else
+                customerGoalPlanningVo = CalculateGoalProfileRT(goalPlanningVo, customerAssumptionVo);
+
                 if (Flag == 0)
                     customerGoalPlanningDao.CreateCustomerGoalPlanning(customerGoalPlanningVo, UserId);
                 //else
@@ -100,7 +107,7 @@ namespace BoFPSuperlite
                 NameValueCollection FunctionInfo = new NameValueCollection();
                 FunctionInfo.Add("Method", "CustomerGoalPlanningBo.cs:CreateCustomerGoalPlanning()");
                 object[] objects = new object[2];
-                objects[0] = GoalPlanningVo;
+                objects[0] = goalPlanningVo;
                 objects[1] = UserId;
                 FunctionInfo = exBase.AddObject(FunctionInfo, objects);
                 exBase.AdditionalInformation = FunctionInfo;
@@ -113,7 +120,7 @@ namespace BoFPSuperlite
         }
 
 
-        public CustomerGoalPlanningVo CalculateGoalProfile(CustomerGoalPlanningVo GoalPlanningVo)
+        public CustomerGoalPlanningVo CalculateGoalProfile(CustomerGoalPlanningVo goalPlanningVo, CustomerAssumptionVo customerAssumptionVo)
         {
             double futureInvValue = 0;
             double futureCost = 0;
@@ -125,18 +132,19 @@ namespace BoFPSuperlite
             double rateOfReturn = 0;
             double inflationValues = 0;
             string goal = string.Empty;
-            double InflationPercent = 8; ;
-
+            double InflationPercent = 8;
+            
             try
             {
-                costToday = GoalPlanningVo.CostOfGoalToday;
+                costToday = goalPlanningVo.CostOfGoalToday;
                 // requiredAfter = GoalProfileVo.GoalYear;
-                requiredAfter = GoalPlanningVo.GoalYear - DateTime.Today.Year;
-                currentValue = GoalPlanningVo.CurrInvestementForGoal;
-                rateEarned = GoalPlanningVo.ROIEarned / 100;
-                rateOfReturn = GoalPlanningVo.ExpectedROI / 100;
-                inflationValues = (Double)InflationPercent / 100;
+                requiredAfter = goalPlanningVo.GoalYear - DateTime.Today.Year;
+                //currentValue = goalPlanningVo.CurrInvestementForGoal;
+                //rateEarned = goalPlanningVo.ROIEarned / 100;
+                //rateOfReturn = goalPlanningVo.ExpectedROI / 100;
+                inflationValues = (Double)customerAssumptionVo.InflationPercent / 100;
 
+                
                 futureCost = Math.Abs(FutureValue(inflationValues, requiredAfter, 0, costToday, 0));
                 if (currentValue == 0 && rateEarned == 0)
                 {
@@ -147,11 +155,11 @@ namespace BoFPSuperlite
                     futureInvValue = Math.Abs(FutureValue(rateEarned, requiredAfter, 0, currentValue, 0));
 
                 requiredSavings = Math.Abs(PMT((rateOfReturn / 12), (requiredAfter * 12), 0, (futureCost - futureInvValue), 0));
-                GoalPlanningVo.MonthlySavingsReq = requiredSavings;
-                GoalPlanningVo.InflationPercent = (Double)InflationPercent;
-                GoalPlanningVo.FutureValueOfCostToday = (Double)futureCost;
+                goalPlanningVo.MonthlySavingsReq =Math.Round(requiredSavings,2);
+                goalPlanningVo.InflationPercent = (Double)InflationPercent;
+                goalPlanningVo.FutureValueOfCostToday =Math.Round((Double)futureCost,2);
 
-                return GoalPlanningVo;
+                return goalPlanningVo;
             }
             catch (BaseApplicationException Ex)
             {
@@ -163,7 +171,7 @@ namespace BoFPSuperlite
                 NameValueCollection FunctionInfo = new NameValueCollection();
                 FunctionInfo.Add("Method", "CustomerGoalPlanningBo.cs:CalculateGoalProfile()");
                 object[] objects = new object[1];
-                objects[0] = GoalPlanningVo;
+                objects[0] = goalPlanningVo;
                 FunctionInfo = exBase.AddObject(FunctionInfo, objects);
                 exBase.AdditionalInformation = FunctionInfo;
                 ExceptionManager.Publish(exBase);
@@ -174,9 +182,63 @@ namespace BoFPSuperlite
         }
 
 
+        public CustomerGoalPlanningVo CalculateGoalProfileRT(CustomerGoalPlanningVo goalPlanningVo, CustomerAssumptionVo customerAssumptionVo)
+        {
+           
+            double yearsLeftForRetirement = customerAssumptionVo.RetirementAge - customerAssumptionVo.CustomerAge;
+            double amountAfterFirstMonthRetirement;
+            double spouseLifeAfterCustomerRet;
+            double adjustedInfluation;
+            double corpusReqAtTimeOfRetirement;
+            double amountToBeSavedPerMonth;
+
+            try
+            {
+                amountAfterFirstMonthRetirement = FutureValue(customerAssumptionVo.InflationPercent / 100, yearsLeftForRetirement, 0, -goalPlanningVo.CostOfGoalToday, 1);
+                spouseLifeAfterCustomerRet = customerAssumptionVo.SpouseEOL - customerAssumptionVo.RetirementAge + (customerAssumptionVo.CustomerAge - customerAssumptionVo.RetirementAge);
+                adjustedInfluation = ((1 + customerAssumptionVo.PostRetirementReturn / 100) / (1 + customerAssumptionVo.InflationPercent / 100) - 1) * 100;
+                if (customerAssumptionVo.CorpusToBeLeftBehind != 0)
+                {
+                    corpusReqAtTimeOfRetirement = PV(adjustedInfluation / 1200, spouseLifeAfterCustomerRet * 12, -amountAfterFirstMonthRetirement, 0, 1);
+                }
+                else
+                {
+                    corpusReqAtTimeOfRetirement = PV(adjustedInfluation / 1200, spouseLifeAfterCustomerRet * 12, -amountAfterFirstMonthRetirement, -customerAssumptionVo.CorpusToBeLeftBehind, 1);
+                }
+                amountToBeSavedPerMonth = PMT(customerAssumptionVo.ReturnOnNewInvestment / 1200, yearsLeftForRetirement * 12, 0, -corpusReqAtTimeOfRetirement, 1);
+                goalPlanningVo.MonthlySavingsReq = amountToBeSavedPerMonth;
+                goalPlanningVo.CorpusLeftBehind = customerAssumptionVo.CorpusToBeLeftBehind;
+
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "CustomerGoalPlanningBo.cs:CalculateGoalProfileRT()");
+
+
+                object[] objects = new object[2];
+                objects[0] = goalPlanningVo;
+                objects[1] = customerAssumptionVo;
 
 
 
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+
+            }
+            return goalPlanningVo;
+
+        }
+
+        
         /// <summary>/// Calculates the present value of a loan based upon constant payments and a constant interest rate.
         /// </summary>
         /// <param name="rate">The interest rate.</param>
@@ -206,5 +268,42 @@ namespace BoFPSuperlite
             result = System.Numeric.Financial.Pmt(rate, nper, pv, fv, 0);
             return result;
         }
+
+
+        public DataSet GetCustomerGoalDetails(int CustomerID)
+        {
+            CustomerGoalPlanningDao customerGoalPlanningDao = new CustomerGoalPlanningDao();
+            DataSet customerGoalDetailsDS;
+
+            try
+            {
+                customerGoalDetailsDS = customerGoalPlanningDao.GetCustomerGoalDetails(CustomerID);
+
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "CustomerGoalPlanningBo:GetCustomerGoalDetails()");
+
+                object[] objects = new object[1];
+                objects[0] = CustomerID;
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+
+            }
+
+            return customerGoalDetailsDS;
+
+        }
+
     }
 }
