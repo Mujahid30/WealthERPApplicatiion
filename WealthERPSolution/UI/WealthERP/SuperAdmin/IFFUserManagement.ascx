664c4fe21612f0a15b2f970b74331ff89e6fffe2
AMP\vpatil <AMP\vpatil>
2011-06-29 17:00:09 +0530

    Re: 3.0 IFF UserManagement..

--- WealthERPSolution/UI/WealthERP/SuperAdmin/IFFUserManagement.ascx
+++ WealthERPSolution/UI/WealthERP/SuperAdmin/IFFUserManagement.ascx
@@ -1,1 +1,225 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="IFFUserManagement.ascx.cs" Inherits="WealthERP.SuperAdmin.IFFUserManagement" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolKit" %>
+<%@ Register Src="~/General/Pager.ascx" TagPrefix="Pager" TagName="Pager" %>
+
+<script src="/Scripts/jquery.js" type="text/javascript"></script>
+
+<script src="/Scripts/jquery.colorbox-min.js" type="text/javascript"></script>
+
+<link href="/CSS/colorbox.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.loadme').click(function() {
+            var panel = document.getElementById('<%= gvIFFUsers.ClientID %>');
+            var chkArray = panel.getElementsByTagName("input");
+            var checked = 0;
+            for (var i = 0; i < chkArray.length; i++) {
+                if (chkArray[i].type == "checkbox" && chkArray[i].checked == true) {
+                    checked = 1;
+                    break;
+                }
+            }
+
+            if (checked != 1) {
+                alert('Please select RM to send Password');
+                return false;
+            }
+            else {
+                $(".loadmediv").colorbox({ width: "260px", overlayClose: false, inline: true, open: true, href: "#LoadImage" });
+            }
+        });
+
+    });
+
+
+
+    function CheckAll() {
+        alert("abc..");
+        var panel = document.getElementById('<%= gvIFFUsers.ClientID %>');
+        var chkArray = gvIFFUsers.getElementsByTagName("input");
+        alert(chkArray);
+        for (var i = 0; i < chkArray.length; i++) {
+            if (chkArray[i].type == "checkbox") {
+                alert(chkArray[i]);
+                if (chkArray[i].checked == false) {
+
+                    chkArray[i].checked = true;
+                }
+                else
+                    chkArray[i].checked = false;
+            }
+        }
+
+
+
+    };
+
+    function checkAllBoxes() {
+
+        //get total number of rows in the gridview and do whatever
+        //you want with it..just grabbing it just cause
+        var totalChkBoxes = parseInt('<%= gvIFFUsers.Rows.Count %>');
+        var gvControl = document.getElementById('<%= gvIFFUsers.ClientID %>');
+
+        //this is the checkbox in the item template...this has to be the same name as the ID of it
+        var gvChkBoxControl = "chkBoxChild";
+
+        //this is the checkbox in the header template
+        var mainChkBox = document.getElementById("chkBoxAll");
+
+        //get an array of input types in the gridview
+        var inputTypes = gvControl.getElementsByTagName("input");
+
+        for (var i = 0; i < inputTypes.length; i++) {
+            //if the input type is a checkbox and the id of it is what we set above
+            //then check or uncheck according to the main checkbox in the header template            
+            if (inputTypes[i].type == 'checkbox' && inputTypes[i].id.indexOf(gvChkBoxControl, 0) >= 0)
+                inputTypes[i].checked = mainChkBox.checked;
+        }
+    } 
+    
+   
+</script>
+
+<table width="100%" class="TableBackground">
+    <tr>
+        <td class="HeaderCell">
+            <asp:Label ID="lblTitle" runat="server" CssClass="HeaderTextBig" Text="IFF User Details"></asp:Label>
+            <hr />
+        </td>
+    </tr>
+</table>
+
+ <table id="tblMessage" width="100%" cellspacing="0" cellpadding="0" runat="server" visible="false">
+    <tr>
+        <td align="center">
+            <div class="success-msg" id="SuccessMsg" runat="server" visible="false" align="center">
+            </div>
+            <div class="failure-msg" id="ErrorMessage" runat="server" visible="false" align="center">
+            </div>
+        </td>
+   </tr>
+ </table>
+ 
+ <table style="width: 100%;">
+    <tr id="trNoRecords" runat="server">
+        <td>
+            <asp:Label ID="lblMsg" class="Error" runat="server" Visible="false" Text="No Records Found...!"></asp:Label>
+        </td>
+    </tr>
+    <tr id="trPagger" runat="server">
+        <td class="leftField">
+            <asp:Label ID="lblCurrentPage" class="Field" runat="server"></asp:Label>
+            <asp:Label ID="lblTotalRows" class="Field" runat="server"></asp:Label>
+        </td>
+    </tr>
+    
+    <tr>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr>
+        <td align="center">
+            <asp:GridView ID="gvIFFUsers" runat="server" CellPadding="4" CssClass="GridViewStyle"
+                AllowSorting="True" ShowFooter="true" AutoGenerateColumns="False" 
+                DataKeyNames="UserId" onrowcommand="gvIFFUsers_RowCommand">
+                <RowStyle CssClass="RowStyle" />
+                <FooterStyle CssClass="FooterStyle" />
+                <PagerStyle HorizontalAlign="Center" CssClass="PagerStyle" />
+                <SelectedRowStyle CssClass="SelectedRowStyle" />
+                <HeaderStyle CssClass="HeaderStyle" HorizontalAlign="Left" VerticalAlign="Middle" />
+                <EditRowStyle CssClass="EditRowStyle" />
+                <AlternatingRowStyle CssClass="AltRowStyle " />
+                <Columns>
+                    <asp:TemplateField ItemStyle-Wrap="false" HeaderStyle-Wrap="false" >
+                        <HeaderTemplate>
+                            <br />
+                           <input id="chkBoxAll"  name="vehicle" value="Bike" type="checkbox" onclick="checkAllBoxes()" />
+                        </HeaderTemplate>
+                        <ItemTemplate>
+                            <asp:CheckBox ID="chkBoxChild" runat="server" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField ItemStyle-Wrap="false" HeaderStyle-Wrap="false">
+                        <HeaderTemplate>
+                            <asp:Label ID="lblIFFName" runat="server" Text="IFF Name"></asp:Label>
+                            <br />
+                            <asp:TextBox ID="txtIFFNameSearch" runat="server" Text='<%# hdnNameFilter.Value %>'
+                                CssClass="GridViewTxtField" onkeydown="return JSdoPostback(event,'ctrl_IFFUserManagement_btnNameSearch');" />
+                        </HeaderTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="lblIFFNameHeader" runat="server" Text='<%# Eval("IFFName").ToString() %>'></asp:Label>
+                        </ItemTemplate>
+                        <HeaderStyle Wrap="False"></HeaderStyle>
+                        <ItemStyle Wrap="False"></ItemStyle>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="LoginId" HeaderText="Login Id" />
+                    <asp:BoundField DataField="EmailId" HeaderText="Email Id" />
+                    <asp:ButtonField CommandName="resetPassword" Text="Reset Password" />
+                </Columns>
+            </asp:GridView>
+
+            <Pager:Pager ID="mypager" runat="server"></Pager:Pager>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Label ID="lblStatus" class="FieldName" runat="server" Text=""></asp:Label>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Button ID="btnSendEmailToIFF" runat="server" Text="Send Login Password" onmouseover="javascript:ChangeButtonCss('hover', 'ctrl_SuperAdminMessageBroadcast_btnSendEmailToIFF', 'L');"
+                onmouseout="javascript:ChangeButtonCss('out', 'ctrl_SuperAdminMessageBroadcast_btnSendEmailToIFF', 'L');" 
+                CssClass="loadme PCGLongButton" onclick="btnSendEmailToIFF_Click" />
+            <div class='loadmediv'>
+            </div>
+        </td>
+    </tr>
+</table>
+
+<table id="tblPager" runat="server" align="center">
+    <tr>
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+</table>
+<table align="center" style="display: none;">
+    <tr>
+        <td>
+            <div id='LoadImage' style="width: 231px">
+                <table align="center" border="1">
+                    <tr>
+                        <td style="background-color: #3D77CB; color: #FFFFFF; font-size: 100%; font-weight: bold">
+                            Processing,please wait...
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="../Images/Wait.gif" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+    </tr>
+</table>
+
+<asp:Button ID="btnNameSearch" runat="server" Text="" BorderStyle="None" OnClick="btnNameSearch_Click" BackColor="Transparent" />
+<asp:HiddenField ID="hdnRecordCount" runat="server" />
+<asp:HiddenField ID="hdnSort" runat="server" Value="IFFName ASC" />
+<asp:HiddenField ID="hdnCurrentPage" runat="server" />
+<asp:HiddenField ID="hdnNameFilter" runat="server" />
+
+
+
+    
+    
+    
--- WealthERPSolution/UI/WealthERP/SuperAdmin/IFFUserManagement.ascx.cs
+++ WealthERPSolution/UI/WealthERP/SuperAdmin/IFFUserManagement.ascx.cs
@@ -1,17 +1,314 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
+using VoUser;
+using BoWerpAdmin;
+using BoCommon;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using System.Data;
+using System.Collections.Generic;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Net.Mail;
+using BoAdvisorProfiling;
+using VoAdvisorProfiling;
+using PCGMailLib;
+using BoUser;
 
 namespace WealthERP.SuperAdmin
 {
     public partial class IFFUserManagement : System.Web.UI.UserControl
     {
+        UserVo userVo = new UserVo();
+        AdvisorVo advisorVo = new AdvisorVo();
+        List<RMVo> IFFUserList = null;
+        int userId;
+        UserBo userBo = new UserBo();
+
+        AdviserMaintenanceBo adviserMaintenanceBo = new AdviserMaintenanceBo();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            SessionBo.CheckSession();
+            try
+            {
+                mypager.CurrentPage = 1;
+                if (!IsPostBack)
+                    this.BindGrid();
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "ViewRM.ascx.cs:Page_Load()");
+                object[] objects = new object[1];
 
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+        private void BindGrid()
+        {
+            try
+            {
+                advisorVo = (AdvisorVo)Session["advisorVo"];
+                userVo = (UserVo)Session["UserVo"];
+                if (hdnCurrentPage.Value.ToString() != "")
+                {
+                    mypager.CurrentPage = Int32.Parse(hdnCurrentPage.Value.ToString());
+                    hdnCurrentPage.Value = "";
+                }
+                ShowIFFs();
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "ViewRMD.ascx.cs:BindGrid()");
+                object[] objects = new object[2];
+                objects[0] = advisorVo;
+                objects[1] = userVo;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+        private void ShowIFFs()
+        {
+            List<AdvisorVo> IFvolist = new List<AdvisorVo>();
+            int count = 0;
+
+            try
+            {
+                IFvolist = adviserMaintenanceBo.GetIFFListForUserManagement(mypager.CurrentPage, out count, hdnSort.Value, hdnNameFilter.Value);
+                if (IFvolist.Count != 0)
+                {
+                    DataTable dtIFFList = new DataTable();
+
+                    dtIFFList.Columns.Add("UserId");
+                    dtIFFList.Columns.Add("IFFName");
+                    dtIFFList.Columns.Add("LoginId");
+                    dtIFFList.Columns.Add("EmailId");
+
+                    foreach (AdvisorVo advisorVo in IFvolist)
+                    {
+                        DataRow drIFFlist = dtIFFList.NewRow();
+                        drIFFlist["UserId"] = advisorVo.UserId;
+                        drIFFlist["IFFName"] = advisorVo.OrganizationName;
+                        drIFFlist["LoginId"] = advisorVo.LoginId;
+                        drIFFlist["EmailId"] = advisorVo.Email;
+                        dtIFFList.Rows.Add(drIFFlist);
+                    }
+                    gvIFFUsers.Visible = true;
+                    gvIFFUsers.DataSource = dtIFFList;
+                    gvIFFUsers.DataBind();
+                    ErrorMessage.Visible = false;
+                }
+                else
+                {
+                    gvIFFUsers.DataSource = null;
+                    gvIFFUsers.Visible = false;
+                    ErrorMessage.Visible = true;
+                }
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "SuperAdminMessageBroadcast.ascx.cs:ShowIFFs(string filterexpression)");
+                object[] objects = new object[3];
+                objects[0] = advisorVo;
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+        protected void btnSendEmailToIFF_Click(object sender, EventArgs e)
+        {
+            int selectedRecords = 0;
+            string statusMessage = string.Empty;
+            advisorVo = (AdvisorVo)Session["advisorVo"];
+            AdvisorBo advisorBo = new AdvisorBo();
+            RMVo rmVo = new RMVo();
+            AdvisorStaffBo advisorStaffBo = new AdvisorStaffBo();
+
+            if (Page.IsValid)
+            {
+                try
+                {
+                    foreach (GridViewRow gvr in gvIFFUsers.Rows)
+                    {
+                        if (((CheckBox)gvr.FindControl("chkBoxChild")).Checked == true)
+                        {
+                            selectedRecords++;
+
+                            userId = int.Parse(gvIFFUsers.DataKeys[gvr.RowIndex].Value.ToString());
+                            advisorVo = advisorBo.GetAdvisorUser(userId);
+
+
+                            rmVo = advisorStaffBo.GetAdvisorStaff(userId);
+
+                            Emailer emailer = new Emailer();
+                            EmailMessage email = new EmailMessage();
+
+                            userVo = userBo.GetUserDetails(userId);
+                            string userName = userVo.FirstName + " " + userVo.MiddleName + " " + userVo.LastName;
+                            email.GetAdviserRMAccountMail(userVo.LoginId, Encryption.Decrypt(userVo.Password), userName);
+                            email.Subject = email.Subject.Replace("WealthERP", advisorVo.OrganizationName);
+                            email.Subject = email.Subject.Replace("MoneyTouch", advisorVo.OrganizationName);
+                            email.Body = email.Body.Replace("[ORGANIZATION]", advisorVo.OrganizationName);
+                            email.To.Add(userVo.Email);
+
+                            AdviserStaffSMTPBo adviserStaffSMTPBo = new AdviserStaffSMTPBo();
+                            //int rmId = Convert.ToInt32(ViewState["rmId"]);
+
+                            AdviserStaffSMTPVo adviserStaffSMTPVo = adviserStaffSMTPBo.GetSMTPCredentials(rmVo.RMId);
+                            if (adviserStaffSMTPVo.HostServer != null && adviserStaffSMTPVo.HostServer != string.Empty)
+                            {
+                                emailer.isDefaultCredentials = !Convert.ToBoolean(adviserStaffSMTPVo.IsAuthenticationRequired);
+
+                                if (!String.IsNullOrEmpty(adviserStaffSMTPVo.Password))
+                                    emailer.smtpPassword = Encryption.Decrypt(adviserStaffSMTPVo.Password);
+                                emailer.smtpPort = int.Parse(adviserStaffSMTPVo.Port);
+                                emailer.smtpServer = adviserStaffSMTPVo.HostServer;
+                                emailer.smtpUserName = adviserStaffSMTPVo.Email;
+
+                                if (Convert.ToBoolean(adviserStaffSMTPVo.IsAuthenticationRequired))
+                                {
+                                    if (ConfigurationSettings.AppSettings["HostName"].ToString() == "Wealtherp")
+                                    {
+                                        email.From = new MailAddress(emailer.smtpUserName, advisorVo.OrganizationName);
+                                    }
+                                    else if (ConfigurationSettings.AppSettings["HostName"].ToString() == "MoneyTouch")
+                                    {
+                                        email.From = new MailAddress(emailer.smtpUserName, advisorVo.OrganizationName);
+                                    }
+
+                                }
+                            }
+                            bool isMailSent = emailer.SendMail(email);
+
+                            if (isMailSent)
+                            {
+                                statusMessage = "Credentials have been sent to selected Adviser";
+                                tblMessage.Visible = true;
+                                ErrorMessage.Visible = false;
+                                SuccessMsg.InnerText = statusMessage;
+                                SuccessMsg.Visible = true;
+                            }
+                            else
+                            {
+                                statusMessage = "An error occurred while sending mail to selected Adviser";
+                                tblMessage.Visible = true;
+                                ErrorMessage.Visible = true;
+                                ErrorMessage.InnerText = statusMessage;
+                                SuccessMsg.Visible = false;
+
+                            }
+                        }
+                    }
+                    ErrorMessage.Visible = false;
+
+                }
+                catch (BaseApplicationException Ex)
+                {
+                    throw Ex;
+                }
+                catch (Exception Ex)
+                {
+                    BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                    NameValueCollection FunctionInfo = new NameValueCollection();
+                    FunctionInfo.Add("Method", "RMCustomerUserDetails.ascx:btnGenerate_Click()");
+
+                    exBase.AdditionalInformation = FunctionInfo;
+                    ExceptionManager.Publish(exBase);
+                    throw exBase;
+                }
+            }
+        }
+
+        protected void btnNameSearch_Click(object sender, EventArgs e)
+        {
+            TextBox txtName = GetRMNameTextBox();
+
+            if (txtName != null)
+            {
+                hdnNameFilter.Value = txtName.Text.Trim();
+                this.ShowIFFs();
+            }
+        }
+
+        private TextBox GetRMNameTextBox()
+        {
+            TextBox txt = new TextBox();
+            if (gvIFFUsers.HeaderRow != null)
+            {
+                if ((TextBox)gvIFFUsers.HeaderRow.FindControl("txtIFFNameSearch") != null)
+                {
+                    txt = (TextBox)gvIFFUsers.HeaderRow.FindControl("txtIFFNameSearch");
+                }
+            }
+            else
+                txt = null;
+
+            return txt;
+        }
+
+        protected void gvIFFUsers_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            Random r = new Random();
+
+            bool isSuccess = false;
+
+            if (e.CommandName == "resetPassword")
+            {
+
+                userVo = userBo.GetUserDetails(Convert.ToInt32(gvIFFUsers.DataKeys[int.Parse(e.CommandArgument.ToString())].Value));
+                if (userVo != null)
+                {
+                    userVo.Password = r.Next(20000, 100000).ToString();
+                    userVo.IsTempPassword = 1;
+                    userVo.Password = Encryption.Encrypt(userVo.Password);
+                    isSuccess = userBo.UpdateUser(userVo);
+                }
+
+                if (isSuccess)
+                {
+                    tblMessage.Visible = true;
+                    SuccessMsg.Visible = true;
+                    ErrorMessage.Visible = false;
+                    SuccessMsg.InnerText = "Password has been reset successfully...";
+                }
+                else
+                {
+                    tblMessage.Visible = true;
+                    SuccessMsg.Visible = false;
+                    ErrorMessage.Visible = true;
+                    ErrorMessage.InnerText = "An error occurred while reseting password.";
+
+                }
+            }
         }
     }
 }
