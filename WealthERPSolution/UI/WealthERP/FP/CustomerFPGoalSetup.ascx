911125cbd905230f9d85ba331603bd8dd097d2d9
AMP\psahoo:5 <AMP\psahoo:5>
2011-07-02 18:48:41 +0530

    RE:3.0-FP Related Work Done

--- WealthERPSolution/UI/WealthERP/FP/CustomerFPGoalSetup.ascx
+++ WealthERPSolution/UI/WealthERP/FP/CustomerFPGoalSetup.ascx
@@ -20,26 +20,29 @@
 
     }
 
-    function ShowHideControls() {
-        var ddlGoalType = document.getElementById("<%=ddlGoalType.ClientID %>").value;
-        if ((ddlGoalType == 'ED') || (ddlGoalType == 'MR'))
-            document.getElementById("<%= trPickChild.ClientID %>").style.display = 'block';
-    }
-    
-    function ShowTrFrequency() {
-        var ddlOccurrence = document.getElementById("<%=ddlOccurrence.ClientID %>").value;
-        if (ddlOccurrence == 'Recurring') 
-              document.getElementById("<%= trFrequency.ClientID %>").style.display = 'block';
-    }
+//    function ShowHideControls() {
+//        var ddlGoalType = document.getElementById("<%=ddlGoalType.ClientID %>").value;
+//        if ((ddlGoalType == 'ED') || (ddlGoalType == 'MR'))
+//            document.getElementById("<%= trPickChild.ClientID %>").style.display = 'block';
+//    }
+//    
+//    function ShowTrFrequency() {
+//        var ddlOccurrence = document.getElementById("<%=ddlOccurrence.ClientID %>").value;
+//        if (ddlOccurrence == 'Recurring') 
+//              document.getElementById("<%= trFrequency.ClientID %>").style.display = 'block';
+//    }
 </script>
 
 <asp:Label ID="headertitle" runat="server" CssClass="HeaderTextBig" Text="Goal Setup"></asp:Label>
 <hr />
+
+
 <table width="100%" cellspacing="0" cellpadding="0">
 <tr>
 <td>
-<telerik:RadToolBar ID="aplToolBar" runat="server" Skin="Telerik" EnableEmbeddedSkins="false" EnableShadows="true" EnableRoundedCorners="true"
-    Width="100%">
+<telerik:RadToolBar ID="aplToolBar" runat="server" Skin="Telerik" 
+        EnableEmbeddedSkins="false" EnableShadows="true" EnableRoundedCorners="true"
+    Width="100%" onbuttonclick="aplToolBar_ButtonClick">
     <Items>
         <telerik:RadToolBarButton ID="btnEdit" runat="server" Text="Edit" Value="Edit" ImageUrl="~/Images/Telerik/EditButton.gif"
             ImagePosition="Left" ToolTip="Edit">            
@@ -66,7 +69,7 @@
 <tr>
     <td style="width:20%">
      <asp:RadioButton ID="rtbNonRT" Text="Non Retirement" runat="server" Class="FieldName" Checked="true" GroupName="GaolType" onClick="return ShowHideGaolType()"/>
-    </td>
+     </td>
     <td style="width:10%">
      <asp:RadioButton ID="rtbRT" runat="server" Text="Retirement" Class="FieldName" GroupName="GaolType" onClick="return ShowHideGaolType()"/>
     </td>
@@ -82,14 +85,19 @@
 <asp:Panel ID="PnlNonRetirement" runat="server">
 
 <table>
+           
+            
               <tr>
                     <td class="leftField">
                         <asp:Label ID="lblGoalbjective" runat="server" CssClass="FieldName" Text="Goal Objective :"></asp:Label>
                     </td>
                     <td class="rightField">
-                        <asp:DropDownList ID="ddlGoalType" runat="server" CssClass="cmbField" onchange="ShowHideControls()">
-                              
-                        </asp:DropDownList>
+                                      
+                         <asp:DropDownList ID="ddlGoalType" runat="server" CssClass="cmbField" 
+                             onselectedindexchanged="ddlGoalType_SelectedIndexChanged" AutoPostBack="true">                              
+                         </asp:DropDownList>
+                        
+                   
                         <span id="spanGoalType" class="spnRequiredField" runat="server">*</span> 
                         <%--<asp:RequiredFieldValidator id="RequiredFieldValidator3" ValidationGroup="btnSave" ControlToValidate="ddlGoalType" ErrorMessage="Expiration Date." Display="Dynamic" InitialValue="Select" Width="100%" runat="server">
                         </asp:Requiredfieldvalidator>--%>
@@ -101,7 +109,7 @@
                         </asp:Requiredfieldvalidator>--%>
                     </td>
                 </tr>
-                <tr id="trPickChild" runat="server" style="display:none">
+              <tr id="trPickChild" runat="server" >
                     <td id="Td4" class="leftField" runat="server">
                         <asp:Label ID="lblPickChild" runat="server" CssClass="FieldName" Text="Select a child for Goal planning :"></asp:Label>
                     </td>
@@ -110,6 +118,10 @@
                         </asp:DropDownList>
                     </td>
                 </tr>
+               
+            
+             
+             
                  <tr id="trGoalDesc" runat="server">
                     <td id="Td2" class="leftField" runat="server">
                         <asp:Label ID="lblGoalDescription" runat="server" CssClass="FieldName" Text="Goal Description :"></asp:Label>
@@ -165,7 +177,9 @@
                         <asp:Label ID="lblOccurrence" runat="server" CssClass="FieldName" Text="Occurrence :"></asp:Label>
                     </td>
                     <td class="rightField">
-                        <asp:DropDownList ID="ddlOccurrence" runat="server" CssClass="cmbField" onchange="ShowTrFrequency()">
+                        <asp:DropDownList ID="ddlOccurrence" runat="server" CssClass="cmbField" 
+                            onchange="ShowTrFrequency()" 
+                            onselectedindexchanged="ddlOccurrence_SelectedIndexChanged" AutoPostBack="true">
                         <asp:ListItem Text="Select" Value="Select">                                              
                         </asp:ListItem>
                         <asp:ListItem Text="One Time" Value="Once">                                              
@@ -197,7 +211,7 @@
                 </tr>  --%>          
                 
                 
-                <tr id="trFrequency" runat="server" style="display:none">
+                <tr id="trFrequency" runat="server">
                     <td class="leftField">
                         <asp:Label ID="lblFrequency" runat="server" CssClass="FieldName" Text="Frequency :" ></asp:Label>
                     </td>
@@ -205,12 +219,7 @@
                         <asp:DropDownList ID="ddlFrequency" runat="server" CssClass="cmbField">
                         <asp:ListItem Text="Select" Value="0">                                              
                         </asp:ListItem>
-                        <asp:ListItem Text="Quaterly" Value="Quaterly">                                              
-                        </asp:ListItem>
-                        <asp:ListItem Text="Half Yearly" Value="HalfYearly">                                              
-                        </asp:ListItem>   
-                        <asp:ListItem Text="Once In Two Year" Value="OnceInTwoYear">                                              
-                        </asp:ListItem>                        
+                                                
                         </asp:DropDownList>
                         <span id="span5" class="spnRequiredField" runat="server">*</span>                      
                     </td>
@@ -267,8 +276,9 @@
                 &nbsp;&nbsp;
                 </td>
                 <td>
-                 <asp:Button ID="btnNonRTSave" runat="server" CssClass="PCGMediumButton" 
-                            Text="Save" ValidationGroup="btnSave" onclick="btnNonRTSave_Click"  />
+                 <asp:Button ID="btnNonRTSave" runat="server" CssClass="PCGMediumButton" Text="Save" ValidationGroup="btnSave" onclick="btnNonRTSave_Click"  />
+                  <asp:Button ID="btnNonRTUpdate" runat="server" CssClass="PCGMediumButton" 
+                        Text="Update" ValidationGroup="btnSave" onclick="btnNonRTUpdate_Click" />
                 </td>
                 </tr>
                 
@@ -279,7 +289,7 @@
 <table>
     <tr>
     <td colspan="2">
-     <asp:Label ID="Label1" runat="server" CssClass="FieldName" Text="You have chosen to do retirement planning with "></asp:Label>
+     <asp:Label ID="lblRTGoalBasis" runat="server" CssClass="FieldName" Text="You have chosen to do retirement planning with "></asp:Label>
     </td>
     </tr>
     <tr>
@@ -291,7 +301,7 @@
                         </asp:TextBox>
                     </td>
 </tr>
-    <tr>
+    <tr id="trRTGoalCorpsToBeLeftBehind" runat="server">
                      <td class="leftField">
                         <asp:Label ID="Label2" runat="server" CssClass="FieldName" Text="Corpus to be left behind :"></asp:Label>
                     </td>
@@ -299,18 +309,25 @@
                         <asp:TextBox ID="txtRTCorpusToBeLeftBehind" runat="server" AutoCompleteType="Disabled" CssClass="txtField">
                         </asp:TextBox>
                     </td>
-</tr>
+ </tr>
     <tr>
                 <td>
                 &nbsp;&nbsp;
                 </td>
                 <td>
-                 <asp:Button ID="btnRTSave" runat="server" CssClass="PCGButton" 
-                            Text="Save" ValidationGroup="btnRTSave"  />
+                 <asp:Button ID="btnRTSave" runat="server" CssClass="PCGButton" Text="Save" 
+                        ValidationGroup="btnRTSave" onclick="btnRTSave_Click"  />
+                 <asp:Button ID="btnRTUpdate" runat="server" CssClass="PCGButton" Text="Update" ValidationGroup="btnRTSave"  />
                 </td>
                 </tr>
 </table>
 </asp:Panel>
+
+
+
+<asp:HiddenField ID="hidRTGoalCorpsLeftBehind" runat="server" />
+<asp:HiddenField ID="hidFPCalculationBasis" runat="server" />
+
 <script type="text/javascript">
     document.getElementById("<%= PnlNonRetirement.ClientID %>").style.display = 'block';
     document.getElementById("<%= PnlRetirement.ClientID %>").style.display = 'none';
--- WealthERPSolution/UI/WealthERP/FP/CustomerFPGoalSetup.ascx.cs
+++ WealthERPSolution/UI/WealthERP/FP/CustomerFPGoalSetup.ascx.cs
@@ -24,6 +24,9 @@ namespace WealthERP.FP
         CustomerVo customerVo = new CustomerVo();
         RMVo rmVo = new RMVo();
         DataSet dsAllDropDownDetails;
+        string goalAction = "";
+        string goalCategory = "";
+        int goalId = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
             rtbNonRT.Attributes.Add("onClick", "javascript:ShowHideGaolType(value);");
@@ -31,14 +34,36 @@ namespace WealthERP.FP
 
             rmVo = (RMVo)Session[SessionContents.RmVo];
             customerVo=(CustomerVo)Session["customerVo"];
+            DataTable dtFPCalculationBasis;
             
             if (!Page.IsPostBack)
             {
+                if (Request.QueryString["GoalId"] != null)
+                    goalId = int.Parse(Request.QueryString["GoalId"].ToString());
+                if (Request.QueryString["GoalCategory"] != null)
+                    goalCategory = Request.QueryString["GoalCategory"];
+                if (Request.QueryString["goalAction"] != null)
+                    goalAction = Request.QueryString["goalAction"];
+
+                dtFPCalculationBasis = goalPlanningBo.GetCustomerFPCalculationBasis(customerVo.CustomerId);
+                RTGoalPageLoadSetup(dtFPCalculationBasis);
                 dsAllDropDownDetails = goalPlanningBo.GetAllGoalDropDownDetails(customerVo.CustomerId);
                 BindGoalObjectiveDropDown(dsAllDropDownDetails.Tables["GoalObjective"]);
                 BindPickChildDropDown(dsAllDropDownDetails.Tables["ChildDetails"]);
                 BindFrequencyDropDown(dsAllDropDownDetails.Tables["GoalFrequency"]);                
-                BindGoalYearDropDown();            
+                BindGoalYearDropDown();
+
+                trPickChild.Visible = false;
+                trFrequency.Visible = false;
+                btnRTUpdate.Visible = false;
+                btnNonRTUpdate.Visible = false;
+
+               if(goalId!=0 && !string.IsNullOrEmpty(goalCategory) && !string.IsNullOrEmpty(goalAction))
+               {
+                   GoalViewEditByGoalId(goalId, goalAction, goalCategory);
+                   rtbNonRT.Enabled = false;
+                   rtbRT.Enabled = false;
+               }
             }
         }
         
@@ -119,10 +144,256 @@ namespace WealthERP.FP
             }
             goalPlanningVo.CreatedBy = int.Parse(rmVo.RMId.ToString());
 
-            goalPlanningBo.CreateCustomerGoalPlanning(goalPlanningVo, customerVo.CustomerId, 0);
-            //goalPlanningBo.
+            goalPlanningBo.CreateCustomerGoalPlanning(goalPlanningVo, customerVo.CustomerId,false);
+            
 
         }
 
+        protected void GoalViewEditByGoalId(int goalId, string goalAction,string goalCategory)
+        {
+            goalPlanningVo = goalPlanningBo.GetGoalDetails(goalId);
+            ViewState["GoalId"] = goalId;
+            if(goalCategory=="NonRT")
+            {
+                rtbNonRT.Checked = true;
+                rtbRT.Checked = false;
+                btnNonRTSave.Visible = false;
+                btnRTUpdate.Visible = false;
+                btnNonRTUpdate.Visible = true;
+ 
+
+            if (goalPlanningVo != null)
+            {
+                ddlGoalType.SelectedValue = goalPlanningVo.Goalcode;
+                txtGoalDescription.Text = goalPlanningVo.GoalDescription;
+                txtGoalDate.Text = goalPlanningVo.GoalProfileDate.ToShortDateString();
+                ddlPriority.SelectedValue = goalPlanningVo.Priority;
+                if (goalPlanningVo.IsOnetimeOccurence == true)
+                    ddlOccurrence.SelectedValue = "Once";
+                else
+                {
+                    ddlOccurrence.SelectedValue = "Recurring";
+                    trFrequency.Visible = true;
+                    ddlFrequency.SelectedValue = goalPlanningVo.Frequency.Trim();
+                }
+
+                ddlGoalYear.SelectedValue = goalPlanningVo.GoalYear.ToString();
+                txtGoalCostToday.Text = goalPlanningVo.CostOfGoalToday.ToString();
+                txtComment.Text = goalPlanningVo.Comments;
+                if (goalAction == "Edit")
+                {
+                    SetControlReadOnly(false);
+                }
+                else if (goalAction == "View")
+                {
+                    SetControlReadOnly(true);
+ 
+                }
+                
+              } 
+                
+            }
+            else if (goalCategory == "RT")
+            {
+                rtbRT.Checked = true;
+                rtbNonRT.Checked = false;
+                btnRTSave.Visible = false;
+                btnRTUpdate.Visible = true;
+                btnNonRTUpdate.Visible = false;
+ 
+            }
+
+            rtbRT.Enabled = false;
+            rtbNonRT.Enabled = false;
+
+ 
+        }
+
+        protected void SetControlReadOnly(bool readOnly)
+        {
+            if (readOnly)
+            {
+                ddlGoalType.Enabled = false;
+                txtGoalDescription.Enabled = false;
+                txtGoalDate.Enabled = false;
+                ddlPriority.Enabled = false;
+                ddlOccurrence.Enabled = false;
+                ddlFrequency.Enabled = false;
+                ddlGoalYear.Enabled = false;
+                txtGoalCostToday.Enabled = false;
+                txtComment.Enabled = false;
+                btnNonRTSave.Enabled = false;
+                btnRTSave.Enabled = false;
+
+                btnRTUpdate.Enabled = false;
+                btnNonRTUpdate.Enabled = false;
+
+                aplToolBar.Enabled = true;
+                
+
+            }
+            else
+            {
+                ddlGoalType.Enabled = false;
+                txtGoalDescription.Enabled = true;
+                txtGoalDate.ReadOnly = true;
+                ddlPriority.Enabled = true;
+                ddlOccurrence.Enabled = true;
+                ddlFrequency.Enabled = true;
+                ddlGoalYear.Enabled = true;
+                txtGoalCostToday.Enabled = true;
+                txtComment.Enabled = true;
+                btnNonRTSave.Enabled = true;
+                btnRTSave.Enabled = true;          
+
+                btnRTUpdate.Enabled = true;
+                btnNonRTUpdate.Enabled = true;
+
+                aplToolBar.Enabled = false;
+ 
+            }
+ 
+        }
+
+        protected void aplToolBar_ButtonClick(object sender, Telerik.Web.UI.RadToolBarEventArgs e)
+        {
+            if (e.Item.Value == "Edit")
+            {
+                SetControlReadOnly(false);
+
+            }
+        }
+
+        protected void btnNonRTUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                goalPlanningVo.GoalId = int.Parse(ViewState["GoalId"].ToString());
+                goalPlanningVo.CostOfGoalToday = double.Parse(txtGoalCostToday.Text);
+                goalPlanningVo.GoalDate = DateTime.Parse(txtGoalDate.Text);
+                goalPlanningVo.Priority = ddlPriority.SelectedValue.ToString();
+                if (ddlOccurrence.SelectedValue.ToString() == "Once")
+                {
+                    goalPlanningVo.IsOnetimeOccurence = true;
+                }
+                else if (ddlOccurrence.SelectedValue.ToString() == "Recurring")
+                {
+                    goalPlanningVo.Frequency = ddlFrequency.SelectedValue.ToString();
+                }
+                goalPlanningVo.GoalYear = int.Parse(ddlGoalYear.SelectedValue);
+                goalPlanningVo.GoalDescription = txtGoalDescription.Text.ToString();
+
+                if (ddlGoalType.SelectedValue == "ED" || ddlGoalType.SelectedValue == "MR")
+                {
+                    if (ddlPickChild.SelectedIndex != 0)
+                        goalPlanningVo.AssociateId = int.Parse(ddlPickChild.SelectedValue.ToString());
+                }
+                if (txtComment.Text != "")
+                {
+                    goalPlanningVo.Comments = txtComment.Text.ToString();
+
+                }
+                goalPlanningBo.CreateCustomerGoalPlanning(goalPlanningVo, customerVo.CustomerId, true);
+
+            }
+            catch (Exception ee)
+            {
+ 
+            }
+
+
+        }
+            
+        protected void ddlGoalType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlGoalType.SelectedValue == "ED" || ddlGoalType.SelectedValue == "MR")
+            {
+                trPickChild.Visible = true;
+            }
+            else
+            {
+                trPickChild.Visible = false;
+            }
+
+        }
+
+        protected void ddlOccurrence_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlOccurrence.SelectedValue == "Once")
+            {
+                trFrequency.Visible = false;
+            }
+            else
+            {
+                trFrequency.Visible = true;
+
+            }
+
+        }
+
+        protected void RTGoalPageLoadSetup(DataTable dtFPCalculationBasis)
+        {
+
+            string rtCalculationBasis = "";
+            DataRow[] drFPCalculationBasis;
+            int rtCalculationBasisId = 0;
+
+            drFPCalculationBasis = dtFPCalculationBasis.Select();
+
+            foreach (DataRow dr in drFPCalculationBasis)
+            {
+                if (dr["WFPC_CalculationId"].ToString() == "1")
+                {                    
+                    rtCalculationBasisId = int.Parse(dr["WFPCB_CalculationBasisId"].ToString());                   
+
+                }
+                else if (dr["WFPC_CalculationId"].ToString() == "2")
+                {
+                    rtCalculationBasis = dr["WFPCB_CalculationBasis"].ToString();
+                    if (dr["WFPCB_CalculationBasisId"].ToString()=="3")
+                      hidFPCalculationBasis.Value = "SELF";
+                    else
+                      hidFPCalculationBasis.Value = "WITH_SPOUSE";
+                }
+
+
+            }
+            lblRTGoalBasis.Text += rtCalculationBasis;
+            if (rtCalculationBasisId == 1)
+            {
+                trRTGoalCorpsToBeLeftBehind.Visible = false;
+                hidRTGoalCorpsLeftBehind.Value = "0";
+            }
+            else
+            {
+                trRTGoalCorpsToBeLeftBehind.Visible = true;
+                hidRTGoalCorpsLeftBehind.Value = "1";
+
+            }
+ 
+ 
+        }
+
+        protected void btnRTSave_Click(object sender, EventArgs e)
+        {
+            SessionBo.CheckSession();
+            goalPlanningVo.CustomerId = customerVo.CustomerId;
+            if (customerVo.Dob != DateTime.MinValue)
+            {
+                goalPlanningVo.CustomerAge = (DateTime.Now.Year - customerVo.Dob.Year);
+            }
+            goalPlanningVo.Goalcode = "RT";
+            goalPlanningVo.CostOfGoalToday = double.Parse(txtRTGoalCostToday.Text);
+            goalPlanningVo.GoalDate =DateTime.Parse(DateTime.Now.ToShortDateString());
+            if (!string.IsNullOrEmpty(hidRTGoalCorpsLeftBehind.Value.ToString()))
+            {
+                goalPlanningVo.CorpusToBeLeftBehind = true;
+                goalPlanningVo.CorpusLeftBehind =int.Parse(txtRTCorpusToBeLeftBehind.Text);
+                
+            }
+            goalPlanningBo.CreateCustomerGoalPlanning(goalPlanningVo, customerVo.CustomerId, false);
+
+        }
+        
     }
 }
