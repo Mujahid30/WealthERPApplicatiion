430736cdf1fe0cbfaa8cb262ed59cd41b393b9c5
AMP\vpatil <AMP\vpatil>
2011-08-07 19:00:28 +0530

    SIP MIS Bug fixes..

--- WealthERPSolution/Bo/CustomerPortfolio/SystematicSetupBo.cs
+++ WealthERPSolution/Bo/CustomerPortfolio/SystematicSetupBo.cs
@@ -223,12 +223,12 @@ namespace BoCustomerPortfolio
         /// <param name="dtFrom"></param>
         /// <param name="dtTo"></param>
         /// <returns></returns>
-        public DataSet GetAllSystematicMISData(string UserType, int AdviserId, int RmId, int CustomerId, int BranchHeadId, int BranchId, int All, string Category, string SysType, string AmcCode, string SchemePlanCode, string StartDate, string EndDate, DateTime dtFrom, DateTime dtTo)
+        public DataSet GetAllSystematicMISData(string UserType, int AdviserId, int RmId, int CustomerId, int BranchHeadId, int BranchId, int All, string Category, string SysType, string AmcCode, string SchemePlanCode, string StartDate, string EndDate, DateTime dtFrom, DateTime dtTo, int isIndividualOrGroup)
         {
             DataSet dsGetSystematicMIS = new DataSet();
             try
             {
-                dsGetSystematicMIS = systematicSetupDao.GetAllSystematicMISData(UserType, AdviserId, RmId, CustomerId, BranchHeadId, BranchId, All, Category, SysType, AmcCode, SchemePlanCode, StartDate, EndDate, dtFrom, dtTo);
+                dsGetSystematicMIS = systematicSetupDao.GetAllSystematicMISData(UserType, AdviserId, RmId, CustomerId, BranchHeadId, BranchId, All, Category, SysType, AmcCode, SchemePlanCode, StartDate, EndDate, dtFrom, dtTo, isIndividualOrGroup);
             }
             catch (BaseApplicationException Ex)
             {
