304cd67157e5c5a4f831c88d616b5c68dab204cf
AMP\vpatil <AMP\vpatil>
2011-09-21 18:00:19 +0530

    RE 4.1 --> Done With Ops Module....

--- WealthERPSolution/UI/WealthERP/Advisor/AdviserEnvironmentSettings.ascx
+++ WealthERPSolution/UI/WealthERP/Advisor/AdviserEnvironmentSettings.ascx
@@ -1,1 +1,125 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AdviserEnvironmentSettings.ascx.cs" Inherits="WealthERP.Advisor.AdviserEnvironmentSettings" %>
+
+<script type="text/javascript">
+
+    function alertingForOpsEnabeling() {
+
+        var chkCheckedOrNot = document.getElementById("<%=chkIsOpsEnable.ClientID%>").checked;
+        if (chkCheckedOrNot == true) {
+            var bool = window.confirm('By Enabling Ops Role All Operational Permissions will be \n Removed From Admin Login & Given to Ops Login.\n\n Click OK to Add a Staff with Ops Role If you have not added staff  \n who is having Ops role.\n\n Click Cancel If you have already added a staff with Ops role \n and to proceed further...\n');
+            if (bool) {
+                document.getElementById("ctrl_AdviserEnvironmentSettings_hdnEnableOpsMsg").value = 1;
+                document.getElementById("ctrl_AdviserEnvironmentSettings_hiddenbtnOpsEnable").click();
+
+                return false;
+            }
+            else {
+                document.getElementById("ctrl_AdviserEnvironmentSettings_hdnEnableOpsMsg").value = 0;
+                return true;
+            }
+        }
+        else {
+
+            var bool = window.confirm('By Disabling Ops Role Your Ops Staff will be Inactive.');
+            if (bool) {
+                document.getElementById("ctrl_AdviserEnvironmentSettings_hdnDisableOpsMsg").value = 1;
+                document.getElementById("ctrl_AdviserEnvironmentSettings_hiddenbtnOpsDisable").click();
+
+                return false;
+            }
+            else {
+                document.getElementById("ctrl_AdviserEnvironmentSettings_hdnDisableOpsMsg").value = 0;
+                return true;
+            }
+        
+        
+        }
+    }
+
+    function alertingForAddDefaultIP() {
+        var chkCheckedOrNot = document.getElementById("<%=chkIsIPEnable.ClientID%>").checked;
+        if (chkCheckedOrNot == true) {
+            alert("By Enabling this your IP Login Security has been activated.!! \n\n You current system IP has been added to your IP list..");
+        }
+        else {
+            alert("By Disabling this you will loose your IP Login Security..!!");
+        }
+    }
+
+</script>
+
+<table width="100%" class="TableBackground">
+    <tr>
+        <td class="HeaderTextBig" colspan="2">
+            <asp:Label ID="lblHeader" runat="server" CssClass="HeaderTextBig" Text="Advisor Environment Settings"></asp:Label>
+            <hr />
+        </td>
+    </tr>
+    
+    </table>
+    
+    <table>
+    <tr>
+        <td colspan="2">
+            <asp:Label ID="lblNoteIP" runat="server" CssClass="txtField" Text="Note: Please check if you have static IP. Feature is useful with static IP only.">
+            </asp:Label>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            &nbsp;
+        </td>
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    
+    <tr>
+        <td class="leftField">
+            <asp:Label ID="lblisIPEnable" runat="server" CssClass="FieldName" Text="IP Enable :"></asp:Label>
+        </td>
+        <td>
+            <asp:CheckBox ID="chkIsIPEnable" onclick="alertingForAddDefaultIP()" style="vertical-align: middle" CssClass="FieldName" runat="server" Text="Is IP Enable ?" AutoPostBack="false" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td class="leftField">
+            <asp:Label ID="lblisOpsEnable" runat="server" CssClass="FieldName" Text="Ops Enable :"></asp:Label>
+        </td>
+        <td>
+                <asp:CheckBox ID="chkIsOpsEnable" runat="server" onclick="return alertingForOpsEnabeling()" style="vertical-align: middle" CssClass="FieldName" Text="Is Ops Enable ?" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>
+            &nbsp;
+        </td>
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    
+     
+    
+    
+    <tr>
+        <td>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" CssClass="PCGButton" onmouseover="javascript:ChangeButtonCss('hover', 'ctrl_EditAdvisorProfile_btnSubmit', 'S');"
+                
+                onmouseout="javascript:ChangeButtonCss('out', 'ctrl_EditAdvisorProfile_btnSubmit', 'S');" 
+                onclick="btnSubmit_Click" />
+        </td>
+    </tr>
+    
+</table>
+
+<asp:Button ID="hiddenbtnOpsEnable" runat="server" 
+    BorderStyle="None" BackColor="Transparent" onclick="hiddenbtnOpsEnable_Click" />
+    
+<%--<asp:Button ID="hiddenbtnOpsDisable" runat="server" BorderStyle="None" BackColor="Transparent"
+    onclick="hiddenbtnOpsDisable_Click" /> --%>   
+    
+<asp:HiddenField ID="hdnEnableOpsMsg" runat="server" />
+<asp:HiddenField ID="hdnDisableOpsMsg" runat="server" />
--- WealthERPSolution/UI/WealthERP/Advisor/AdviserEnvironmentSettings.ascx.cs
+++ WealthERPSolution/UI/WealthERP/Advisor/AdviserEnvironmentSettings.ascx.cs
@@ -4,14 +4,188 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VoUser;
+using BoCommon;
+using System.Configuration;
+using BoAdvisorProfiling;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using System.Data;
 
 namespace WealthERP.Advisor
 {
     public partial class AdviserEnvironmentSettings : System.Web.UI.UserControl
     {
+        UserVo userVo = new UserVo();
+        AdvisorBo advisorBo = new AdvisorBo();
+        AdvisorVo advisorVo = new AdvisorVo();
+        AdviserIPVo adviserIPVo = new AdviserIPVo();
+        DataSet dsGetAllAdviserIPFromIPPool = new DataSet();
+        string PreviousPage = string.Empty;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            string FromPageToCheckOps = string.Empty;
+            try
+            {
+                SessionBo.CheckSession();
+                string path = Server.MapPath(ConfigurationManager.AppSettings["xmllookuppath"]).ToString();
 
+                userVo = (UserVo)Session["UserVo"];
+                if (Session["advisorVo"] != null)
+                {
+                    advisorVo = (AdvisorVo)Session["advisorVo"];
+                }
+                else
+                {
+                    advisorVo = advisorBo.GetAdvisorUser(userVo.UserId);
+                }
+
+                if (!IsPostBack)
+                {
+                    if (Request.QueryString["PreviousPageName"] != null)
+                        FromPageToCheckOps = Request.QueryString["PreviousPageName"].ToString();
+                    // To Check Whether Adviser already Enabled IP Security or not..
+                    if (advisorVo.IsIPEnable == 1)
+                        chkIsIPEnable.Checked = true;
+                    else
+                        chkIsIPEnable.Checked = false;
+
+                    // To Check Whether Adviser already Enabled Ops Login or not..
+                    if ((advisorVo.IsOpsEnable == 1) || (FromPageToCheckOps != string.Empty))
+                        chkIsOpsEnable.Checked = true;
+                    else
+                        chkIsOpsEnable.Checked = false;
+                }
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "EditAdvisorProfile.ascx:Page_Load()");
+
+                object[] objects = new object[4];
+
+                objects[0] = advisorBo;
+                objects[1] = advisorVo;
+                objects[2] = userVo;
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+
+            }
+        }
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string IPAddress = string.Empty;
+            int createdById = 0;
+            bool RecordStatus = false;
+
+            advisorVo.UserId = userVo.UserId;
+
+            // To Check IP CheckBox Checked or not..
+            if (chkIsIPEnable.Checked == true)
+                advisorVo.IsIPEnable = 1;
+            else
+                advisorVo.IsIPEnable = 0;
+
+            
+
+            // To Check Ops CheckBox Checked or not..
+            if (chkIsOpsEnable.Checked == true)
+                advisorVo.IsOpsEnable = 1;
+            else
+                advisorVo.IsOpsEnable = 0;
+
+
+
+            advisorBo.UpdateAdvisorUser(advisorVo);
+
+            Session["advisorVo"] = (AdvisorVo)advisorVo;
+
+            if (advisorVo.IsIPEnable == 1)
+            {
+                int i = 0;
+                IPAddress = HttpContext.Current.Request.UserHostAddress.ToString();
+                dsGetAllAdviserIPFromIPPool = advisorBo.GetAdviserIPPoolsInformation(advisorVo.advisorId);
+                if (dsGetAllAdviserIPFromIPPool.Tables.Count != 0)
+                {
+                    foreach (DataRow dr in dsGetAllAdviserIPFromIPPool.Tables[0].Rows)
+                    {
+                        if (IPAddress == dr["AIPP_IP"].ToString())
+                        {
+                            i = 1;
+                        }
+                    }
+                }
+                if (i != 1)
+                {
+                    if (IPAddress != "")
+                        adviserIPVo.AdviserIPs = IPAddress;
+                    adviserIPVo.AdviserIPComments = "Adviser's Default IP";
+                    adviserIPVo.advisorId = advisorVo.advisorId;
+                    createdById = userVo.UserId;
+
+                    RecordStatus = advisorBo.CreateAdviserIPPools(adviserIPVo, createdById);
+                }
+            }
+
+            Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loadcontrol('AdviserEnvironmentSettings','none');", true);
+            Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "adviserpaneleftttttt", "loadlinks('AdvisorLeftPane','login');", true);
+        }
+
+        protected void hiddenbtnOpsEnable_Click(object sender, EventArgs e)
+        {
+            //int rmId = 0;
+            //DataSet dsGetAllOpsStaffs = new DataSet();
+            //dsGetAllOpsStaffs = advisorBo.GetAllOpsStaffsForAdviser(advisorVo.advisorId, "Ops");
+
+            //if (dsGetAllOpsStaffs.Tables.Count > 0)
+            //{
+            //    if (dsGetAllOpsStaffs.Tables[0].Rows.Count > 0)
+            //    {
+            //        foreach (DataRow dr in dsGetAllOpsStaffs.Tables[0].Rows)
+            //        {
+            //            rmId = int.Parse(dr["AR_RMId"].ToString());
+            //            advisorBo.UpdateOpsStaffLoginStatus(rmId, 1);
+            //        }
+            //    }
+            //}
+
+            if (hdnEnableOpsMsg.Value == "1")
+            {
+                PreviousPage = "EnvironmentalSettings";
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loadcontrol('AddRM','PreviousPageName=" + PreviousPage + "');", true);
+
+            }
+        }
+
+        protected void hiddenbtnOpsDisable_Click(object sender, EventArgs e)
+        {
+            //int rmId = 0;
+            //DataSet dsGetAllOpsStaffs = new DataSet();
+            //dsGetAllOpsStaffs = advisorBo.GetAllOpsStaffsForAdviser(advisorVo.advisorId, "Ops");
+
+            //if (dsGetAllOpsStaffs.Tables.Count > 0)
+            //{
+            //    if (dsGetAllOpsStaffs.Tables[0].Rows.Count > 0)
+            //    {
+            //        foreach (DataRow dr in dsGetAllOpsStaffs.Tables[0].Rows)
+            //        {
+            //            rmId = int.Parse(dr["AR_RMId"].ToString());
+            //            advisorBo.UpdateOpsStaffLoginStatus(rmId, 0);
+            //        }
+            //    }
+            //}
         }
     }
 }
