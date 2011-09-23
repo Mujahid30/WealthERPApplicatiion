a57095c3eea8ebe4e53cc7126e8310197f4479db
AMP\bhoopendra <AMP\bhoopendra>
2011-09-23 13:07:51 +0530

    RE(4.0) --> Function changed for CS ISSUE 264 & 265.

--- WealthERPSolution/Dao/Uploads/RejectedTransactionsDao.cs
+++ WealthERPSolution/Dao/Uploads/RejectedTransactionsDao.cs
@@ -91,48 +91,48 @@ namespace DaoUploads
                 return false;
         }
 
-        //public bool MapRejectedFoliosToCustomer(int mfFolioStagingId, int customerId, int userId)
-        //{
-        //    Database db;
-        //    DbCommand cmd;
-        //    int affectedMFRecords = 0;
-        //    try
-        //    {
-        //        db = DatabaseFactory.CreateDatabase("wealtherp");
-        //        cmd = db.GetStoredProcCommand("SP_MapRejectedMFFolioToCustomer");
-        //        db.AddInParameter(cmd, "@MFFolioStagingId", DbType.Int32, mfFolioStagingId);
-        //        db.AddInParameter(cmd, "@CustomerId", DbType.Int32, customerId);
-        //        db.AddInParameter(cmd, "@CurrentUser", DbType.Int32, userId);
-        //        affectedMFRecords = db.ExecuteNonQuery(cmd);
+        public bool MapRejectedFoliosToCustomer(int mfFolioStagingId, int customerId, int userId)
+        {
+            Database db;
+            DbCommand cmd;
+            int affectedMFRecords = 0;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                cmd = db.GetStoredProcCommand("SP_MapRejectedMFFolioToCustomer");
+                db.AddInParameter(cmd, "@MFFolioStagingId", DbType.Int32, mfFolioStagingId);
+                db.AddInParameter(cmd, "@CustomerId", DbType.Int32, customerId);
+                db.AddInParameter(cmd, "@CurrentUser", DbType.Int32, userId);
+                affectedMFRecords = db.ExecuteNonQuery(cmd);
 
-        //    }
-        //    catch (BaseApplicationException Ex)
-        //    {
-        //        throw Ex;
-        //    }
-        //    catch (Exception Ex)
-        //    {
-        //        BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
-        //        NameValueCollection FunctionInfo = new NameValueCollection();
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
 
-        //        FunctionInfo.Add("Method", "RejectedTransactionsDao.cs:GetRejectedRecord()");
+                FunctionInfo.Add("Method", "RejectedTransactionsDao.cs:GetRejectedRecord()");
 
-        //        object[] objects = new object[3];
-        //        objects[0] = mfFolioStagingId;
-        //        objects[1] = customerId;
-        //        objects[2] = userId;
+                object[] objects = new object[3];
+                objects[0] = mfFolioStagingId;
+                objects[1] = customerId;
+                objects[2] = userId;
 
 
-        //        FunctionInfo = exBase.AddObject(FunctionInfo, objects);
-        //        exBase.AdditionalInformation = FunctionInfo;
-        //        ExceptionManager.Publish(exBase);
-        //        throw exBase;
-        //    }
-        //    if (affectedMFRecords > 0)
-        //        return true;
-        //    else
-        //        return false;
-        //}
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            if (affectedMFRecords > 0)
+                return true;
+            else
+                return false;
+        }
 
         public bool MapEquityToCustomer(int transactionStagingId, int customerId, int userId)
         {
