8db188d3afdef31b7ad82510cf18faa45d8a0a5e
AMP\bhoopendra <AMP\bhoopendra>
2011-12-28 18:41:15 +0530

    RE5.0: Enhancement done for Risk Score Calculation.

--- WealthERPSolution/UI/WealthERP/Research/RiskScore.ascx
+++ WealthERPSolution/UI/WealthERP/Research/RiskScore.ascx
@@ -1,1 +1,136 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="RiskScore.ascx.cs" Inherits="WealthERP.Research.RiskScore" %>
+
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
+<%@ Register TagPrefix="qsf" Namespace="Telerik" %>
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Charting" Assembly="Telerik.Web.UI" %>
+<script src="../Scripts/jquery.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+    function Validate() {
+        var assumptionValue = document.getElementById('ctrl_CustomerAssumptionsPreferencesSetup_RadGrid1_ctl00__2').value;
+        return true;
+    }
+
+</script>
+<telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+</telerik:RadScriptManager> 
+<br /> 
+    
+<telerik:RadTabStrip ID="RadTabStrip1" runat="server" EnableTheming="True" Skin="Telerik"
+    EnableEmbeddedSkins="False" MultiPageID="RiskScoreCalculationId" 
+    SelectedIndex="0">
+    <Tabs>
+        <telerik:RadTab runat="server" Text="Risk Score" onclick="HideStatusMsg()"
+            Value="RiskScore" TabIndex="0" Selected="True">
+        </telerik:RadTab>
+        <telerik:RadTab runat="server" Text="Risk Questions and Scores" onclick="HideStatusMsg()"
+            Value="RiskQuestionsandScores" TabIndex="1">
+        </telerik:RadTab>        
+    </Tabs>
+</telerik:RadTabStrip>
+
+<telerik:RadMultiPage ID="RiskScoreId" EnableViewState="true" runat="server" SelectedIndex="0">
+<telerik:RadPageView ID="RadPageView1" runat="server">
+<asp:Panel ID="pnlRiskScore" runat="server">
+<br />
+<table style="width:75%">
+    <tr>
+        <td>
+        </td>
+        <td>
+            <telerik:RadGrid ID="RadGrid1" runat="server" CssClass="RadGrid" GridLines="None"
+            AllowPaging="True" PageSize="20" AllowSorting="True" AutoGenerateColumns="False" Skin="Telerik"
+            ShowStatusBar="true" AllowAutomaticDeletes="false" AllowAutomaticInserts="False"  OnItemDataBound="RadGrid1_ItemDataBound"
+        OnInsertCommand="RadGrid1_InsertCommand" OnUpdateCommand="RadGrid1_UpdateCommand" OnDeleteCommand="RadGrid1_DeleteCommand"
+         OnItemCommand="RadGrid1_ItemCommand" OnPreRender="RadGrid1_PreRender" AllowAutomaticUpdates="False" HorizontalAlign="NotSet" DataKeyNames="XRC_RiskClassCode">            
+            <MasterTableView CommandItemDisplay="Top" EditMode="PopUp" DataKeyNames="XRC_RiskClassCode">
+                <Columns>
+                    <telerik:GridEditCommandColumn>
+                    </telerik:GridEditCommandColumn>
+                    <telerik:GridBoundColumn UniqueName="XRC_RiskClass" HeaderText="Risk Class" DataField="XRC_RiskClass" >
+                    </telerik:GridBoundColumn>                  
+                    <telerik:GridBoundColumn UniqueName="WRPR_RiskScoreLowerLimit" HeaderText="Lower Limit" DataField="WRPR_RiskScoreLowerLimit" >
+                    </telerik:GridBoundColumn>
+                     <telerik:GridBoundColumn UniqueName="WRPR_RiskScoreUpperLimit" HeaderText="Upper Limit" DataField="WRPR_RiskScoreUpperLimit" >
+                    </telerik:GridBoundColumn>
+                    <%--<telerik:GridBoundColumn UniqueName="CPA_Value" HeaderText="Value" DataField="CPA_Value" >
+                    </telerik:GridBoundColumn>--%>
+                     <telerik:GridButtonColumn CommandName="Delete" Text="Delete" UniqueName="column">
+                    </telerik:GridButtonColumn>
+                </Columns>
+                <EditFormSettings  InsertCaption="Add new item" FormTableStyle-HorizontalAlign="Center" 
+                PopUpSettings-Modal="true" PopUpSettings-ZIndex="60" CaptionFormatString="Edit Risk ClassCode: {0}"
+                EditFormType="Template">
+                <%--CaptionDataField="CPA_Value"--%>
+                    <FormTemplate>
+                        <table id="Table1" cellspacing="1" cellpadding="1" width="250" border="0">                        
+                        <tr>
+                            <td>
+                                <asp:Label ID="lblPickClass" runat="server" Text="Pick a Risk class :" CssClass="FieldName"></asp:Label>
+                            </td>
+                            <td>
+                               <asp:DropDownList ID="ddlPickRiskClass" runat="server">                                    
+                                </asp:DropDownList>
+                            </td>
+                        </tr>
+                         <tr>
+                            <td>
+                                <asp:Label ID="lblLowerrLimit" runat="server" Text="Lowerr Limit :" CssClass="FieldName"></asp:Label>
+                            </td>
+                            <td>
+                                <asp:TextBox ID="txtLowerrLimit" Text='<%# Bind( "WRPR_RiskScoreLowerLimit") %>' runat="server">
+                                </asp:TextBox>
+                            </td>
+                        </tr> 
+                        <tr>
+                            <td>
+                                <asp:Label ID="lblUpperLimit" runat="server" Text="Upper Limit :" CssClass="FieldName"></asp:Label>
+                            </td>
+                            <td>
+                                <asp:TextBox ID="txtUpperLimit" Text='<%# Bind( "WRPR_RiskScoreUpperLimit") %>' runat="server">
+                                </asp:TextBox>
+                            </td>
+                        </tr>                                           
+                    </table>
+                        <table style="width: 80%">
+                            <tr>
+                                <td align="right" colspan="2">
+                                    <asp:Button ID="Button1" Text='<%# (Container is GridEditFormInsertItem) ? "Insert" : "Update" %>'
+                                        runat="server" CssClass="PCGButton" OnClientClick="return Validate()" ValidationGroup="vgbtnSubmit" CommandName='<%# (Container is GridEditFormInsertItem) ? "PerformInsert" : "Update" %>'>
+                                    </asp:Button>&nbsp;
+                                    <asp:Button ID="Button2" CssClass="PCGButton" Text="Cancel" runat="server" CausesValidation="False" CommandName="Cancel">
+                                    </asp:Button>
+                                </td>
+                            </tr>
+                        </table>
+                    </FormTemplate>
+                </EditFormSettings>
+            </MasterTableView>
+            <ClientSettings>
+                <%--<ClientEvents OnRowDblClick="RowDblClick" />--%>
+            </ClientSettings>
+          
+                    
+                </telerik:RadGrid>  
+        </td>
+    </tr>
+</table>   
+</asp:Panel>
+</telerik:RadPageView>
+    
+ 
+<telerik:RadPageView ID="RadPageView2" runat="server">
+<asp:Panel ID="Panel2" runat="server">
+<br/>
+<table style="width:75%">
+    <tr>
+        <td>
+        </td>
+        
+    </tr>
+</table>
+
+</asp:Panel>
+</telerik:RadPageView>         
+</telerik:RadMultiPage>
--- WealthERPSolution/UI/WealthERP/Research/RiskScore.ascx.cs
+++ WealthERPSolution/UI/WealthERP/Research/RiskScore.ascx.cs
@@ -4,14 +4,164 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VoUser;
+using BoResearch;
+using System.Data;
+using Telerik.Web.UI;
+using BoCustomerRiskProfiling;
 
 namespace WealthERP.Research
 {
     public partial class RiskScore : System.Web.UI.UserControl
     {
+        DataSet dsGlobal = new DataSet();
+        AdvisorVo adviserVo = new AdvisorVo();
+        AdviserFPConfigurationBo adviserFPConfigurationBo = new AdviserFPConfigurationBo();
+        RiskProfileBo riskprofilebo = new RiskProfileBo();
+        ModelPortfolioBo modelPortfolioBo = new ModelPortfolioBo();
+        int expiryAge = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
+            adviserVo = (AdvisorVo)Session["AdvisorVo"];
+            BindAdviserAssumptions();
+        }
 
+        public void BindAdviserAssumptions()
+        {
+            //DataSet dsRiskScore;
+            DataTable dt = new DataTable();
+            dt = adviserFPConfigurationBo.GetAdviserRiskScore(adviserVo.advisorId);
+            DataTable dtRiskScore = new DataTable();
+            dtRiskScore.Columns.Add("XRC_RiskClass");
+            dtRiskScore.Columns.Add("WRPR_RiskScoreLowerLimit");
+            dtRiskScore.Columns.Add("WRPR_RiskScoreUpperLimit");
+            dtRiskScore.Columns.Add("XRC_RiskClassCode");
+            DataRow drRiskScore;
+            foreach (DataRow dr in dt.Rows)
+            {
+                drRiskScore = dtRiskScore.NewRow();
+                drRiskScore["XRC_RiskClassCode"] = dr["XRC_RiskClassCode"].ToString();
+                drRiskScore["XRC_RiskClass"] = dr["XRC_RiskClass"].ToString();
+                drRiskScore["WRPR_RiskScoreLowerLimit"] = dr["WRPR_RiskScoreLowerLimit"].ToString();
+                drRiskScore["WRPR_RiskScoreUpperLimit"] = dr["WRPR_RiskScoreUpperLimit"].ToString();
+                dtRiskScore.Rows.Add(drRiskScore);
+            }
+            RadGrid1.DataSource = dtRiskScore;
+            RadGrid1.DataBind();
+        }
+
+        protected void RadGrid1_UpdateCommand(object source, GridCommandEventArgs e)
+        {
+            try
+            {
+                decimal lowerLimit = 0;
+                decimal upperLimit = 0;
+                GridEditableItem gridEditableItem = (GridEditableItem)e.Item;
+                DropDownList ddl = (DropDownList)e.Item.FindControl("ddlPickRiskClass");
+                TextBox txtLower = (TextBox)e.Item.FindControl("txtLowerrLimit");
+                TextBox txtUpper = (TextBox)e.Item.FindControl("txtUpperLimit");
+                lowerLimit = Convert.ToDecimal(txtLower.Text);
+                upperLimit = Convert.ToDecimal(txtUpper.Text);
+                adviserFPConfigurationBo.InsertUpdateRiskClassScore(ddl.SelectedValue, lowerLimit, upperLimit, adviserVo.advisorId, adviserVo.UserId);
+                RadGrid1.Rebind();
+            }
+            catch (Exception ex)
+            {
+                RadGrid1.Controls.Add(new LiteralControl("Unable to update Employee. Reason: " + ex.Message));
+                e.Canceled = true;
+            } 
+        }
+
+        protected void RadGrid1_InsertCommand(object source, GridCommandEventArgs e)
+        {
+            try
+            {
+                decimal lowerLimit = 0;
+                decimal upperLimit = 0;                
+                GridEditableItem gridEditableItem = (GridEditableItem)e.Item;
+                DropDownList ddl = (DropDownList)e.Item.FindControl("ddlPickRiskClass");
+                TextBox txtLower = (TextBox)e.Item.FindControl("txtLowerrLimit");
+                TextBox txtUpper = (TextBox)e.Item.FindControl("txtUpperLimit");
+                lowerLimit = Convert.ToDecimal(txtLower.Text);
+                upperLimit = Convert.ToDecimal(txtUpper.Text);
+                adviserFPConfigurationBo.InsertUpdateRiskClassScore(ddl.SelectedValue, lowerLimit, upperLimit, adviserVo.advisorId, adviserVo.UserId);
+                RadGrid1.Rebind();
+            }
+            catch (Exception ex)
+            {
+                RadGrid1.Controls.Add(new LiteralControl("Unable to insert Employee. Reason: " + ex.Message));
+                e.Canceled = true;
+            } 
+        }
+
+        protected void RadGrid1_DeleteCommand(object source, GridCommandEventArgs e)
+        {            
+            try
+            {              
+                //GridEditableItem gridEditableItem = (GridEditableItem)e.Item;
+                string riskClassCode = RadGrid1.MasterTableView.DataKeyValues[e.Item.ItemIndex]["XRC_RiskClassCode"].ToString();
+                adviserFPConfigurationBo.DeleteRiskClassScore(riskClassCode, adviserVo.advisorId);
+                BindAdviserAssumptions();
+                RadGrid1.Rebind();
+            }
+            catch (Exception ex)
+            {
+                RadGrid1.Controls.Add(new LiteralControl("Unable to delete Employee. Reason: " + ex.Message));
+                e.Canceled = true;
+            } 
+        }
+
+        protected void RadGrid1_ItemCommand(object source, GridCommandEventArgs e)
+        {
+            //if (e.CommandName == RadGrid.InitInsertCommandName)
+            //{
+            //    e.Canceled = true;
+            //    RadGrid1.EditIndexes.Clear();
+            //    GridEditableItem item = (GridEditableItem)e.Item;
+            //    DropDownList ddl = (DropDownList)e.Item.FindControl("ddlPickRiskClass");
+            //    ddl.Enabled = false;
+            //    e.Item.OwnerTableView.EditFormSettings.UserControlName = "ddlPickRiskClass";
+            //    e.Item.OwnerTableView.InsertItem();
+            //}
+            //else if (e.CommandName == RadGrid.EditCommandName)
+            //{
+            //    DropDownList ddl = (DropDownList)e.Item.FindControl("ddlPickRiskClass");
+            //    ddl.Enabled = false;
+            //    e.Item.OwnerTableView.IsItemInserted = false;
+            //    e.Item.OwnerTableView.EditFormSettings.UserControlName = "ddlPickRiskClass";
+            //}
+        }
+
+        protected void RadGrid1_ItemCreated(object sender, GridItemEventArgs e)
+        {
+            if (e.Item is GridCommandItem)
+            {
+                e.Item.FindControl("InitInsertButton").Parent.Visible = false;
+            }
+        }
+        protected void RadGrid1_PreRender(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadGrid1_ItemDataBound(object sender, GridItemEventArgs e)
+        {
+            if ((e.Item is GridEditFormItem) && (e.Item.IsInEditMode))
+            {
+                GridEditFormItem editform = (GridEditFormItem)e.Item;
+                DropDownList ddl = (DropDownList)editform.FindControl("ddlPickRiskClass");
+                int adviserId = adviserVo.advisorId;
+
+                dsGlobal = riskprofilebo.GetAdviserRiskClasses(adviserId);
+                if (dsGlobal.Tables[0].Rows.Count > 0)
+                {
+                    ddl.DataSource = dsGlobal.Tables[0];
+                    ddl.DataValueField = dsGlobal.Tables[0].Columns["XRC_RiskClassCode"].ToString();
+                    ddl.DataTextField = dsGlobal.Tables[0].Columns["XRC_RiskClass"].ToString();
+                    ddl.DataBind();
+                }
+                ddl.Items.Insert(0, new System.Web.UI.WebControls.ListItem("Select Risk Class", "Select Risk Class"));
+            }
         }
     }
 }
