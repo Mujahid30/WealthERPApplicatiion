2c7b7e881a84188210337005cd99123432382ffc
AMP\Kirteeshree <AMP\Kirteeshree>
2012-01-03 15:21:40 +0530

    RE: 5.0--> export to doc option added

--- WealthERPSolution/UI/WealthERP/Reports/FPOfflineForm.ascx.cs
+++ WealthERPSolution/UI/WealthERP/Reports/FPOfflineForm.ascx.cs
@@ -8,11 +8,43 @@ using BoCommon;
 
 namespace WealthERP.Reports
 {
-    public partial class FPOfflineForm1 : System.Web.UI.UserControl
+    public partial class FPOfflineForm : System.Web.UI.UserControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
             SessionBo.CheckSession();
+            //if (!IsPostBack)
+            //{
+            //    btnViewReport.Visible = true;
+            //    btnViewInPDF.Visible = false;
+            //    btnViewInDOC.Visible = false;
+            //}
+           
         }
+        //protected void ddlBtnSelect_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    if (ddlBtnSelect.SelectedIndex != -1)
+        //    {
+        //        if (ddlBtnSelect.SelectedValue == "ViewReport")
+        //        {
+        //            btnViewReport.Visible = true;
+        //            btnViewInPDF.Visible = false;
+        //            btnViewInDOC.Visible = false;
+        //        }
+        //        if (ddlBtnSelect.SelectedValue == "ViewInPdf")
+        //        {
+        //            btnViewReport.Visible = false;
+        //            btnViewInPDF.Visible = true;
+        //            btnViewInDOC.Visible = false;
+        //        }
+        //        if (ddlBtnSelect.SelectedValue == "ViewInDoc")
+        //        {
+        //            btnViewReport.Visible = false;
+        //            btnViewInPDF.Visible = false;
+        //            btnViewInDOC.Visible = true;
+        //        }
+        //    }
+        //}
     }
+
 }
