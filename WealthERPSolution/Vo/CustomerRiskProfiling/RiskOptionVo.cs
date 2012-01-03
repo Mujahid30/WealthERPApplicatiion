ba0d9afd24bae87d757227514e382c7759dab9f0
AMP\vpatil <AMP\vpatil>
2012-01-03 18:00:29 +0530

    Re: 5.0 --> Dynamic Adviser Risk Profile...

--- WealthERPSolution/Vo/CustomerRiskProfiling/RiskOptionVo.cs
+++ WealthERPSolution/Vo/CustomerRiskProfiling/RiskOptionVo.cs
@@ -25,4 +25,66 @@ namespace VoCustomerRiskProfiling
             
 
     }
+
+    /// <summary>
+    /// Adviser dynamic risk profile Questions and options Encapsulation vo..
+    /// Added by Vinayak Patil..
+    /// </summary>
+    
+    public class AdviserDynamicRiskQuestionsVo
+    {
+        #region Fields
+        // Questions Vo 
+        private int m_QuestionId;
+        private string m_Question;
+        private string m_Purpose;
+        private int m_AdviserId;
+
+        // Option Vo
+        private int m_OptionId;
+        private string m_Option;
+        private int m_Weightage;
+
+        #endregion Fields
+
+
+        #region Properties
+        public int QuestionId
+        {
+            get { return m_QuestionId; }
+            set { m_QuestionId = value; }
+        }
+        public int AdviserId
+        {
+            get { return m_AdviserId; }
+            set { m_AdviserId = value; }
+        }
+        public int OptionId
+        {
+            get { return m_OptionId; }
+            set { m_OptionId = value; }
+        }
+        public int Weightage
+        {
+            get { return m_Weightage; }
+            set { m_Weightage = value; }
+        }
+
+        public string Question
+        {
+            get { return m_Question; }
+            set { m_Question = value; }
+        }
+        public string Purpose
+        {
+            get { return m_Purpose; }
+            set { m_Purpose = value; }
+        }
+        public string Option
+        {
+            get { return m_Option; }
+            set { m_Option = value; }
+        }
+        #endregion Properties
+    }
 }
