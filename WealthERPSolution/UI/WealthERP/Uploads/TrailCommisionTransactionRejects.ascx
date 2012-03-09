a665abc0d9f1273202e8d05fddf7612c57ede206
AMP\Administrator:4 <AMP\Administrator:4>
2012-03-09 13:40:25 +0530

    RE 6.0 : Trail Uploads

--- WealthERPSolution/UI/WealthERP/Uploads/TrailCommisionTransactionRejects.ascx
+++ WealthERPSolution/UI/WealthERP/Uploads/TrailCommisionTransactionRejects.ascx
@@ -1,1 +1,145 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="TrailCommisionTransactionRejects.ascx.cs" Inherits="WealthERP.Uploads.TrailCommisionTransactionRejects" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" %>
+
+<telerik:RadStyleSheetManager ID="RadStyleSheetManager1" runat="server" />
+    
+<telerik:RadScriptManager ID="RadScriptManager1" runat="server">  
+</telerik:RadScriptManager>
+
+
+<table width="100%">
+    <tr>
+        <td align="center">
+            <div id="msgReprocessComplete" runat="server" class="success-msg" align="center"
+                visible="false">
+                Reprocess successfully Completed
+            </div>
+        </td>
+    </tr>
+</table>
+ <table width="100%">
+    <tr>
+        <td align="center">
+            <div id="msgDelete" runat="server" class="success-msg" align="center"
+                visible="false">
+                Records has been deleted successfully 
+            </div>
+        </td>
+    </tr>
+</table>
+<table width="100%">
+    <tr>
+        <td align="center">
+            <div id="msgReprocessincomplete" runat="server" class="failure-msg" align="center"
+                visible="false">
+                Reprocess Failed!
+            </div>
+        </td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <td>
+                <telerik:RadGrid ID="GVTrailTransactionRejects" runat="server" GridLines="None" AutoGenerateColumns="False"
+                    PageSize="10" AllowSorting="true" AllowPaging="True" ShowStatusBar="True" ShowFooter="true"
+                    Skin="Telerik" EnableEmbeddedSkins="false" Width="120%" AllowFilteringByColumn="true" 
+                    AllowAutomaticInserts="false" OnNeedDataSource="GVTrailTransactionRejects_OnNeedDataSource">
+                    <ExportSettings ExportOnlyData="true"></ExportSettings>
+                    <MasterTableView DataKeyNames="CMFTCCS_Id,A_AdviserId,Adul_ProcessId" Width="100%" AllowMultiColumnSorting="True" AutoGenerateColumns="false" CommandItemDisplay="Top">
+                     <CommandItemSettings ShowExportToWordButton="true" ShowExportToExcelButton="true"
+                    ShowExportToCsvButton="true" ShowAddNewRecordButton="false" ShowRefreshButton="true"/>
+                      <Columns>  
+                            <telerik:GridTemplateColumn AllowFiltering="false">
+                           
+                           <HeaderTemplate>
+                                     <asp:CheckBox runat="server" ID="ChkALL" AutoPostBack="True" OnCheckedChanged="ToggleSelectedState" />
+                           </HeaderTemplate>
+                           <ItemTemplate>
+                                     <asp:CheckBox runat="server" ID="ChkOne" AutoPostBack="True" OnCheckedChanged="ToggleRowSelection"/>
+                                   </ItemTemplate>
+                                </telerik:GridTemplateColumn>
+                                                      
+                        
+                         
+                        <telerik:GridBoundColumn DataField="WRR_RejectReasonDescription" AllowFiltering="false"  HeaderText="Reject Reason" UniqueName="RejectReason" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn DataField="Adul_ProcessId" AllowFiltering="false"  HeaderText="Process Id" UniqueName="ProcessId" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" Datafield="ADUL_FileName"  HeaderText="File Name" UniqueName="FileName" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="CMFA_FolioNum"  HeaderText="Folio Number" UniqueName="FolioNumber" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="PASP_SchemePlanName"  HeaderText="Scheme Name" UniqueName="SchemeName" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="CMFTTCS_FROMDate"  HeaderText="From Date" UniqueName="FromDate" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="CMFTTCS_TODate"  HeaderText="To Date" UniqueName="ToDate" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="CMFTTCS_PRDate"  HeaderText="Purchase Date" UniqueName="purchasedate" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="CMFTTCS_TransactionType" HeaderText="Transaction Type" UniqueName="transactiontype" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="CMFTTCS_Units"  HeaderText="Units" UniqueName="units" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="CMFTTCS_Amount"  HeaderText="Amount" UniqueName="amount" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>
+                        
+                        <telerik:GridBoundColumn AllowFiltering="false" DataField="CMFTTCS_SubBroker" HeaderText="Sub Broker" UniqueName="subbroker" >
+                            <ItemStyle  Width="" HorizontalAlign="left"  Wrap="false" VerticalAlign="Top" />                            
+                        </telerik:GridBoundColumn>                        
+                        
+                        </Columns>
+                    </MasterTableView>
+                    <ClientSettings>                       
+                        <ClientEvents OnRowClick="onRowClick" />
+                    </ClientSettings>
+                 </telerik:RadGrid>
+          </td>
+    </tr>
+</table>
+
+<table width="100%">
+ <tr id="trReprocess" runat="server">
+        <td class="SubmitCell">
+            <asp:Button ID="btnReprocess"  runat="server" Text="Reprocess"
+                CssClass="PCGLongButton" OnClick=btnReprocess_Click OnClientClick="Loading(true);" />           
+                <asp:Button ID="btnDelete" runat="server" CssClass="PCGLongButton" Text="Delete Records" OnClick="btnDelete_Click"
+                />
+        </td>
+    </tr>
+    <tr id="trMessage" runat="server" visible="false">
+        <td class="Message">
+            <label id="lblEmptyMsg" class="FieldName">
+                There are no records to be displayed!</label>
+        </td>
+    </tr>
+    <tr id="trErrorMessage" runat="server" visible="false">
+        <td class="Message">
+            <asp:Label ID="lblError" CssClass="Message" runat="server">
+            </asp:Label>
+        </td>
+    </tr>
+    </table>
--- WealthERPSolution/UI/WealthERP/Uploads/TrailCommisionTransactionRejects.ascx.cs
+++ WealthERPSolution/UI/WealthERP/Uploads/TrailCommisionTransactionRejects.ascx.cs
@@ -4,14 +4,226 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+using VoUploads;
+using VoCustomerProfiling;
+using VoUser;
+using BoUploads;
+using BoCustomerProfiling;
+using BoUser;
+using VoCustomerPortfolio;
+using BoCustomerPortfolio;
+using System.Data;
+using System.Data.OleDb;
+using System.Xml;
+using System.Collections;
+using WealthERP.Base;
+using BoCommon;
+using System.Configuration;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using System.Globalization;
 
 namespace WealthERP.Uploads
 {
     public partial class TrailCommisionTransactionRejects : System.Web.UI.UserControl
     {
+        UploadCommonBo uploadsCommonBo;
+        AdvisorVo advisorVo;
+        CustomerVo customerVo;
+        int processId;
+        string configPath;
+        UploadProcessLogVo uploadProcessLogVo;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            customerVo = (CustomerVo)Session["customerVo"];
+            advisorVo = (AdvisorVo)Session["advisorVo"];
+            processId = 0;
+            configPath = Server.MapPath(ConfigurationManager.AppSettings["SSISConfigPath"].ToString());
+            if (Request.QueryString["processId"] != null)
+                processId = Int32.Parse(Request.QueryString["processId"].ToString());
+            if (!Page.IsPostBack)
+            {
+                BindTrailCommissionRejectedGrid(processId);
+
+            }
+        }
+
+        protected void ToggleRowSelection(object sender, EventArgs e)
+        {
+            ((sender as CheckBox).NamingContainer as GridItem).Selected = (sender as CheckBox).Checked;
+        }
+
+
+        protected void ToggleSelectedState(object sender, EventArgs e)
+        {
+            CheckBox headerCheckBox = (sender as CheckBox);
+            foreach (GridDataItem dataItem in GVTrailTransactionRejects.MasterTableView.Items)
+            {
+                (dataItem.FindControl("ChkOne") as CheckBox).Checked = headerCheckBox.Checked;
+            }
+        }
+
+        public void BindTrailCommissionRejectedGrid(int processId)
+        {
+            try
+            {
+                uploadsCommonBo = new UploadCommonBo();
+                DataSet dsRejectedSIP = new DataSet();
+                dsRejectedSIP = uploadsCommonBo.GetTrailCommissionRejectRejectDetails(advisorVo.advisorId, processId);
+                GVTrailTransactionRejects.DataSource = dsRejectedSIP;
+                GVTrailTransactionRejects.DataBind();
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+
+
+            foreach (GridDataItem gvr in this.GVTrailTransactionRejects.Items)
+            {
+                if (((CheckBox)gvr.FindControl("ChkOne")).Checked == true)
+                {
+                    i = i + 1;
+                }
+            }
+
+            if (i == 0)
+            {
+                ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "pageloadscript", "alert('Please select record to delete!');", true);
+                msgDelete.Visible = false;
+            }
+            else
+            {
+                msgDelete.Visible = true;
+                CustomerTrailCommissionTransactionDelete();
+                msgDelete.Visible = true;
+            }
 
         }
+
+        private void CustomerTrailCommissionTransactionDelete()
+        {
+            UploadCommonBo uploadsCommonBo = new UploadCommonBo();
+            foreach (GridDataItem gvr in this.GVTrailTransactionRejects.Items)
+            {
+                if (((CheckBox)gvr.FindControl("ChkOne")).Checked == true)
+                {
+                    int selectedRow = gvr.ItemIndex + 1;
+                    int StagingID = int.Parse((GVTrailTransactionRejects.MasterTableView.DataKeyValues[selectedRow - 1]["CMFTCCS_Id"].ToString()));
+                    uploadsCommonBo.DeleteMFTrailTransactionStaging(StagingID);
+                    BindTrailCommissionRejectedGrid(processId);
+                }
+            }
+
+        }
+
+        protected void btnReprocess_Click(object sender, EventArgs e)
+        {
+            bool blResult = false;
+            uploadsCommonBo = new UploadCommonBo();
+            UploadProcessLogVo processlogVo = new UploadProcessLogVo();
+            string error = "";
+            int processIdReprocessAll = 0;
+
+
+            if (Request.QueryString["processId"] != null)
+            {
+                processId = Int32.Parse(Request.QueryString["processId"].ToString());
+                processlogVo = uploadsCommonBo.GetProcessLogInfo(processId);
+                blResult = MFTrailTransactionInsertion(processId);
+
+            }
+
+            else
+            {
+                DataSet ds = uploadsCommonBo.GetSIPUploadRejectDistinctProcessIdForAdviser(advisorVo.advisorId);
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    processIdReprocessAll = int.Parse(dr["WUPL_ProcessId"].ToString());
+                    processlogVo = uploadsCommonBo.GetProcessLogInfo(processIdReprocessAll);
+
+                    blResult = MFTrailTransactionInsertion(processIdReprocessAll);
+
+                    if (blResult == false)
+                    {
+                        error = error + "Error when reprocessing for the processid:" + processIdReprocessAll + ";";
+                    }
+                }
+
+            }
+
+            if (error == "")
+            {
+                // Success Message
+                //trErrorMessage.Visible = true;
+                //lblError.Text = "Reprocess Done Successfully!";
+                msgReprocessComplete.Visible = true;
+
+            }
+            else
+            {
+                // Failure Message
+                trErrorMessage.Visible = true;
+                msgReprocessincomplete.Visible = true;
+                lblError.Text = "ErrorStatus:" + error;
+            }
+
+            BindTrailCommissionRejectedGrid(processId);
+        }
+
+
+        public bool MFTrailTransactionInsertion(int UploadProcessId)
+        {
+            bool blResult = false;
+            uploadProcessLogVo = new UploadProcessLogVo();
+            uploadsCommonBo = new UploadCommonBo();
+            CamsUploadsBo camsUploadsBo = new CamsUploadsBo();
+            bool TempletonTrailCommonStagingChk = false;
+            bool TempletonTrailCommonStagingToSetUp = false;
+            bool updateProcessLog = false;
+            string packagePath;
+
+
+            uploadProcessLogVo = uploadsCommonBo.GetProcessLogInfo(UploadProcessId);
+
+            packagePath = Server.MapPath("\\UploadPackages\\TrailCommisionUploadPackage\\TrailCommissionUpload\\TrailCommissionUpload\\callOnReprocess.dtsx");
+
+            TempletonTrailCommonStagingChk = uploadsCommonBo.TrailCommissionCommonStagingCheck(advisorVo.advisorId, UploadProcessId, packagePath, configPath);
+            uploadProcessLogVo.NoOfTransactionInserted = uploadsCommonBo.GetUploadSystematicInsertCount(UploadProcessId, "TN");
+
+            updateProcessLog = uploadsCommonBo.UpdateUploadProcessLog(uploadProcessLogVo);
+            if (TempletonTrailCommonStagingChk)
+            {
+                packagePath = Server.MapPath("\\UploadPackages\\TrailCommisionUploadPackage\\TrailCommissionUpload\\TrailCommissionUpload\\commonStagingToTrailSetUp.dtsx");
+                //TempletonTrailCommonStagingToSetUp = camsUploadsBo.CamsSIPCommonStagingToWERP(UploadProcessId, packagePath, configPath);
+                TempletonTrailCommonStagingToSetUp = camsUploadsBo.TempletonTrailCommissionCommonStagingChk(UploadProcessId, packagePath, configPath, "TN");
+
+
+                if (TempletonTrailCommonStagingToSetUp)
+                {
+                    uploadProcessLogVo.IsInsertionToWERPComplete = 1;
+                    uploadProcessLogVo.EndTime = DateTime.Now;
+                    uploadProcessLogVo.NoOfRejectedRecords = uploadsCommonBo.GetUploadSystematicRejectCount(UploadProcessId, "TN");
+                    blResult = uploadsCommonBo.UpdateUploadProcessLog(uploadProcessLogVo);
+                }
+            }
+            return blResult;
+        }
+
+        protected void GVTrailTransactionRejects_OnNeedDataSource(object source, GridNeedDataSourceEventArgs e)
+        {
+            uploadsCommonBo = new UploadCommonBo();
+            DataSet dsRejectedSIP = new DataSet();
+            dsRejectedSIP = uploadsCommonBo.GetTrailCommissionRejectRejectDetails(advisorVo.advisorId, processId);
+            GVTrailTransactionRejects.DataSource = dsRejectedSIP;
+        }
+
     }
 }
