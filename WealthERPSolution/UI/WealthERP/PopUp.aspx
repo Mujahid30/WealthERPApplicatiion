7d9cdd3d4793fa1fdfab1d59b3bfe57ff5486e0e
AMP\gobinda <AMP\gobinda>
2012-03-15 17:23:52 +0530

    R 6.0: popup page done

--- WealthERPSolution/UI/WealthERP/PopUp.aspx
+++ WealthERPSolution/UI/WealthERP/PopUp.aspx
@@ -1,16 +1,18 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PopUp.aspx.cs" Inherits="WealthERP.PopUp" %>
 
+<%@ Register Src="~/Customer/AddBankDetails.ascx" TagName="AddBankDetails" TagPrefix="Master" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" >
-<head runat="server">
-    <title></title>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Untitled Page</title>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        <asp:PlaceHolder ID="phLoadControl" runat="server"></asp:PlaceHolder>
+   
     </div>
     </form>
 </body>
 </html>
+
--- WealthERPSolution/UI/WealthERP/PopUp.aspx.cs
+++ WealthERPSolution/UI/WealthERP/PopUp.aspx.cs
@@ -4,14 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Resources;
 
 namespace WealthERP
 {
     public partial class PopUp : System.Web.UI.Page
     {
+        string path;
+        string pageID;
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Page.Theme = Session["Theme"].ToString();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["PageId"] != null)
+                pageID = Request.QueryString["PageId"].ToString();
+            path = Getpagepath(pageID);
+            UserControl uc1 = new UserControl();
 
+            uc1 = (UserControl)this.Page.LoadControl(path);
+            uc1.ID = "ctrl_" + pageID;
+            phLoadControl.Controls.Clear();
+            phLoadControl.Controls.Add(uc1);
         }
+
+        protected string Getpagepath(string pageID)
+        {
+            ResourceManager resourceMessages = new ResourceManager("WealthERP.ControlMapping", typeof(ControlHost).Assembly);
+            return resourceMessages.GetString(pageID);
+        }
+
     }
+
 }
+
