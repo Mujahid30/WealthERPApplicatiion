ab2771e7e0ce1bd5a8005b2a82dca686a17b9887
AMP\joshan <AMP\joshan>
2012-03-16 12:58:19 +0530

    Created Vo for Repository....

--- WealthERPSolution/Vo/VoCommon/RepositoryVo.cs
+++ WealthERPSolution/Vo/VoCommon/RepositoryVo.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace VoCommon
 {
-   public class RepositoryVo
+    public class RepositoryVo
     {
+        public int RepositoryId { get; set; }
+        public int AdviserId { get; set; }
+        public string CategoryCode { get; set; }
+        public string HeadingText { get; set; }
+        public bool IsFile { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
     }
 }
