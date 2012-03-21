56e680613be5a99e7c157c31e3f1f0e16fd4b7d6
AMP\bhoopendra <AMP\bhoopendra>
2012-03-21 11:42:13 +0530

    RE(6.0)--> UI design for filters & their functionality added.

--- WealthERPSolution/UI/WealthERP/BusinessMIS/MultiProductMIS.ascx
+++ WealthERPSolution/UI/WealthERP/BusinessMIS/MultiProductMIS.ascx
@@ -1,1 +1,134 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="MultiProductMIS.ascx.cs" Inherits="WealthERP.BusinessMIS.MultiProductMIS" %>
+<%@ Register Src="~/General/Pager.ascx" TagPrefix="Pager" TagName="Pager" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" %>
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Charting" Assembly="Telerik.Web.UI" %>
+
+<script type="text/javascript" language="javascript">
+    function GetCustomerId(source, eventArgs) {
+        document.getElementById("<%= hdnCustomerId.ClientID %>").value = eventArgs.get_value();
+        return false;
+    };
+</script>
+
+<asp:ScriptManager ID="scrptMgr" runat="server">
+</asp:ScriptManager>
+<style type="text/css">
+    .fk-lres-header {
+    font-size: 13px;
+    margin-bottom: 10px;
+    padding: 5px 7px;
+    border:solid 1px;
+    }
+</style>
+<table style="width:100%;">
+    <tr>
+        <td class="HeaderTextBig">
+            <asp:Label ID="lblMultiAssetMIS" runat="server" CssClass="HeaderTextBig" Text="Multi-Asset MIS"></asp:Label>
+            <hr />
+        </td>                
+    </tr>
+</table>
+<%--<div class="divSectionHeading" style="vertical-align: text-bottom">
+</div>--%>
+<table class="TableBackground" width="100%">
+    
+    <tr id="trBranchRM" runat="server">
+        <td class="leftField" style="width:15%">
+            <asp:Label ID="lblBranch" runat="server" CssClass="FieldName" Text="Branch:"></asp:Label>
+        </td>
+        <td class="rightField" style="width:15%">
+            <asp:DropDownList ID="ddlBranch" runat="server" style="vertical-align: middle" AutoPostBack="true"
+            CssClass="cmbField" onselectedindexchanged="ddlBranch_SelectedIndexChanged" >
+         </asp:DropDownList> 
+        </td>
+        <td class="leftField" style="width:10%">
+            <asp:Label ID="lblRM" runat="server" CssClass="FieldName" Text="RM:"></asp:Label>
+        </td>
+        <td class="rightField" style="width:25%">
+            <asp:DropDownList ID="ddlRM" runat="server" CssClass="cmbField" style="vertical-align: middle" >
+            </asp:DropDownList>
+        </td>
+        <td class="leftField" style="width:35%"></td>
+    </tr>
+    <%--<asp:UpdatePanel ID="UPPickCustomer" runat="server">
+    <ContentTemplate>--%>
+    <tr>        
+        <td class="leftField" style="width:15%">
+            <asp:Label ID="lblGrpOrInd" runat="server" CssClass="FieldName" Text="MIS for :"></asp:Label>
+        </td>
+        <td class="rightField" style="width:15%">
+            <asp:DropDownList ID="ddlCustomerType" runat="server" CssClass="cmbField" style="vertical-align: middle" AutoPostBack="true"  onselectedindexchanged="ddlCustomerType_SelectedIndexChanged">
+                <asp:ListItem Value="All Customer" Text="All Customer"></asp:ListItem>
+                <asp:ListItem Value="Pick Customer" Text="Pick Customer"></asp:ListItem>
+            </asp:DropDownList>
+            <%--<asp:RadioButton runat="server" ID="rdoAllCustomer" Text="All Customers" AutoPostBack="true"
+              Class="cmbField" GroupName="SelectCustomer" Checked="True" oncheckedchanged="rdoAllCustomer_CheckedChanged"/> --%>  
+        </td>
+        <td class="leftField" style="width:10%">
+            <asp:Label ID="lblSelectTypeOfCustomer" runat="server" CssClass="FieldName" Text="Customer Type: "></asp:Label>
+        </td>
+        <td class="rightField" style="width:25%">
+            <%--<asp:RadioButton runat="server" ID="rdoPickCustomer" Text="Pick Customer" AutoPostBack="true"
+              Class="cmbField" GroupName="SelectCustomer" oncheckedchanged="rdoPickCustomer_CheckedChanged"/>--%>
+            <asp:DropDownList ID="ddlSelectCustomer" style="vertical-align: middle" runat="server" CssClass="cmbField" AutoPostBack="true" onselectedindexchanged="ddlSelectCustomer_SelectedIndexChanged">
+                <asp:ListItem Value="Select" Text="Select" Selected="True"></asp:ListItem>
+                <asp:ListItem Value="Group Head" Text="Group Head"></asp:ListItem>
+                <asp:ListItem Value="Individual" Text="Individual"></asp:ListItem>
+            </asp:DropDownList>   
+        </td>
+        <td class="leftField" style="width:35%"></td>
+    </tr>
+    <tr>        
+        <td class="leftField" style="width:15%">
+            &nbsp;</td>
+        <td class="rightField" style="width:15%">
+            &nbsp;</td>
+        <td class="leftField" style="width:10%">
+            <asp:Label ID="lblselectCustomer" runat="server" CssClass="FieldName" Text="Search Customer: "></asp:Label>
+        </td>
+        <td class="rightField" style="width:25%">
+                <asp:TextBox ID="txtIndividualCustomer" runat="server" CssClass="txtField" AutoComplete="Off" AutoPostBack="True">  </asp:TextBox>
+                <cc1:TextBoxWatermarkExtender ID="txtIndividualCustomer_water" TargetControlID="txtIndividualCustomer" WatermarkText="Enter few chars of Customer"
+                runat="server" EnableViewState="false"></cc1:TextBoxWatermarkExtender>
+                <ajaxToolkit:AutoCompleteExtender ID="txtIndividualCustomer_autoCompleteExtender" runat="server"
+                TargetControlID="txtIndividualCustomer" ServiceMethod="GetCustomerName" ServicePath="~/CustomerPortfolio/AutoComplete.asmx"
+                MinimumPrefixLength="1" EnableCaching="False" CompletionSetCount="5" CompletionInterval="100"
+                CompletionListCssClass="AutoCompleteExtender_CompletionList" CompletionListItemCssClass="AutoCompleteExtender_CompletionListItem"
+                CompletionListHighlightedItemCssClass="AutoCompleteExtender_HighlightedItem"
+                UseContextKey="True" OnClientItemSelected="GetCustomerId" DelimiterCharacters="" Enabled="True"  />
+                  
+         <asp:RequiredFieldValidator ID="rquiredFieldValidatorIndivudialCustomer" Display="Dynamic" ControlToValidate="txtIndividualCustomer" CssClass="rfvPCG" ErrorMessage="<br />Please select the customer" runat="server" ValidationGroup="btnGo">
+         </asp:RequiredFieldValidator>
+        </td>
+        <td class="leftField" style="width:35%"></td>
+    </tr>
+    <%--</ContentTemplate>
+    </asp:UpdatePanel> --%>   
+</table>
+<div class="fk-lres-header" style="margin-left:10%; padding-bottom:5px;width:43%;text-align:center">
+    <asp:LinkButton ID="lnkBtnMultiProductMIS" Text="Multi-Product" CssClass="LinkButtons" runat="server"></asp:LinkButton>
+    <span>|</span>
+
+    <asp:LinkButton ID="lnkBtnLifeInsuranceMIS" Text="LifeInsurance" CssClass="LinkButtons" runat="server"></asp:LinkButton>
+    <span>|</span>
+
+    <asp:LinkButton ID="lnkBtnGeneralInsuranceMIS" Text="General Insurance" CssClass="LinkButtons" runat="server"></asp:LinkButton>
+    <span>|</span>
+
+    <asp:LinkButton ID="lnkBtnInvestmentMIS" Text="Investment" CssClass="LinkButtons" runat="server"></asp:LinkButton>       
+</div>
+
+<div class="divSectionHeading" style="vertical-align: text-bottom">
+</div>
+<table class="TableBackground">
+    <tr>
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+</table>
+
+<asp:HiddenField ID="hdnCustomerId" runat="server"/>
+<asp:HiddenField ID="hdnIndividualOrGroup" runat="server" />
--- WealthERPSolution/UI/WealthERP/BusinessMIS/MultiProductMIS.ascx.cs
+++ WealthERPSolution/UI/WealthERP/BusinessMIS/MultiProductMIS.ascx.cs
@@ -4,14 +4,426 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BoAdvisorProfiling;
+using System.Data;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using WealthERP.Base;
+using VoUser;
+using BoUploads;
+using BoCustomerProfiling;
 
 namespace WealthERP.BusinessMIS
 {
     public partial class MultiProductMIS : System.Web.UI.UserControl
     {
+        AdvisorVo advisorVo = new AdvisorVo();
+        RMVo rmVo = new RMVo();
+        CustomerVo customerVo = new CustomerVo();
+        AdvisorBranchBo advisorBranchBo = new AdvisorBranchBo();
+        CustomerBo customerBo = new CustomerBo();
+
+        int advisorId = 0;
+        int customerId = 0;
+        String userType;
+        int rmId = 0;
+        int bmID = 0;
+        string customerType = string.Empty;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            advisorVo = (AdvisorVo)Session["advisorVo"];
+            rmVo = (RMVo)Session[SessionContents.RmVo];
 
+            if (Session[SessionContents.CurrentUserRole].ToString().ToLower() == "admin")
+                userType = "advisor";
+            else if (Session[SessionContents.CurrentUserRole].ToString().ToLower() == "rm")
+                userType = "rm";
+            else if (Session[SessionContents.CurrentUserRole].ToString().ToLower() == "bm")
+                userType = "bm";
+            else
+                userType = Session[SessionContents.CurrentUserRole].ToString().ToLower();
+
+            advisorId = advisorVo.advisorId;
+            int RMId = rmVo.RMId;
+            customerId = customerVo.CustomerId;
+            rmId = rmVo.RMId;
+            bmID = rmVo.RMId;
+
+            if (!IsPostBack)
+            {
+                ddlSelectCustomer.Visible = false;
+                lblSelectTypeOfCustomer.Visible = false;
+                lblselectCustomer.Visible = false;
+                txtIndividualCustomer.Visible = false;              
+               
+                Session["ButtonGo"] = null;
+                rquiredFieldValidatorIndivudialCustomer.Visible = false;
+
+                if (userType == "advisor")
+                {
+                    BindBranchDropDown();
+                    BindRMDropDown();
+                }
+                else if (userType == "rm")
+                {
+                    trBranchRM.Visible = false;
+                }
+                if (userType == "bm")
+                {
+                    BindBranchForBMDropDown();
+                    BindRMforBranchDropdown(0, bmID);
+                }
+            }
         }
+
+        //protected void hdnCustomerId_ValueChanged(object sender, EventArgs e)
+        //{
+        //    if (!string.IsNullOrEmpty(hdnCustomerId.Value.ToString().Trim()))
+        //    {
+        //        customerVo = customerBo.GetCustomer(int.Parse(hdnCustomerId.Value));
+        //    }
+        //}
+
+        private void BindRMDropDown()
+        {
+            try
+            {
+                AdvisorStaffBo advisorStaffBo = new AdvisorStaffBo();
+                DataTable dt = advisorStaffBo.GetAdviserRM(advisorVo.advisorId);
+                if (dt.Rows.Count > 0)
+                {
+                    ddlRM.DataSource = dt;
+                    ddlRM.DataValueField = dt.Columns["AR_RMId"].ToString();
+                    ddlRM.DataTextField = dt.Columns["RMName"].ToString();
+                    ddlRM.DataBind();
+                }
+                ddlRM.Items.Insert(0, new System.Web.UI.WebControls.ListItem("All", "2"));
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "RMAMCSchemewiseMIS.ascx:BindRMDropDown()");
+
+                object[] objects = new object[0];
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+        private void BindBranchDropDown()
+        {
+
+            RMVo rmVo = new RMVo();
+            rmVo = (RMVo)Session[SessionContents.RmVo];
+            int bmID = rmVo.RMId;
+            try
+            {
+                UploadCommonBo uploadsCommonDao = new UploadCommonBo();
+                DataSet ds = uploadsCommonDao.GetAdviserBranchList(advisorVo.advisorId, "adviser");
+                if (ds != null)
+                {
+                    ddlBranch.DataSource = ds;
+                    ddlBranch.DataValueField = ds.Tables[0].Columns["AB_BranchId"].ToString();
+                    ddlBranch.DataTextField = ds.Tables[0].Columns["AB_BranchName"].ToString();
+                    ddlBranch.DataBind();
+                }
+                ddlBranch.Items.Insert(0, new System.Web.UI.WebControls.ListItem("All", bmID.ToString()));
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "RMAMCSchemewiseMIS.ascx:BindBranchDropDown()");
+
+                object[] objects = new object[4];
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+        private void BindRMforBranchDropdown(int branchId, int branchHeadId)
+        {
+
+            try
+            {
+
+                DataSet ds = advisorBranchBo.GetAllRMsWithOutBMRole(branchId, branchHeadId);
+                if (ds != null)
+                {
+                    ddlRM.DataSource = ds.Tables[0]; ;
+                    ddlRM.DataValueField = ds.Tables[0].Columns["RmID"].ToString();
+                    ddlRM.DataTextField = ds.Tables[0].Columns["RMName"].ToString();
+                    ddlRM.DataBind();
+                }
+                ddlRM.Items.Insert(0, new System.Web.UI.WebControls.ListItem("All", "0"));
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdviserEQMIS.ascx:BindRMforBranchDropdown()");
+
+                object[] objects = new object[4];
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+        private void BindBranchForBMDropDown()
+        {
+            try
+            {
+                DataSet ds = advisorBranchBo.GetBranchsRMForBMDp(0, bmID, 0);
+                if (ds != null)
+                {
+                    ddlBranch.DataSource = ds.Tables[1]; ;
+                    ddlBranch.DataValueField = ds.Tables[1].Columns["AB_BranchId"].ToString();
+                    ddlBranch.DataTextField = ds.Tables[1].Columns["AB_BranchName"].ToString();
+                    ddlBranch.DataBind();
+                }
+                ddlBranch.Items.Insert(0, new System.Web.UI.WebControls.ListItem("All", bmID.ToString()));
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdviserRMMFSystematicMIS.ascx:BindBranchDropDown()");
+
+                object[] objects = new object[4];
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+        protected void ddlBranch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlBranch.SelectedIndex == 0)
+            {
+                BindRMforBranchDropdown(0, bmID);
+            }
+            else
+            {
+                BindRMforBranchDropdown(int.Parse(ddlBranch.SelectedValue.ToString()), 0);
+            }
+        }
+
+        protected void ddlSelectCustomer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtIndividualCustomer.Text = string.Empty;
+            hdnIndividualOrGroup.Value = ddlSelectCustomer.SelectedItem.Value;
+            if (ddlSelectCustomer.SelectedItem.Value == "Group Head")
+            {
+                customerType = "GROUP";
+                if (Session[SessionContents.CurrentUserRole].ToString() == "RM")
+                {
+                    txtIndividualCustomer_autoCompleteExtender.ContextKey = rmVo.RMId.ToString();
+                    txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetParentCustomerName";
+                }
+                else if (Session[SessionContents.CurrentUserRole].ToString() == "Admin")
+                {
+                    if ((ddlBranch.SelectedIndex == 0) && (ddlRM.SelectedIndex == 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = advisorVo.advisorId.ToString();
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetAdviserGroupCustomerName";
+                    }
+                    else if ((ddlBranch.SelectedIndex != 0) && (ddlRM.SelectedIndex == 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlBranch.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetPerticularBranchsAllGroupCustomers";
+                    }
+                    else if ((ddlBranch.SelectedIndex == 0) && (ddlRM.SelectedIndex != 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlRM.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetParentCustomerName";
+                    }
+                    else if ((ddlBranch.SelectedIndex != 0) && (ddlRM.SelectedIndex != 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlBranch.SelectedValue + '~' + ddlRM.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetAllBranchAndRMGroupCustomers";
+                    }
+                }
+
+                else if (Session[SessionContents.CurrentUserRole].ToString() == "BM")
+                {
+                    if ((ddlBranch.SelectedIndex == 0) && (ddlRM.SelectedIndex == 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = rmVo.RMId.ToString();
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetBMParentCustomerNames";
+                    }
+                    if ((ddlBranch.SelectedIndex != 0) && (ddlRM.SelectedIndex == 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlBranch.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetPerticularBranchsAllGroupCustomers";
+                    }
+                    if ((ddlBranch.SelectedIndex == 0) && (ddlRM.SelectedIndex != 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlRM.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetParentCustomerName";
+                    }
+                    if ((ddlBranch.SelectedIndex != 0) && (ddlRM.SelectedIndex != 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlBranch.SelectedValue + '~' + ddlRM.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetAllBranchAndRMGroupCustomers";
+                    }
+                }
+            }
+            else if (ddlSelectCustomer.SelectedItem.Value == "Individual")
+            {
+                txtIndividualCustomer.Visible = true;
+                customerType = "IND";
+
+                //rquiredFieldValidatorIndivudialCustomer.Visible = true;
+                if (Session[SessionContents.CurrentUserRole].ToString() == "RM")
+                {
+                    txtIndividualCustomer_autoCompleteExtender.ContextKey = rmVo.RMId.ToString();
+                    txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetMemberCustomerName";
+
+                }
+                else if (Session[SessionContents.CurrentUserRole].ToString() == "Admin")
+                {
+                    if ((ddlBranch.SelectedIndex == 0) && (ddlRM.SelectedIndex == 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = advisorVo.advisorId.ToString();
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetAdviserCustomerName";
+                    }
+                    else if ((ddlBranch.SelectedIndex != 0) && (ddlRM.SelectedIndex == 0))
+                    {
+
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlBranch.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetPerticularBranchsAllIndividualCustomers";
+                    }
+                    else if ((ddlBranch.SelectedIndex == 0) && (ddlRM.SelectedIndex != 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlRM.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetMemberCustomerName";
+                    }
+                    else if ((ddlBranch.SelectedIndex != 0) && (ddlRM.SelectedIndex != 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlBranch.SelectedValue + '~' + ddlRM.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetAllBranchAndRMIndividualCustomers";
+                    }
+                }
+                else if (Session[SessionContents.CurrentUserRole].ToString() == "BM")
+                {
+                    if ((ddlBranch.SelectedIndex == 0) && (ddlRM.SelectedIndex == 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = rmVo.RMId.ToString();
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetBMIndividualCustomerNames";
+                    }
+                    else if ((ddlBranch.SelectedIndex != 0) && (ddlRM.SelectedIndex == 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlBranch.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetPerticularBranchsAllIndividualCustomers";
+                    }
+                    else if ((ddlBranch.SelectedIndex == 0) && (ddlRM.SelectedIndex != 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlRM.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetMemberCustomerName";
+                    }
+                    else if ((ddlBranch.SelectedIndex != 0) && (ddlRM.SelectedIndex != 0))
+                    {
+                        txtIndividualCustomer_autoCompleteExtender.ContextKey = ddlBranch.SelectedValue + '~' + ddlRM.SelectedValue;
+                        txtIndividualCustomer_autoCompleteExtender.ServiceMethod = "GetAllBranchAndRMIndividualCustomers";
+                    }
+                }
+            }
+        }
+
+        protected void ddlCustomerType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlCustomerType.SelectedItem.Value == "All Customer")
+            {
+                ddlSelectCustomer.Visible = false;
+                lblSelectTypeOfCustomer.Visible = false;
+                txtIndividualCustomer.Visible = false;
+                lblselectCustomer.Visible = false;
+                rquiredFieldValidatorIndivudialCustomer.Visible = false;
+
+                ViewState["GroupHeadCustomers"] = null;
+                ViewState["IndividualCustomers"] = null;
+            }
+            if (ddlCustomerType.SelectedItem.Value == "Pick Customer")
+            {
+                ddlSelectCustomer.Visible = true;
+                lblSelectTypeOfCustomer.Visible = true;
+                txtIndividualCustomer.Visible = true;
+                txtIndividualCustomer.Text = string.Empty;
+                lblselectCustomer.Visible = true;
+                rquiredFieldValidatorIndivudialCustomer.Visible = true;
+
+                ViewState["GroupHeadCustomers"] = null;
+                ViewState["IndividualCustomers"] = null;
+                ddlSelectCustomer.SelectedIndex = 0;
+            }
+        }
+
+        //protected void rdoAllCustomer_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    ddlSelectCustomer.Visible = false;
+        //    lblSelectTypeOfCustomer.Visible = false;
+        //    txtIndividualCustomer.Visible = false;
+        //    lblselectCustomer.Visible = false;
+        //    rquiredFieldValidatorIndivudialCustomer.Visible = false;
+
+        //    ViewState["GroupHeadCustomers"] = null;
+        //    ViewState["IndividualCustomers"] = null;
+            
+            
+        //    //tblMessage.Visible = true;
+        //    //ErrorMessage.Visible = true;
+        //    //ErrorMessage.InnerText = "No Records Found...!";
+
+        //}
+
+        //protected void rdoPickCustomer_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    ddlSelectCustomer.Visible = true;
+        //    lblSelectTypeOfCustomer.Visible = true;
+        //    txtIndividualCustomer.Visible = true;
+        //    txtIndividualCustomer.Text = string.Empty;
+        //    lblselectCustomer.Visible = true;
+        //    rquiredFieldValidatorIndivudialCustomer.Visible = true;
+        //    ViewState["GroupHeadCustomers"] = null;
+        //    ViewState["IndividualCustomers"] = null;
+        //    ddlSelectCustomer.SelectedIndex = 0;
+            
+        //    //tblMessage.Visible = true;
+        //    //ErrorMessage.Visible = true;
+        //    //ErrorMessage.InnerText = "No Records Found...!";
+        //}
     }
 }
