57d74ad7d1a017b664beba59a1dcba4b156a42bb
AMP\psahoo:5 <AMP\psahoo:5>
2012-04-24 14:55:35 +0530

    RE:6.0-Portfolio XIRR Extra 100 multiplication issue solved

--- WealthERPSolution/Bo/CustomerPortfolio/CustomerPortfolioBo.cs
+++ WealthERPSolution/Bo/CustomerPortfolio/CustomerPortfolioBo.cs
@@ -3437,7 +3437,7 @@ namespace BoCustomerPortfolio
                 drXIRR["CustomerName"] = dr["C_CustomerName"];
                 drXIRR["PortfolioId"] = tempPortfoliId;
                 drXIRR["PortfolioName"] = dr["CP_PortfolioName"];
-                drXIRR["XIRR"] = Math.Round(tempPortfolioXIRR*100,5);
+                drXIRR["XIRR"] = Math.Round(tempPortfolioXIRR,5);
                 dtCustomerPortfolioXIRR.Rows.Add(drXIRR);
             }
 
