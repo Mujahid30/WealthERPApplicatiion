ab2847cdabf72168efff9c3bde93e3e97b1fdb4a
AMP\gobinda <AMP\gobinda>
2012-05-21 12:33:35 +0530

    RE 6.2 : email vo

--- WealthERPSolution/Vo/VoEmailSMS/EmailVo.cs
+++ WealthERPSolution/Vo/VoEmailSMS/EmailVo.cs
@@ -7,6 +7,10 @@ namespace VoEmailSMS
 {
     public class EmailVo
     {
+        //custom and standard email sending .. 
+
+        #region for custom email       
+
         private int m_EmailQueueId;
         private int m_AdviserId;
         private int m_CustomerId;
@@ -64,12 +68,88 @@ namespace VoEmailSMS
             get { return m_EmailQueueId; }
             set { m_EmailQueueId = value; }
         }
-        
+
 
         public int AdviserId
         {
             get { return m_AdviserId; }
             set { m_AdviserId = value; }
         }
+
+        #endregion
+
+        #region for sending the email to queue
+
+        private string m_To;
+        private string m_Cc;
+        private string m_Bcc;
+        private string m_Subject;
+        private string m_Body;
+        private int m_Sent;
+        private int m_NoOfRetries;
+        private string m_DaemonCode;
+        private DateTime m_AllocatedOn;
+        private string m_ErrorMsg;
+
+        public string To            
+        {
+            get { return m_To; }
+            set { m_To = value; }
+        }
+
+        public string Cc
+        {
+            get { return m_Cc; }
+            set { m_Cc = value; }
+        }
+
+        public string Bcc
+        {
+            get { return m_Bcc; }
+            set { m_Bcc = value; }
+        }
+
+        public string Subject
+        {
+            get { return m_Subject; }
+            set { m_Subject = value; }
+        }
+
+        public string Body
+        {
+            get { return m_Body; }
+            set { m_Body = value; }
+        }
+
+        public int Sent
+        {
+            get { return m_Sent; }
+            set { m_Sent = value; }
+        }
+
+        public int NoOfRetries
+        {
+            get { return m_NoOfRetries; }
+            set { m_NoOfRetries = value; }
+        }
+
+        public string DaemonCode
+        {
+            get { return m_DaemonCode; }
+            set { m_DaemonCode = value; }
+        }
+
+        public DateTime AllocatedOn
+        {
+            get { return m_AllocatedOn; }
+            set { m_AllocatedOn = value; }
+        }
+
+        public string ErrorMsg
+        {
+            get { return m_ErrorMsg; }
+            set { m_ErrorMsg = value; }
+        }
+        #endregion
     }
 }
