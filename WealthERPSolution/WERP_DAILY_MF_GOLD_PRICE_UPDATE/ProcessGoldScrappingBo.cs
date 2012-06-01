e4a5e63b436c5d0978597616085ea957313b5038
AMP\Administrator:4 <AMP\Administrator:4>
2012-06-01 15:28:48 +0530

    RE:6.2-Goal Scrpping Project added to TFS

---
+++ WealthERPSolution/WERP_DAILY_MF_GOLD_PRICE_UPDATE/ProcessGoldScrappingBo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections;
+using WatiN.Core;
+using BoCommon;
+
+namespace WERP_DAILY_MF_GOLD_PRICE_UPDATE
+{
+    public class ProcessGoldScrappingBo
+    {
+        private static DateTime goldUpdatedDate;
+        private static decimal goldPrice;
+        
+        public void GoldPriceUpdateProcessor()
+        {
+           OpenNCDXWebsiteforGoldPriceScrapping();
+            
+        }
+        private void OpenNCDXWebsiteforGoldPriceScrapping()
+        {
+            using (var browser = new IE("http://www.ncdex.com/MarketData/LiveSpotQuotes.aspx"))
+            {
+                RetrieveGoldPricefromNCDXPage(browser);
+            }
+        }
+
+        private void RetrieveGoldPricefromNCDXPage(IE browser)
+        {
+            ProcessGoldScrappingDao processGoldScrappingDao = new ProcessGoldScrappingDao();
+            int i = 0;
+            Table _table = browser.Table(Find.ById("ctl00_ContentPlaceHolder3_gvSpotQuotes"));
+            if (_table.TableRows.Count != 0)
+            {
+                foreach (TableRow TR in _table.TableRows)
+                {
+                    if ((_table.TableRows[i].TableCells[1].Text == "GOLD (100 gms)" && _table.TableRows[i].TableCells[3].Text == "Mumbai"))
+                    {
+                        goldPrice = decimal.Parse(_table.TableRows[i].TableCells[5].Text);
+                        goldUpdatedDate = DateTime.Now;
+                        break;
+                    }
+                    else
+                        i++;
+                }
+            }
+            processGoldScrappingDao.UpdateGoldPrice(goldPrice, goldUpdatedDate);
+
+        }
+
+        
+    }
+}
---
+++ WealthERPSolution/WERP_DAILY_MF_GOLD_PRICE_UPDATE/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_DAILY_MF_GOLD_PRICE_UPDATE
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            ProcessGoldScrapping();
+
+        }
+
+        public static void ProcessGoldScrapping()
+        {
+
+ 
+        }
+    }
+}
