063d4ae59e6f9ffd69f1fd944fd67e468556c116
AMP\Administrator:4 <AMP\Administrator:4>
2012-06-01 12:46:11 +0530

    RE:6.2-Trasaction Balance Valuation project added to TFS

---
+++ WealthERPSolution/WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION/MFTransactionBalanceProcessBo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VoUser;
+using BoValuation;
+using BoWerpAdmin;
+using VoCustomerPortfolio;
+using BoCustomerPortfolio;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+
+namespace WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION
+{
+    public class MFTransactionBalanceProcessBo
+    {
+        AdviserMaintenanceBo adviserMaintenanceBo = new AdviserMaintenanceBo();
+        List<AdvisorVo> adviserVoList = new List<AdvisorVo>();
+        BoValuation.MFEngineBo.ValuationLabel valuationFor = BoValuation.MFEngineBo.ValuationLabel.Advisor;
+        MFEngineBo mfEngineBo = new MFEngineBo();
+        CustomerPortfolioBo customerPortfolioBo = new CustomerPortfolioBo();
+        
+        public void CreateMFTransactionBalanceForAllAdviser()
+        {
+            adviserVoList = adviserMaintenanceBo.GetAdviserList();
+            for (int i = 0; i < adviserVoList.Count; i++)
+            {              
+                
+                try
+                {
+                    mfEngineBo.MFBalanceCreation(adviserVoList[i].advisorId, 0, valuationFor);
+                   
+                }
+                catch
+                {
+
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ WealthERPSolution/WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProcessMFTransactionBalance();
+
+        }
+        public static void ProcessMFTransactionBalance()
+        {
+            //MFTransactionBalanceCreation MFBalance = new MFTransactionBalanceCreation();
+            //MFBalance.CreateMFTransactionBalanceForAllAdviser();
+        }
+
+    }
+}
