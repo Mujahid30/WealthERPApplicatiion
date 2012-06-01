ca0316d85bc2c08e0ec7fb5510ab8231942a0d9a
AMP\Administrator:4 <AMP\Administrator:4>
2012-06-01 12:20:20 +0530

    RE:6.2-MF NetPosition daily Valuation added to TFS

---
+++ WealthERPSolution/WERP_DAILY_MF_NETPOSITION_VALUATION/MFNetpositionProcessBo.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VoUser;
+using BoValuation;
+using BoWerpAdmin;
+using VoCustomerPortfolio;
+using BoCustomerPortfolio;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+
+namespace WERP_DAILY_MF_NETPOSITION_VALUATION
+{
+    public class MFNetpositionProcessBo
+    {
+        AdviserMaintenanceBo adviserMaintenanceBo = new AdviserMaintenanceBo();
+        List<AdvisorVo> adviserVoList = new List<AdvisorVo>();
+        BoValuation.MFEngineBo.ValuationLabel valuationFor = BoValuation.MFEngineBo.ValuationLabel.Advisor;
+        MFEngineBo mfEngineBo = new MFEngineBo();
+        CustomerPortfolioBo customerPortfolioBo = new CustomerPortfolioBo();
+        public void CreateMFNetpositionForAllAdviser()
+        {
+            adviserVoList = adviserMaintenanceBo.GetAdviserList();
+            for (int i = 0; i < adviserVoList.Count; i++)
+            {
+                int logId = 0;
+                logId = CreateAdviserEODLog("MF", DateTime.Today.AddDays(-1), adviserVoList[i].advisorId);
+                try
+                {
+                    mfEngineBo.MFNetPositionCreation(adviserVoList[i].advisorId, 0, valuationFor, DateTime.Today.AddDays(-1));
+                    UpdateAdviserEODLog("MF", 1, logId);
+                }
+                catch
+                {
+
+
+                }
+
+            }
+        }
+
+
+        private int CreateAdviserEODLog(string assetType, DateTime dt, int adviserId)
+        {
+            int LogId = 0;
+
+            AdviserDailyLOGVo adviserDaliyLOGVo = new AdviserDailyLOGVo();
+            try
+            {
+                adviserDaliyLOGVo.AdviserId = adviserId;
+                adviserDaliyLOGVo.CreatedBy = 1000;
+                adviserDaliyLOGVo.StartTime = DateTime.Now;
+                adviserDaliyLOGVo.ProcessDate = dt;
+                adviserDaliyLOGVo.AssetGroup = assetType;
+                LogId = customerPortfolioBo.CreateAdviserEODLog(adviserDaliyLOGVo);
+            }
+            catch (BaseApplicationException Ex)
+            {
+
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "MFNetpositionCreation:CreateAdviserEODLog()");
+                object[] objects = new object[2];
+                objects[0] = assetType;
+                objects[1] = dt;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return LogId;
+        }
+
+        protected void UpdateAdviserEODLog(string group, int IsComplete, int LogId)
+        {
+            AdviserDailyLOGVo adviserDaliyLOGVo = new AdviserDailyLOGVo();
+            try
+            {
+                adviserDaliyLOGVo.IsEquityCleanUpComplete = 0;
+                adviserDaliyLOGVo.IsValuationComplete = IsComplete;
+                adviserDaliyLOGVo.ModifiedBy = 1000;
+                adviserDaliyLOGVo.EODLogId = LogId;
+                adviserDaliyLOGVo.AssetGroup = group;
+                adviserDaliyLOGVo.EndTime = DateTime.Now;
+                customerPortfolioBo.UpdateAdviserEODLog(adviserDaliyLOGVo);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "MFNetpositionCreation:UpdateAdviserEODLog()");
+                object[] objects = new object[3];
+                objects[0] = group;
+                objects[1] = IsComplete;
+                objects[2] = LogId;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+    }
+}
---
+++ WealthERPSolution/WERP_DAILY_MF_NETPOSITION_VALUATION/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_DAILY_MF_NETPOSITION_VALUATION
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProcessMFTransactionBalance();
+
+        }
+
+        public static void ProcessMFTransactionBalance()
+        {
+            MFNetpositionProcessBo MFNetposition = new MFNetpositionProcessBo();
+            MFNetposition.CreateMFNetpositionForAllAdviser();
+        }
+    }
+}
