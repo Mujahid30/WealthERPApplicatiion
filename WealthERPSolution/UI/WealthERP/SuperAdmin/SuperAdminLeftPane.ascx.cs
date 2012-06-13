c888eda444f24ae51712f13ba1f32de923055b97
AMP\psahoo:5 <AMP\psahoo:5>
2012-06-13 12:19:13 +0530

    RE:6.3-For SuperAdmin MF query and other items in top header

--- WealthERPSolution/UI/WealthERP/SuperAdmin/SuperAdminLeftPane.ascx.cs
+++ WealthERPSolution/UI/WealthERP/SuperAdmin/SuperAdminLeftPane.ascx.cs
@@ -27,7 +27,7 @@ namespace WealthERP.SuperAdmin
             if (!IsPostBack)
             {
                 RadPanelBar1.CollapseAllItems();
-                ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "pageloadscript", "loadtopmenu('SuperAdminLeftPane');", true);
+                ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "pageloadscript", "loadtopmenu('AdvisorLeftPane');", true);
             }
         }
 
