00d89eef141082a10939a454008e1491f53c8a9a
AMP\Administrator:4 <AMP\Administrator:4>
2012-07-13 18:17:12 +0530

    RE:6.4-For each type order added one one class

---
+++ WealthERPSolution/DaoOps/LIOrderDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using VoOps;
+
+namespace DaoOps
+{
+    public class LIOrderDao:OrderDao
+    {
+
+    }
+}
