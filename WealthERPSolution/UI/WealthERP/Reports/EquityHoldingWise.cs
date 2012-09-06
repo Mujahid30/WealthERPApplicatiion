b7d480ff6304e6b98da79351c6024798bb850410
AMP\baivab <AMP\baivab>
2012-09-06 16:58:43 +0530

    RE:6.5 Changes Done

--- WealthERPSolution/UI/WealthERP/Reports/EquityHoldingWise.cs
+++ WealthERPSolution/UI/WealthERP/Reports/EquityHoldingWise.cs
@@ -114,9 +114,17 @@ namespace WealthERP.Reports {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section5 {
+        public CrystalDecisions.CrystalReports.Engine.Section PageFooterSection2 {
             get {
                 return this.ReportDefinition.Sections[8];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section Section5 {
+            get {
+                return this.ReportDefinition.Sections[9];
             }
         }
         
