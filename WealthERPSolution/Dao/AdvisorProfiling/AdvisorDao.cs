53403bd9766ff741fb4af5576ad840f6b7b3bbb6
AMP\baivab <AMP\baivab>
2012-09-06 16:55:55 +0530

    RE:6.5 Done Enhancement

--- WealthERPSolution/Dao/AdvisorProfiling/AdvisorDao.cs
+++ WealthERPSolution/Dao/AdvisorProfiling/AdvisorDao.cs
@@ -2402,16 +2402,16 @@ namespace DaoAdvisorProfiling
                             aoTALVo.AMCLinkTypeCode = int.Parse(dr["XLTY_LinkTypeCode"].ToString());
                             aoTALVo.AMCLinks = dr["AL_Link"].ToString();
                             aoTALVo.AMCImagePath = dr["WLM_LinkImagePath"].ToString();
-                            if(!string.IsNullOrEmpty(dr["WELM_LinkCode"].ToString()))
-                            {
-                                aoTALVo.ExternalLinkCode=dr["WELM_LinkCode"].ToString();
-                            }
+                            //if(!string.IsNullOrEmpty(dr["WELM_LinkCode"].ToString()))
+                            //{
+                            //    aoTALVo.ExternalLinkCode=dr["WELM_LinkCode"].ToString();
+                            //}
                             aoTALVo.WerpMasterlinkId = int.Parse(dr["WLM_Id"].ToString());
                             aoTALVo.IsAMCLinksWithPin= int.Parse(dr["AL_LinkWithPin"].ToString());
-                            //if (!string.IsNullOrEmpty(dr["AL_AltLinkName"].ToString()))
-                            //{
-                            //    aoTALVo.AltLinkName = dr["AL_AltLinkName"].ToString();
-                            //}
+                            if (!string.IsNullOrEmpty(dr["WLM_AltLinkName"].ToString()))
+                            {
+                                aoTALVo.AltLinkName = dr["WLM_AltLinkName"].ToString();
+                            }
                             adviserOTALink.Add(aoTALVo);
                         }
                     }
