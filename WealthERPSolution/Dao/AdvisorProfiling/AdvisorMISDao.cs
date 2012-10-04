75ee6c7df73d91b8ee007d0e1c81a245889f369a
AMP\gobinda <AMP\gobinda>
2012-10-04 18:08:28 +0530

    RE 6.6 : mis for AUM

--- WealthERPSolution/Dao/AdvisorProfiling/AdvisorMISDao.cs
+++ WealthERPSolution/Dao/AdvisorProfiling/AdvisorMISDao.cs
@@ -638,7 +638,330 @@ namespace DaoAdvisorProfiling
 
         }
 
+        public DataSet GetCustomerAMCSchemewiseAUMForAdviser(int adviserid, int branchid, int rmid, DateTime valuationDate)
+        {
 
+            Database db;
+            DbCommand getLoanMICmd;
+            DataSet AMCSchemewiseMIS = null;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getLoanMICmd = db.GetStoredProcCommand("SP_GetCustomerAMCSchemewiseAUMForAdviser");
+                db.AddInParameter(getLoanMICmd, "@A_AdviserId", DbType.Int32, adviserid);
+                if (branchid != 0)
+                    db.AddInParameter(getLoanMICmd, "@AB_BranchId", DbType.Int32, branchid);
+                else
+                    db.AddInParameter(getLoanMICmd, "@AB_BranchId", DbType.Int32, DBNull.Value);
+                if (rmid != 0)
+                    db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, rmid);
+                else
+                    db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, DBNull.Value);
+
+                db.AddInParameter(getLoanMICmd, "@Valuation_Date", DbType.DateTime, valuationDate);
+             
+                getLoanMICmd.CommandTimeout = 60 * 60;
+                AMCSchemewiseMIS = db.ExecuteDataSet(getLoanMICmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdvisorMFDao.cs:GetCustomerAMCSchemewiseMISForAdviser()");
+
+                object[] objects = new object[3];
+                objects[0] = rmid;
+                objects[1] = valuationDate;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return AMCSchemewiseMIS;
+
+        }
+
+        public DataSet GetCustomerAMCSchemewiseAUMForRM(int rmid, DateTime valuationDate)
+        {
+
+            Database db;
+            DbCommand getLoanMICmd;
+            DataSet AMCSchemewiseMIS = null;
+           
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getLoanMICmd = db.GetStoredProcCommand("SP_GetCustomerAMCSchemewiseAUMForRM");
+                db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, rmid);
+                db.AddInParameter(getLoanMICmd, "@Valuation_Date", DbType.DateTime, valuationDate);
+         
+                AMCSchemewiseMIS = db.ExecuteDataSet(getLoanMICmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdvisorMFDao.cs:GetCustomerAMCSchemewiseMISForRM()");
+
+                object[] objects = new object[3];
+                objects[0] = rmid;
+                objects[1] = valuationDate;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return AMCSchemewiseMIS;
+        }
+
+        public DataSet GetAMCSchemewiseAUMForRM(int rmid, DateTime valuationDate)
+        {
+            Database db;
+            DbCommand getLoanMICmd;
+            DataSet AMCSchemewiseMIS = null;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getLoanMICmd = db.GetStoredProcCommand("SP_GetAMCSchemewiseMISForRM");
+                db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, rmid);
+                db.AddInParameter(getLoanMICmd, "@Valuation_Date", DbType.DateTime, valuationDate);
+                AMCSchemewiseMIS = db.ExecuteDataSet(getLoanMICmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdvisorMFDao.cs:GetAMCSchemewiseMISForRM()");
+
+                object[] objects = new object[5];
+                objects[0] = rmid;
+                objects[1] = valuationDate;
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return AMCSchemewiseMIS;
+        }
+
+        public DataSet GetAMCwiseAUMForRM(int rmid, DateTime valuationDate)
+        {
+            Database db;
+            DbCommand getLoanMICmd;
+            DataSet AMCSchemewiseMIS = null;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getLoanMICmd = db.GetStoredProcCommand("SP_GetAMCwiseMISForRM");
+                db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, rmid);
+                db.AddInParameter(getLoanMICmd, "@Valuation_Date", DbType.DateTime, valuationDate); 
+                AMCSchemewiseMIS = db.ExecuteDataSet(getLoanMICmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdvisorMFDao.cs:GetAMCwiseMISForRM()");
+
+                object[] objects = new object[3];
+                objects[0] = rmid;
+                objects[1] = valuationDate;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return AMCSchemewiseMIS;
+        }
+
+        public DataSet GetAUMForBM(int rmid, int branchID, int branchHeadId, int XWise, int all, DateTime valuationDate)
+        {
+            Database db;
+            DbCommand getLoanMICmd;
+            DataSet AMCSchemewiseMIS = null;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getLoanMICmd = db.GetStoredProcCommand("SP_GetAllAMCwiseMISforBM");
+                db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, rmid);
+                db.AddInParameter(getLoanMICmd, "@BranchId", DbType.Int32, branchID);
+                db.AddInParameter(getLoanMICmd, "@BranchHeadId", DbType.Int32, branchHeadId);
+                db.AddInParameter(getLoanMICmd, "@all", DbType.Int32, all);
+                db.AddInParameter(getLoanMICmd, "@Valuation_Date", DbType.DateTime, valuationDate);
+                db.AddInParameter(getLoanMICmd, "@XWise", DbType.Int32, XWise);               
+                AMCSchemewiseMIS = db.ExecuteDataSet(getLoanMICmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdvisorMFDao.cs:GetMISForBM()");
+
+                object[] objects = new object[8];
+                objects[0] = rmid;
+                objects[1] = branchID;
+                objects[2] = branchHeadId;
+                objects[3] = all;
+                objects[4] = valuationDate;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return AMCSchemewiseMIS;
+        }
+
+        public DataSet GetAUMForBM(int rmId, int branchId, int branchHeadId, DateTime Valuationdate)
+        {
+            Database db;
+            DbCommand getLoanMICmd;
+            DataSet AMCSchemewiseMIS = null;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getLoanMICmd = db.GetStoredProcCommand("SP_GetAllAMCwiseAUMforBM");
+                db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, rmId);     
+                db.AddInParameter(getLoanMICmd, "@BranchId", DbType.Int32, branchId);
+                db.AddInParameter(getLoanMICmd, "@BranchHeadId", DbType.Int32, branchHeadId);
+                db.AddInParameter(getLoanMICmd, "@Valuation_Date", DbType.DateTime, Valuationdate);
+                AMCSchemewiseMIS = db.ExecuteDataSet(getLoanMICmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "AdvisorMFDao.cs:GetMISForBM()");
+                object[] objects = new object[8];
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return AMCSchemewiseMIS;
+
+        }
+        #region AUM MIS
+        
+        public DataSet GetAMCwiseAUMForAdviser(int adviserid, int branchid, int rmid, DateTime valuationDate)
+        {
+
+            Database db;
+            DbCommand getLoanMICmd;
+            DataSet AMCwiseAUMForAdviser = null;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getLoanMICmd = db.GetStoredProcCommand("SP_GetAMCwiseAUMForAdviser");
+                db.AddInParameter(getLoanMICmd, "@A_AdviserId", DbType.Int32, adviserid);
+                if (branchid != 0)
+                    db.AddInParameter(getLoanMICmd, "@AB_BranchId", DbType.Int32, branchid);
+                else
+                    db.AddInParameter(getLoanMICmd, "@AB_BranchId", DbType.Int32, DBNull.Value);
+                if (rmid != 0)
+                    db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, rmid);
+                else
+                    db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, DBNull.Value);
+
+                db.AddInParameter(getLoanMICmd, "@Valuation_Date", DbType.DateTime, valuationDate);
+
+                AMCwiseAUMForAdviser = db.ExecuteDataSet(getLoanMICmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdvisorMFDao.cs:GetAMCwiseMISForAdviser()");
+
+                object[] objects = new object[3];
+                objects[0] = rmid;
+                objects[1] = valuationDate;
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return AMCwiseAUMForAdviser;
+
+        }
+
+        public DataSet GetAMCSchemewiseAUMForAdviser(int adviserid, int branchid, int rmid, DateTime valuationDate)
+        {
+
+            Database db;
+            DbCommand getLoanMICmd;
+            DataSet AMCSchemewiseMIS = null;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                getLoanMICmd = db.GetStoredProcCommand("SP_GetAMCSchemewiseAUMForAdviser");
+                db.AddInParameter(getLoanMICmd, "@A_AdviserId", DbType.Int32, adviserid);
+                if (branchid != 0)
+                    db.AddInParameter(getLoanMICmd, "@AB_BranchId", DbType.Int32, branchid);
+                if (rmid != 0)
+                    db.AddInParameter(getLoanMICmd, "@RMId", DbType.Int32, rmid);
+                db.AddInParameter(getLoanMICmd, "@Valuation_Date", DbType.DateTime, valuationDate);
+                AMCSchemewiseMIS = db.ExecuteDataSet(getLoanMICmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "AdvisorMFDao.cs:GetAMCSchemewiseMISForAdviser()");
+
+                object[] objects = new object[5];
+                objects[0] = rmid;
+                objects[1] = valuationDate;
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return AMCSchemewiseMIS;
+        }
+
+        #endregion
         /* For BMMIS for Scheeme wise */
 
         public DataSet GetMISForBM(int rmid, int branchID, int branchHeadId, int XWise, int all, DateTime valuationDate, int amcCode, int schemeplanid, int CurrentPage, string AMCSearchVal, string SchemeSearchVal, string CustomerName, string FolioNum, string CategoryFilterVal, out int Count, int AllPageExportCount)
