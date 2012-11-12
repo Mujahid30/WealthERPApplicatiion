e683ceb7bc6eb75491824d82d1fe053096e19d78
AMP\Administrator:4 <AMP\Administrator:4>
2012-11-12 21:43:26 +0530

    RE:7.0-Equity Trade Date validation Implementation

--- WealthERPSolution/WERP_DAILY_EQUITY_VALUATION/EquityNetpositionProcessBo.cs
+++ WealthERPSolution/WERP_DAILY_EQUITY_VALUATION/EquityNetpositionProcessBo.cs
@@ -9,100 +9,106 @@ using VoCustomerPortfolio;
 using BoCustomerPortfolio;
 using Microsoft.ApplicationBlocks.ExceptionManagement;
 using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WERP_DAILY_EQUITY_VALUATION
 {
     public class EquityNetpositionProcessBo
     {
         AdviserMaintenanceBo adviserMaintenanceBo = new AdviserMaintenanceBo();
-        List<AdvisorVo> adviserVoList = new List<AdvisorVo>();
-        BoValuation.MFEngineBo.ValuationLabel valuationFor = BoValuation.MFEngineBo.ValuationLabel.Advisor;
+        List<AdvisorVo> adviserVoList = new List<AdvisorVo>();       
         MFEngineBo mfEngineBo = new MFEngineBo();
         CustomerPortfolioBo customerPortfolioBo = new CustomerPortfolioBo();
         List<int> customerList = new List<int>();
         List<CustomerPortfolioVo> customerPortfolioList = new List<CustomerPortfolioVo>();
         PortfolioBo portfolioBo = new PortfolioBo();
         List<EQPortfolioVo> eqPortfolioList = null;
+        bool isValuationDateTradeDate = false;
 
         public void CreateEQNetpositionForAllAdviser()
         {
             int adviserId = 0;
             int customerId = 0;
-            adviserVoList = adviserMaintenanceBo.GetAdviserList();
-            for (int i = 0; i < adviserVoList.Count; i++)
+            CheckForTradeDate(DateTime.Today.AddDays(-1));
+            if (isValuationDateTradeDate)
             {
-                adviserId = adviserVoList[i].advisorId;
-                int logId = 0;
-                logId = CreateAdviserEODLog("EQ", DateTime.Today.AddDays(-1), adviserId);
-                try
+                adviserVoList = adviserMaintenanceBo.GetAdviserList();
+                for (int i = 0; i < adviserVoList.Count; i++)
                 {
-                    customerList = customerPortfolioBo.GetAdviserCustomerList_EQ(adviserId);
-
-                    if (customerList != null && customerList.Count>0)
+                    adviserId = adviserVoList[i].advisorId;
+                    int logId = 0;
+                    logId = CreateAdviserEODLog("EQ", DateTime.Today.AddDays(-1), adviserId);
+                    try
                     {
-                        for (int j = 0; j < customerList.Count; j++)
-                        {
-                            customerId = customerList[j];
-                            try
-                            {
-                                customerPortfolioList = portfolioBo.GetCustomerPortfolios(customerId);
-                                customerPortfolioBo.DeleteEquityNetPosition(customerId, DateTime.Today.AddDays(-1));
-                                if (customerPortfolioList != null)
-                                {
-                                    for (int k = 0; k < customerPortfolioList.Count; k++)
-                                    {
-                                        eqPortfolioList = customerPortfolioBo.GetCustomerEquityPortfolio(customerId, customerPortfolioList[k].PortfolioId, DateTime.Today.AddDays(-1), string.Empty, string.Empty);
-                                        if (eqPortfolioList != null)
-                                        {
-                                            customerPortfolioBo.AddEquityNetPosition(eqPortfolioList, 1000);
+                        customerList = customerPortfolioBo.GetAdviserCustomerList_EQ(adviserId);
 
+                        if (customerList != null && customerList.Count > 0)
+                        {
+                            for (int j = 0; j < customerList.Count; j++)
+                            {
+                                customerId = customerList[j];
+                                try
+                                {
+                                    customerPortfolioList = portfolioBo.GetCustomerPortfolios(customerId);
+                                    customerPortfolioBo.DeleteEquityNetPosition(customerId, DateTime.Today.AddDays(-1));
+                                    if (customerPortfolioList != null)
+                                    {
+                                        for (int k = 0; k < customerPortfolioList.Count; k++)
+                                        {
+                                            eqPortfolioList = customerPortfolioBo.GetCustomerEquityPortfolio(customerId, customerPortfolioList[k].PortfolioId, DateTime.Today.AddDays(-1), string.Empty, string.Empty);
+                                            if (eqPortfolioList != null)
+                                            {
+                                                customerPortfolioBo.AddEquityNetPosition(eqPortfolioList, 1000);
+
+                                            }
                                         }
                                     }
                                 }
-                            }
-                            catch (BaseApplicationException Ex)
-                            {
+                                catch (BaseApplicationException Ex)
+                                {
 
-                                throw Ex;
-                            }
-                            catch (Exception Ex)
-                            {
-                                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
-                                NameValueCollection FunctionInfo = new NameValueCollection();
-                                FunctionInfo.Add("Method", "EquityNetpositionProcessBo:CreateEQNetpositionForAllAdviser()");
-                                object[] objects = new object[2];
-                                objects[0] = "EQ";
-                                objects[1] = customerId;                               
-                                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
-                                exBase.AdditionalInformation = FunctionInfo;
-                                ExceptionManager.Publish(exBase);
-                                throw exBase;
-                            }
-                                                      
+                                    throw Ex;
+                                }
+                                catch (Exception Ex)
+                                {
+                                    BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                                    NameValueCollection FunctionInfo = new NameValueCollection();
+                                    FunctionInfo.Add("Method", "EquityNetpositionProcessBo:CreateEQNetpositionForAllAdviser()");
+                                    object[] objects = new object[2];
+                                    objects[0] = "EQ";
+                                    objects[1] = customerId;
+                                    FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                                    exBase.AdditionalInformation = FunctionInfo;
+                                    ExceptionManager.Publish(exBase);
+                                    throw exBase;
+                                }
 
+
+                            }
+                            UpdateAdviserEODLog("EQ", 1, logId);
                         }
-                        UpdateAdviserEODLog("EQ", 1, logId);
                     }
-                }
-                catch (BaseApplicationException Ex)
-                {
+                    catch (BaseApplicationException Ex)
+                    {
 
-                    throw Ex;
-                }
-                catch (Exception Ex)
-                {
-                    BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
-                    NameValueCollection FunctionInfo = new NameValueCollection();
-                    FunctionInfo.Add("Method", "EquityNetpositionProcessBo:CreateEQNetpositionForAllAdviser()");
-                    object[] objects = new object[2];
-                    objects[0] = "EQ";
-                    objects[1] = adviserId;
-                    FunctionInfo = exBase.AddObject(FunctionInfo, objects);
-                    exBase.AdditionalInformation = FunctionInfo;
-                    ExceptionManager.Publish(exBase);
-                    throw exBase;
-                }
+                        throw Ex;
+                    }
+                    catch (Exception Ex)
+                    {
+                        BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                        NameValueCollection FunctionInfo = new NameValueCollection();
+                        FunctionInfo.Add("Method", "EquityNetpositionProcessBo:CreateEQNetpositionForAllAdviser()");
+                        object[] objects = new object[2];
+                        objects[0] = "EQ";
+                        objects[1] = adviserId;
+                        FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                        exBase.AdditionalInformation = FunctionInfo;
+                        ExceptionManager.Publish(exBase);
+                        throw exBase;
+                    }
 
+                }
             }
         }
 
@@ -172,6 +178,18 @@ namespace WERP_DAILY_EQUITY_VALUATION
                 exBase.AdditionalInformation = FunctionInfo;
                 ExceptionManager.Publish(exBase);
                 throw exBase;
+            }
+        }
+
+        protected void CheckForTradeDate(DateTime valuationDate)
+        {
+            SqlParameter[] Params = new SqlParameter[1];
+            Params[0] = new SqlParameter("@ValuationDate", valuationDate);
+            Params[0].DbType = DbType.DateTime;
+            DataSet DS = Utils.ExecuteDataSet("SPROC_CheckForTradeDate", Params);
+            if (DS.Tables[0].Rows.Count > 0)
+            {
+                isValuationDateTradeDate = true;
             }
         }
 
