8fabf5a8de85a7a92c53a2e44bbd530bc275718d
AMP\Administrator:4 <AMP\Administrator:4>
2012-12-26 18:03:11 +0530

    Reports and project added for Historical Valuation Queue

---
+++ WealthERPSolution/WERP_MF_Historical_ValuationQueue/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_MF_Historical_ValuationQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
