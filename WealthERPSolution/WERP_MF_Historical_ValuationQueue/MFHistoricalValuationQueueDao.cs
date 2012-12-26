21f62d7a7013556f7adca258521388a8d18c74f4
AMP\Administrator:4 <AMP\Administrator:4>
2012-12-26 19:35:25 +0530

    RE:7.2 Added Dao For MFHistoricalValuationQueue

---
+++ WealthERPSolution/WERP_MF_Historical_ValuationQueue/MFHistoricalValuationQueueDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_MF_Historical_ValuationQueue
+{
+    class MFHistoricalValuationQueueDao
+    {
+    }
+}
