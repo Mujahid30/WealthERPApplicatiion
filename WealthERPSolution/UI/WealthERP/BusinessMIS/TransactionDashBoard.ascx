4c7633d7984893ed27f6ebdff93981ece0f6e3be
AMP\Kirteeshree <AMP\Kirteeshree>
2013-01-07 20:29:36 +0530

    RE: 7.2 New DashBoard

--- WealthERPSolution/UI/WealthERP/BusinessMIS/TransactionDashBoard.ascx
+++ WealthERPSolution/UI/WealthERP/BusinessMIS/TransactionDashBoard.ascx
@@ -1,1 +1,94 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="TransactionDashBoard.ascx.cs" Inherits="WealthERP.BusinessMIS.TransactionDashBoard" %>
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" %>
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Charting" Assembly="Telerik.Web.UI" %>
+<asp:ScriptManager ID="scptMgr" runat="server" EnablePartialRendering="true">
+</asp:ScriptManager>
+
+<table width="100%">
+    <tr>
+        <td colspan="3">
+            <div class="divPageHeading">
+                <table cellspacing="0" cellpadding="3" width="100%">
+                    <tr>
+                        <td align="left">
+                            Transactions
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+    </tr>
+    </table>
+        <table width="100%">
+    <tr>
+    </tr>
+    <tr>
+    <td style="width: 15%;"></td>
+        <td style="width: 70%;">
+            <table width="100%">
+                <tr>
+                    <td>
+                        <asp:Repeater ID="rptTransationTree" runat="server" OnItemCommand="rptTransationTree_ItemCommand" OnItemDataBound="rptTransationTree_ItemDataBound">
+                            <ItemTemplate>
+                                <tr>
+                                    <td id="td1" runat="server" align="center">
+                                        <div class="divDashBoardMouseInLinks" onmouseover="this.className='divDashBoardMouseOutLinks'"
+                                            onmouseout="this.className='divDashBoardMouseInLinks'">
+                                            <asp:ImageButton ID="imgbtnTransation1" ImageUrl='<%# Eval("Path1").ToString() %>' runat="server"
+                                                Width="70px" CommandName="Tree_Navi_Row1" CommandArgument='<%# Eval("TreeNode1").ToString() %>' />
+                                            <br />
+                                            <asp:LinkButton ID="lnkTransationTreeNode1" CssClass="FieldName" Text='<%# Eval("TreeNodeText1").ToString() %>'
+                                                runat="server" Font-Underline="false" CommandName="Tree_Navi_Row1" CommandArgument='<%# Eval("TreeNode1").ToString() %>'>  
+                                            </asp:LinkButton>
+                                        </div>
+                                    </td>
+                                    <td id="td2" runat="server" align="center">
+                                        <div class="divDashBoardMouseInLinks" onmouseover="this.className='divDashBoardMouseOutLinks'"
+                                            onmouseout="this.className='divDashBoardMouseInLinks'">
+                                            <asp:ImageButton ID="imgbtnTransation2"  ImageUrl='<%# Eval("Path2").ToString() %>' runat="server"
+                                                Width="70px" CommandName="Tree_Navi_Row2" CommandArgument='<%# Eval("TreeNode2").ToString() %>' />
+                                            <br />
+                                            <asp:LinkButton ID="lnkTransationTreeNode2" Font-Underline="false" CssClass="FieldName" Text='<%# Eval("TreeNodeText2").ToString() %>'
+                                                runat="server" CommandName="Tree_Navi_Row2" CommandArgument='<%# Eval("TreeNode2").ToString() %>'>  
+                                            </asp:LinkButton>
+                                        </div>
+                                    </td>
+                                    <td id="td3" runat="server" align="center">
+                                        <div class="divDashBoardMouseInLinks" onmouseover="this.className='divDashBoardMouseOutLinks'"
+                                            onmouseout="this.className='divDashBoardMouseInLinks'">
+                                            <asp:ImageButton ID="imgbtnTransation3" ImageUrl='<%# Eval("Path3").ToString() %>' runat="server"
+                                                Width="70px" CommandName="Tree_Navi_Row3" CommandArgument='<%# Eval("TreeNode3").ToString() %>' />
+                                            <br />
+                                            <asp:LinkButton ID="lnkTransationTreeNode3" CssClass="FieldName" Font-Underline="false" Text='<%# Eval("TreeNodeText3").ToString() %>'
+                                                runat="server" CommandName="Tree_Navi_Row3" CommandArgument='<%# Eval("TreeNode3").ToString() %>'>  
+                                            </asp:LinkButton>
+                                        </div>
+                                    </td>
+                                    
+                                    <td id="td4" runat="server" align="center">
+                                        <div class="divDashBoardMouseInLinks" onmouseover="this.className='divDashBoardMouseOutLinks'"
+                                            onmouseout="this.className='divDashBoardMouseInLinks'">
+                                            <asp:ImageButton ID="imgbtnTransation4" ImageUrl='<%# Eval("Path4").ToString() %>' runat="server"
+                                                Width="70px" CommandName="Tree_Navi_Row4" CommandArgument='<%# Eval("TreeNode4").ToString() %>' />
+                                            <br />
+                                            <asp:LinkButton ID="lnkTransationTreeNode4" CssClass="FieldName" Text='<%# Eval("TreeNodeText4").ToString() %>'
+                                                runat="server" Font-Underline="false" CommandName="Tree_Navi_Row4" CommandArgument='<%# Eval("TreeNode4").ToString() %>'>  
+                                            </asp:LinkButton>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </ItemTemplate>
+                            <FooterTemplate>
+                               
+                            </FooterTemplate>
+                        </asp:Repeater>
+                    </td>
+                </tr>
+            </table>
+        </td>
+        <td style="width: 15%;"></td>
+    </tr>
+</table>
+
+
+
--- WealthERPSolution/UI/WealthERP/BusinessMIS/TransactionDashBoard.ascx.cs
+++ WealthERPSolution/UI/WealthERP/BusinessMIS/TransactionDashBoard.ascx.cs
@@ -4,14 +4,199 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BoUser;
+using VoUser;
+using WealthERP.Base;
+using BoCommon;
+using System.Configuration;
+using BoCustomerPortfolio;
+using System.Data;
 
 namespace WealthERP.BusinessMIS
 {
     public partial class TransactionDashBoard : System.Web.UI.UserControl
     {
+        UserBo userBo;
+        UserVo userVo;
+        AdvisorVo advisorVo;
+        string path;
         protected void Page_Load(object sender, EventArgs e)
         {
+            SessionBo.CheckSession();
+            userBo = new UserBo();
+            advisorVo = (AdvisorVo)Session[SessionContents.AdvisorVo];
+            userVo = (UserVo)Session[SessionContents.UserVo];
+            AssetBo assetBo = new AssetBo();
+            path = Server.MapPath(ConfigurationManager.AppSettings["xmllookuppath"].ToString());
+            if (!IsPostBack)
+            {
+                BindReptTransactionDashBoard();
+            }
+        }
 
+        private void BindReptTransactionDashBoard()
+        {
+            DataSet dsTreeNodes = new DataSet();
+            dsTreeNodes = XMLBo.GetSuperAdminTreeNodes(path);
+            DataRow[] drXmlTreeSubSubNode;
+            DataRow drTransationTreeNode;
+            DataTable dtTransationTreeNode = new DataTable();
+
+            dtTransationTreeNode.Columns.Add("TreeNode1", typeof(Int32));
+
+            dtTransationTreeNode.Columns.Add("TreeNodeText1", typeof(String));
+
+            dtTransationTreeNode.Columns.Add("Path1", typeof(String));
+
+            dtTransationTreeNode.Columns.Add("TreeNode2", typeof(Int32));
+
+            dtTransationTreeNode.Columns.Add("TreeNodeText2", typeof(String));
+
+            dtTransationTreeNode.Columns.Add("Path2", typeof(String));
+
+            dtTransationTreeNode.Columns.Add("TreeNode3", typeof(Int32));
+
+            dtTransationTreeNode.Columns.Add("TreeNodeText3", typeof(String));
+
+            dtTransationTreeNode.Columns.Add("Path3", typeof(String));
+
+            dtTransationTreeNode.Columns.Add("TreeNode4", typeof(Int32));
+
+            dtTransationTreeNode.Columns.Add("TreeNodeText4", typeof(String));
+
+            dtTransationTreeNode.Columns.Add("Path4", typeof(String));
+
+            //For Transaction 2014 is Tree Node Id in Sub Table in XML...
+            int treeSubNodeId = 2014;
+            drXmlTreeSubSubNode = dsTreeNodes.Tables[2].Select("TreeSubNodeCode=" + treeSubNodeId.ToString());
+
+            int count = 0;
+            drTransationTreeNode = dtTransationTreeNode.NewRow();
+            foreach (DataRow drSubSubNode in drXmlTreeSubSubNode)
+            {
+                if (count == 0)
+                {
+
+                    count++;
+                    drTransationTreeNode["TreeNode1"] = drSubSubNode["TreeSubSubNodeCode"].ToString();
+                    drTransationTreeNode["TreeNodeText1"] = drSubSubNode["TreeSubSubNodeText"].ToString();
+                    drTransationTreeNode["Path1"] = drSubSubNode["Path"].ToString();
+                    dtTransationTreeNode.Rows.Add(drTransationTreeNode);
+
+                }
+                else if (count == 1)
+                {
+                    count++;
+                    drTransationTreeNode["TreeNode2"] = drSubSubNode["TreeSubSubNodeCode"].ToString();
+                    drTransationTreeNode["TreeNodeText2"] = drSubSubNode["TreeSubSubNodeText"].ToString();
+                    drTransationTreeNode["Path2"] = drSubSubNode["Path"].ToString();
+
+                }
+                else if (count == 2)
+                {
+                    count++;
+                    drTransationTreeNode["TreeNode3"] = drSubSubNode["TreeSubSubNodeCode"].ToString();
+                    drTransationTreeNode["TreeNodeText3"] = drSubSubNode["TreeSubSubNodeText"].ToString();
+                    drTransationTreeNode["Path3"] = drSubSubNode["Path"].ToString();
+                }
+                else if (count == 3)
+                {
+                    count++;
+                    drTransationTreeNode["TreeNode4"] = drSubSubNode["TreeSubSubNodeCode"].ToString();
+                    drTransationTreeNode["TreeNodeText4"] = drSubSubNode["TreeSubSubNodeText"].ToString();
+                    drTransationTreeNode["Path4"] = drSubSubNode["Path"].ToString();
+                    count = 0;
+                    drTransationTreeNode = dtTransationTreeNode.NewRow();
+                }
+
+
+            }
+            rptTransationTree.DataSource = dtTransationTreeNode;
+            rptTransationTree.DataBind();
+        }
+        protected void rptTransationTree_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            ImageButton imgBtn1 = e.Item.FindControl("imgbtnTransation1") as ImageButton;
+            LinkButton lnkbtn1 = e.Item.FindControl("lnkTransationTreeNode1") as LinkButton;
+
+            ImageButton imgBtn2 = e.Item.FindControl("imgbtnTransation2") as ImageButton;
+            LinkButton lnkbtn2 = e.Item.FindControl("lnkTransationTreeNode2") as LinkButton;
+
+            ImageButton imgBtn3 = e.Item.FindControl("imgbtnTransation3") as ImageButton;
+            LinkButton lnkbtn3 = e.Item.FindControl("lnkTransationTreeNode3") as LinkButton;
+
+            ImageButton imgBtn4 = e.Item.FindControl("imgbtnTransation4") as ImageButton;
+            LinkButton lnkbtn4 = e.Item.FindControl("lnkTransationTreeNode4") as LinkButton;
+
+            if (e.CommandName == "Tree_Navi_Row1")
+            {
+
+                if (imgBtn1.CommandArgument == "3024" || lnkbtn1.CommandArgument == "3024")
+                {
+                    Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "LoadTransaction", "loadcontrol('RMMultipleTransactionView','login');", true);
+                }
+            }
+            if (e.CommandName == "Tree_Navi_Row2")
+            {
+
+                if (imgBtn2.CommandArgument == "3025" || lnkbtn2.CommandArgument == "3025")
+                {
+                    Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "pageloadscript", "loadcontrol('RMEQMultipleTransactionsView','login');", true);
+                }
+
+            }
+
+
+            //if (e.CommandName == "Tree_Navi_Row3")
+            //{
+            //    if (imgBtn3.CommandArgument == "3014" || lnkbtn3.CommandArgument == "3014")
+            //    {
+            //        Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "LoadUploads", "loadcontrol('EquityReports','login');", true);
+            //    }
+
+            //}
+            //if (e.CommandName == "Tree_Navi_Row4")
+            //{
+            //    if (imgBtn4.CommandArgument == "3015" || lnkbtn4.CommandArgument == "3015")
+            //    {
+            //        Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "LoadUploads", "loadcontrol('FPSectional','login');", true);
+            //    }
+
+            //}
+
+
+        }
+        protected void rptTransationTree_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+
+                ImageButton col0 = e.Item.FindControl("imgbtnTransation1") as ImageButton;
+                ImageButton col1 = e.Item.FindControl("imgbtnTransation2") as ImageButton;
+                ImageButton col2 = e.Item.FindControl("imgbtnTransation3") as ImageButton;
+                ImageButton col3 = e.Item.FindControl("imgbtnTransation4") as ImageButton;
+                if (col1.ImageUrl == "")
+                {
+                    var a = e.Item.FindControl("td2");
+                    a.Visible = false;
+                }
+                if (col0.ImageUrl == "")
+                {
+                    var a = e.Item.FindControl("td1");
+                    a.Visible = false;
+                }
+                if (col2.ImageUrl == "")
+                {
+                    var a = e.Item.FindControl("td3");
+                    a.Visible = false;
+                }
+                if (col3.ImageUrl == "")
+                {
+                    var a = e.Item.FindControl("td4");
+                    a.Visible = false;
+                }
+
+            }
         }
     }
 }
