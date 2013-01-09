760e81bfacf273b659faabca7997a8d1d9fb5589
AMP\Administrator:4 <AMP\Administrator:4>
2013-01-09 19:51:39 +0530

    RE:7.2-Adviser Preference UI setup related Enahncemnet Done

--- WealthERPSolution/Vo/AdvisorProfiling/AdvisorPreferenceVo.cs
+++ WealthERPSolution/Vo/AdvisorProfiling/AdvisorPreferenceVo.cs
@@ -8,10 +8,11 @@ namespace VoAdvisorProfiling
     public class AdvisorPreferenceVo
     {
         public string ValtPath { get; set; }
-        public bool IsLoginWidgetActive { get; set; }
+        public bool IsLoginWidgetEnable { get; set; }
         public string LoginWidgetLogOutPageURL { get; set; }
         public string BrowserTitleBarName { get; set; }
-        public string BrowserTitleBarIconImageName { get; set; }       
+        public string BrowserTitleBarIconImageName { get; set; }
+        public string WebSiteDomainName { get; set; }
         
     }
 }
