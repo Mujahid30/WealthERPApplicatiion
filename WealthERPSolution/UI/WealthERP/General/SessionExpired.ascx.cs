d6e1f8f11b30386bd582d0d15868b43dfa3da828
AMP\Administrator:4 <AMP\Administrator:4>
2013-01-11 17:13:26 +0530

    RE:7.2-Session Expired should go to Adviser LoginWidget Page,  New format implemented

--- WealthERPSolution/UI/WealthERP/General/SessionExpired.ascx.cs
+++ WealthERPSolution/UI/WealthERP/General/SessionExpired.ascx.cs
@@ -9,9 +9,42 @@ namespace WealthERP.General
 {
     public partial class SessionExpired : System.Web.UI.UserControl
     {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (Request.Cookies["UserPreference"] != null) 
+            {
+                // get the cookie
+                HttpCookie cookie = Request.Cookies["UserPreference"];
+                // get the cookie value
+                string userTheme = Request.Cookies["UserPreference"].Values["UserTheme"];
+                Page.Theme = userTheme;
+            }
+ 
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void lblSignOut_Click(object sender, EventArgs e)
+        {
+            if (Request.Cookies["UserPreference"] != null)
+            {
+                // get the cookie
+                HttpCookie cookie = Request.Cookies["UserPreference"];
+                // get the cookie value
+                string userLoginPageURL = Request.Cookies["UserPreference"].Values["UserLoginPageURL"];
+                if (!string.IsNullOrEmpty(userLoginPageURL))
+                {
+                    Session.Abandon();
+                    Response.Redirect(userLoginPageURL);
+                }
+                else
+                {
+                    Session.Abandon();
+                    Response.Redirect("https://app.wealtherp.com/");
+                }
+            }
         }
     }
 }
