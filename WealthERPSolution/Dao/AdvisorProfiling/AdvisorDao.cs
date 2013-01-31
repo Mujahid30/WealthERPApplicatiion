da81d92c373c821fe65bb0b31c3b91801926ee14
AMP\rajnikant <AMP\rajnikant>
2013-01-31 17:59:32 +0530

    RE 7.3.3 Add column processId in Advisercustomerlist

--- WealthERPSolution/Dao/AdvisorProfiling/AdvisorDao.cs
+++ WealthERPSolution/Dao/AdvisorProfiling/AdvisorDao.cs
@@ -1078,7 +1078,15 @@ namespace DaoAdvisorProfiling
                         customerVo = new CustomerVo();
 
                         customerVo.CustomerId = int.Parse(dr["C_CustomerId"].ToString());
-                        customerVo.ProcessId = int.Parse(dr["ADUL_ProcessId"].ToString());
+                        if (dr["ADUL_ProcessId"].ToString() == null && dr["ADUL_ProcessId"].ToString() == "")
+                        {
+                            customerVo.ProcessId = 0;
+
+                        }
+                        else
+                        {
+                            customerVo.ProcessId = int.Parse(dr["ADUL_ProcessId"].ToString());
+                        }
                         customerVo.FirstName = dr["C_FirstName"].ToString();
                         customerVo.UserId = int.Parse(dr["U_UMId"].ToString());
                         customerVo.MiddleName = dr["C_MiddleName"].ToString();
