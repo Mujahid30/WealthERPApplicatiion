3cdf24468c85a39465edeef83d981f238a413b1d
AMP\rajnikant <AMP\rajnikant>
2013-02-06 18:17:29 +0530

    RE7.3.3 fixed some bug in customerlist

--- WealthERPSolution/Dao/AdvisorProfiling/AdvisorDao.cs
+++ WealthERPSolution/Dao/AdvisorProfiling/AdvisorDao.cs
@@ -947,6 +947,15 @@ namespace DaoAdvisorProfiling
                         customerVo = new CustomerVo();
 
                         customerVo.CustomerId = int.Parse(dr["C_CustomerId"].ToString());
+                        if (dr["ADUL_ProcessId"].ToString() == null || dr["ADUL_ProcessId"].ToString() == "")
+                        {
+                            customerVo.ProcessId = 0;
+
+                        }
+                        else
+                        {
+                            customerVo.ProcessId = int.Parse(dr["ADUL_ProcessId"].ToString());
+                        }
                         customerVo.FirstName = dr["C_FirstName"].ToString();
                         customerVo.UserId = int.Parse(dr["U_UMId"].ToString());
                         customerVo.MiddleName = dr["C_MiddleName"].ToString();
