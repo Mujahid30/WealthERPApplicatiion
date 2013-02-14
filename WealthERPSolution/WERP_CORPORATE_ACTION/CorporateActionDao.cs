a634ae0611cc5174e7f28bd7a3678bde0ad736f8
AMP\Administrator:4 <AMP\Administrator:4>
2013-02-14 13:12:26 +0530

    RE 7.4 : Project Added for Corporate Action

---
+++ WealthERPSolution/WERP_CORPORATE_ACTION/CorporateActionDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_CORPORATE_ACTION
+{
+    class CorporateActionDao
+    {
+    }
+}
---
+++ WealthERPSolution/WERP_CORPORATE_ACTION/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_CORPORATE_ACTION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
