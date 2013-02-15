c5f379a647c7a47bc7aa673a2e2115f8e5a11345
AMP\Administrator:4 <AMP\Administrator:4>
2013-02-15 17:38:17 +0530

    RE:7.3.2-PATCH-Solved Transaction export date fields hide ..

--- WealthERPSolution/UI/WealthERP/CustomerPortfolio/RMMultipleTransactionView.ascx.cs
+++ WealthERPSolution/UI/WealthERP/CustomerPortfolio/RMMultipleTransactionView.ascx.cs
@@ -166,6 +166,7 @@ namespace WealthERP.CustomerPortfolio
                         PasssedFolioValue = Convert.ToInt32(ht["Account"].ToString());
                         BindGrid(DateTime.Parse((txtFromDate.SelectedDate).ToString()), DateTime.Parse((txtToDate.SelectedDate).ToString()));
                         Session.Remove("tranDates");
+                        hdnExportType.Value = "TV";
                     }
 
                     //if (Session["tranDates"] != null)
