b6a7372403f10ebf4587864d23dd71dbff53e3d2
AMP\Administrator:4 <AMP\Administrator:4>
2013-03-11 14:37:50 +0530

    RE:7.4-Bo Functinality Added

--- WealthERPSolution/BoCommisionManagement/CommisionReceivableBo.cs
+++ WealthERPSolution/BoCommisionManagement/CommisionReceivableBo.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using System.Data;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using VoCommisionManagement;
+using DaoCommisionManagement;
 
 namespace BoCommisionManagement
 {
-    class CommisionReceivableBo
+    public class CommisionReceivableBo
     {
+
+        public DataSet GetLookupDataForReceivableSetUP(int adviserId)
+        {
+            CommisionReceivableDao commisionReceivableDao = new CommisionReceivableDao();
+            DataSet dsLookupData;
+            try
+            {
+                dsLookupData = commisionReceivableDao.GetLookupDataForReceivableSetUP(adviserId);
+                
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "CommisionReceivableBo.cs:GetLookupDataForReceivableSetUP(int adviserId)");
+                object[] objects = new object[1];
+                objects[0] = adviserId;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return dsLookupData;
+        }
     }
 }
