586690e0140c730b1c10729f0c8969c2c44924f9
AMP\Administrator:4 <AMP\Administrator:4>
2013-03-11 14:32:30 +0530

    RE:7.4-Bulk Report Creation

---
+++ WealthERPSolution/WERP_BULK_REPORT_GENERATION_QUEUE/BulkReportRenerationBo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_BULK_REPORT_GENERATION_QUEUE
+{
+    public class BulkReportRenerationBo
+    {
+    }
+}
---
+++ WealthERPSolution/WERP_BULK_REPORT_GENERATION_QUEUE/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_BULK_REPORT_GENERATION_QUEUE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
