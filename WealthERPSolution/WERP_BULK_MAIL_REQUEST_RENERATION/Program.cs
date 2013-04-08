1756ea5db929be2f1bd220680717aa64c18a7934
AMP\Administrator:4 <AMP\Administrator:4>
2013-04-08 10:05:47 +0530

    RE:7.5:Added New Project for Bulk mail Request generation engine

---
+++ WealthERPSolution/WERP_BULK_MAIL_REQUEST_RENERATION/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_BULK_MAIL_REQUEST_RENERATION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
