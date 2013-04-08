511220aa9d110c7747e693b0b81c1a1a48e0778f
AMP\Administrator:4 <AMP\Administrator:4>
2013-04-08 10:21:19 +0530

    RE:7.5-Added new class for all common lookup

---
+++ WealthERPSolution/Bo/Common/BoCommon/CommonLookupBo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DaoCommon;
+using System.Collections.Specialized;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Data;
+
+
+namespace BoCommon
+{
+    public class CommonLookupBo
+    {
+
+        public DataTable GetMFInstrumentSubCategory(string categoryCode)
+        {
+            CommonLookupDao commonLookupDao=new CommonLookupDao();
+            DataTable dtSubCategory;
+
+            try
+            {
+                dtSubCategory = commonLookupDao.GetMFInstrumentSubCategory(categoryCode);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "CommonLookupBo.cs:GetMFInstrumentSubCategory(string categoryCode)");
+                object[] objects = new object[1];
+                objects[0] = categoryCode;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return dtSubCategory;
+        }
+    }
+}
