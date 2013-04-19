cb5b5a35f1151f344f914c5e659f5c43951af373
AMP\Administrator:4 <AMP\Administrator:4>
2013-04-19 10:24:27 +0530

    Testing

--- WealthERPSolution/WealthERPDB/WealthERPDB/Change Scripts/Stored Procedures/SQL_Alter_SP_GetUser.sql
+++ WealthERPSolution/WealthERPDB/WealthERPDB/Change Scripts/Stored Procedures/SQL_Alter_SP_GetUser.sql
@@ -3,9 +3,28 @@
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-ALTER procedure [dbo].[SP_GetUser]
-@U_LoginId varchar(50)
-as
-select * from [User] where U_LoginId=@U_LoginId
- 
- Seelct * from [User] WHERE 1=2
+ALTER procedure [dbo].[SP_GetUser]  
+@U_LoginId varchar(50)  
+as  
+  
+SELECT [U_UserId]  
+      ,[U_Password]  
+      ,[U_FirstName]  
+      ,[U_MiddleName]  
+      ,[U_LastName]  
+      ,[U_Email]  
+      ,[U_UserType]  
+      ,[U_LoginId]  
+      ,[U_CreatedBy]  
+      ,[U_ModifiedBy]  
+      ,[U_CreatedOn]  
+      ,[U_ModifiedOn]  
+      ,[U_IsTempPassword]  
+      ,[U_PwdSaltValue]  
+      ,[U_Theme]  
+      ,dbo.Fn_GetAdvisorRMRoleList(U.U_UserId) RoleList  
+      ,(SELECT COUNT(MR_MessageRecipientId) FROM MessageRecipients WHERE MR_RecipientId = [U_UserId] AND MR_ReadByUser = 0) AS UnreadMessages
+      ,dbo.Fn_GetStaffPermissionList(U_UserId) AS PermissionList    
+  FROM [User] U where U_LoginId=@U_LoginId  
+  
+SELECT * FROM [USER] where 1=2
