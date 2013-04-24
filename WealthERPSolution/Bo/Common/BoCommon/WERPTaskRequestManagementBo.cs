15261b321d0b1bd019b780ab93cdce55f7a5ded9
AMP\Administrator:4 <AMP\Administrator:4>
2013-04-24 18:06:05 +0530

    RE:7.7-Report Request Queue implementation

---
+++ WealthERPSolution/Bo/Common/BoCommon/WERPTaskRequestManagementBo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DaoCommon;
+using System.Data;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using VoCommon;
+using VoReports;
+
+
+namespace BoCommon
+{
+    public class WERPTaskRequestManagementBo
+    {
+        /// <summary>
+        /// Create Task Request returns RequestId
+        /// </summary>
+        /// <param name="portfolioIDs"></param>
+        /// <param name="subreportype"></param>
+        /// <param name="fromDate"></param>
+        /// <returns></returns>
+        public void CreateTaskRequest(int taskId, int userId, out int taskRequestId)
+        {
+            WERPTaskRequestManagementDao requestManagementDao = new WERPTaskRequestManagementDao();
+
+            try
+            {
+                requestManagementDao.CreateTaskRequest(taskId, userId, out taskRequestId); 
+
+            }
+            catch (BaseApplicationException ex)
+            {
+                throw (ex);
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "WERPTaskRequestManagementBo.cs:CreateTaskRequest()");
+                object[] objects = new object[2];
+                objects[0] = taskId;
+                objects[1] = userId;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+
+        }
+
+        /// <summary>
+        /// Create Bulk Report request to Queue
+        /// </summary>
+        /// <param name="portfolioIDs"></param>
+        /// <param name="subreportype"></param>
+        /// <param name="fromDate"></param>
+        /// <returns></returns>
+        public void CreateBulkReportRequest(List<MFReportVo> mfReportVoList, int parentRequestId, int taskId, int userId)
+        {
+            WERPTaskRequestManagementDao requestManagementDao = new WERPTaskRequestManagementDao();
+            try
+            {
+                requestManagementDao.CreateBulkReportRequest(mfReportVoList, parentRequestId, taskId, userId);
+
+            }
+            catch (BaseApplicationException ex)
+            {
+                throw (ex);
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "WERPTaskRequestManagementBo.cs:CreateBulkReportRequest()");
+                object[] objects = new object[3];
+                objects[0] = taskId;
+                objects[1] = userId;
+                objects[2] = parentRequestId;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+    }
+}
