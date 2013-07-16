ad078cc2627a22a73ed483bcd4607fb88f7d2975
AMP\Administrator:4 <AMP\Administrator:4>
2013-07-16 14:56:20 +0530

    RE:SBI-work in progress commission rule setup

--- WealthERPSolution/VoCommisionManagement/CommisionReceivableVo.cs
+++ WealthERPSolution/VoCommisionManagement/CommisionReceivableVo.cs
@@ -5,62 +5,71 @@ using System.Text;
 
 namespace VoCommisionManagement
 {
-    public class ReceivableStructureMasterVo
+    public class CommissionStructureMasterVo
     {
-        public string CommissionTypeCode { get; set; }
-        //public bool IsMonetary { get; set; }
         public int AdviserId { get; set; }
-        public string ApplicableLEvelCode { get; set; }
-        public string AssetGroupCode { get; set; }
-        public bool IsArchived { get; set; }
-        public DateTime ArchivedOn { get; set; }
+        public string ProductType { get; set; }
+        //public bool IsMonetary { get; set; }
+        public string AssetCategory { get; set; }
+        public string Issuer { get; set; }
+        public string ApplicableLevelCode { get; set; }
 
-        public bool IsServicetaxReduced { get; set; }
+        public DateTime ValidityStartDate { get; set; }
+        public DateTime ValidityEndDate { get; set; }
+
+        public bool IsServiceTaxReduced { get; set; }
         public bool IsTDSReduced { get; set; }
-        public bool IsOtherTAxReduced { get; set; }
+        public bool IsOtherTaxReduced { get; set; }
+        public bool IsClawBackApplicable { get; set; }
+
+        public bool IsArchived { get; set; }
+        public DateTime ArchivedOn { get; set; }    
 
         public string CommissionStructureName { get; set; }
         public bool IsNonMonetaryReward { get; set; }
+       
         public string ReceivableFrequency { get; set; }
+        public string StructureNote { get; set; }
 
-        public string RecurringiSIPFrequency { get; set; }
-        public bool IsStructureFromIssuer { get; set; }
-        public string Note { get; set; }
+        public int StructureMasterCreatedBy { get; set; }
+        public int StructureMasterModifiedBy { get; set; }
+        public DateTime StructureMasterCreatedOn { get; set; }
+        public DateTime StructureMasterModifiedOn { get; set; }
+        public StringBuilder AssetSubCategory { get; set; }
 
-        public int MasterCreatedBy { get; set; }
-        public int MasterModifiedBy { get; set; }
-        public DateTime MasterCreatedOn { get; set; }
-        public DateTime MasterModifiedOn { get; set; }
+        public Dictionary<string, string> AssetSubCategoryDict { get; set; }
 
     }
 
-    public class ReceivableStructureRuleVo : ReceivableStructureMasterVo
+    public class CommissionStructureRuleVo : CommissionStructureMasterVo
     {
+        public Int32 CommissionStructureId { get; set; }
+        public string CommissionType { get; set; }
+        public string CustomerType { get; set; }
+
+        public decimal MinInvestmentAmount { get; set; }
+        public decimal MaxInvestmentAmount { get; set; }
+
+        public int TenureMin { get; set; }
+        public int TenureMax { get; set; }
+        public string TenureUnit { get; set; }
+
+        public int MinInvestmentAge { get; set; }
+        public int MaxInvestmentAge { get; set; }
+        public string InvestmentAgeUnit { get; set; }
+
+        public string TransactionType { get; set; }
+        public Int32 MinNumberofApplications { get; set; }
+
+        public decimal BrokerageValue { get; set; }
+        public string BrokerageUnitCode { get; set; }
+        
         public string CalculatedOnCode { get; set; }
         public string AUMFrequency { get; set; }
         public decimal AUMMonth { get; set; }
-        public decimal MinInvestmentAmount { get; set; }
 
-        public decimal MaxInvestmentAmount { get; set; }
-        public int TenureMin { get; set; }
-        public int TenureMax { get; set; }
-        public Int64 TenureUnit { get; set; }
-
-        public string CustomerType { get; set; }
-        public string TransType { get; set; }
-        public Int32 MinNumberofApplications { get; set; }
-        public decimal BrokerageValue { get; set; }
-
-        public string UnitCode { get; set; }
-        public int MinInvestmentAge { get; set; }
-        public int MaxInvestmentAge { get; set; }
-        public int InvestmentAgeUnit { get; set; }
-       
-        public string CityGroupCode { get; set; }
-        public string BranchCategory { get; set; }
-
-        public DateTime RuleValidityStartDate { get; set; }
-        public DateTime RuleValidityEndDate { get; set; }
+        //public DateTime RuleValidityStartDate { get; set; }
+        //public DateTime RuleValidityEndDate { get; set; }
 
         public int RuleCreatedBy { get; set; }
         public int RuleModifiedBy { get; set; }
