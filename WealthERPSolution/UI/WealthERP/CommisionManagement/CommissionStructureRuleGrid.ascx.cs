220bafcdee808b8668ea77bf10c20f88a84a3b84
AMP\kbajpai <AMP\kbajpai>
2013-07-19 17:07:14 +0530

    RE:SBI- CommissionStructure grid implementation done

--- WealthERPSolution/UI/WealthERP/CommisionManagement/CommissionStructureRuleGrid.ascx.cs
+++ WealthERPSolution/UI/WealthERP/CommisionManagement/CommissionStructureRuleGrid.ascx.cs
@@ -4,14 +4,180 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BoUser;
+using VoUser;
+using WealthERP.Base;
+using BoCommon;
+using BoCommisionManagement;
+using VoCommisionManagement;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using Telerik.Web.UI;
+using System.Text;
+using System.Data;
 
 namespace WealthERP.CommisionManagement
 {
     public partial class CommissionStructureRuleGrid : System.Web.UI.UserControl
     {
+        UserVo userVo;
+        AdvisorVo advisorVo;
+        RMVo rmVo;
+        CommisionReceivableBo commisionReceivableBo = new CommisionReceivableBo();
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+            SessionBo.CheckSession();
+            userVo = (UserVo)Session[SessionContents.UserVo];
+            advisorVo = (AdvisorVo)Session["advisorVo"];
+            rmVo = (RMVo)Session["rmVo"];
+
+            if (!IsPostBack) {
+                pnlGrid.Visible = false;
+
+                BindProductDropdown();
+                ddProduct.SelectedValue = "MF";
+                ddProduct.Enabled = false;
+
+                BindCategoryDropdown(ddProduct.SelectedValue);
+                BindIssuerDropdown();
+            }
+            
+        }
+        
+        private void bindDropdown(DropDownList ddList, DataSet dsListItems) {
+
+        }
+
+        private void BindProductDropdown()
+        {
+            DataSet dsLookupData = commisionReceivableBo.GetProductType();
+
+            //Populating the product dropdown
+            DataRow drProduct = dsLookupData.Tables[0].NewRow();
+            drProduct["PAG_AssetGroupCode"] = "All";
+            drProduct["PAG_AssetGroupName"] = "All";
+            dsLookupData.Tables[0].Rows.InsertAt(drProduct, 0);
+            ddProduct.DataSource = dsLookupData.Tables[0];
+            ddProduct.DataValueField = dsLookupData.Tables[0].Columns["PAG_AssetGroupCode"].ToString();
+            ddProduct.DataTextField = dsLookupData.Tables[0].Columns["PAG_AssetGroupName"].ToString();
+            ddProduct.DataBind();
+        }
+
+        private void BindCategoryDropdown(string prodType)
+        {
+            DataSet dsLookupData = commisionReceivableBo.GetCategories(prodType);
+
+            //Populating the categories dropdown
+            DataRow drProduct = dsLookupData.Tables[0].NewRow();
+            drProduct["PAIC_AssetInstrumentCategoryCode"] = "All";
+            drProduct["PAIC_AssetInstrumentCategoryName"] = "All";
+            dsLookupData.Tables[0].Rows.InsertAt(drProduct, 0);
+            ddCategory.DataSource = dsLookupData.Tables[0];
+            ddCategory.DataValueField = dsLookupData.Tables[0].Columns["PAIC_AssetInstrumentCategoryCode"].ToString();
+            ddCategory.DataTextField = dsLookupData.Tables[0].Columns["PAIC_AssetInstrumentCategoryName"].ToString();
+            ddCategory.DataBind();
+        }
+
+        private void BindSubCategoryDropdown(string cat)
+        {
+            DataSet dsLookupData = commisionReceivableBo.GetSubCategories(cat);
+
+            //Populating the categories dropdown
+            DataRow drProduct = dsLookupData.Tables[0].NewRow();
+            drProduct["PAISC_AssetInstrumentSubCategoryCode"] = "All";
+            drProduct["PAISC_AssetInstrumentSubCategoryName"] = "All";
+            dsLookupData.Tables[0].Rows.InsertAt(drProduct, 0);
+            ddSubCategory.DataSource = dsLookupData.Tables[0];
+            ddSubCategory.DataValueField = dsLookupData.Tables[0].Columns["PAISC_AssetInstrumentSubCategoryCode"].ToString();
+            ddSubCategory.DataTextField = dsLookupData.Tables[0].Columns["PAISC_AssetInstrumentSubCategoryName"].ToString();
+            ddSubCategory.DataBind();
+        }
+
+        private void BindIssuerDropdown() 
+        {
+            DataSet dsLookupData = commisionReceivableBo.GetProdAmc();
+
+            //Populating the categories dropdown
+            DataRow drProduct = dsLookupData.Tables[0].NewRow();
+            drProduct["PA_AMCCode"] = 0;
+            drProduct["PA_AMCName"] = "All";
+            dsLookupData.Tables[0].Rows.InsertAt(drProduct, 0);
+            ddIssuer.DataSource = dsLookupData.Tables[0];
+            ddIssuer.DataValueField = dsLookupData.Tables[0].Columns["PA_AMCCode"].ToString();
+            ddIssuer.DataTextField = dsLookupData.Tables[0].Columns["PA_AMCName"].ToString();
+            ddIssuer.DataBind();
+        }
+
+        protected void ddProduct_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BindCategoryDropdown(ddProduct.SelectedValue);
+        }
+
+        protected void ddCategory_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            BindSubCategoryDropdown(ddCategory.SelectedValue);
+            ddSubCategory.Enabled = true;
+        }
+
+        protected void BindStructureRuleGrid()
+        {
+            DataSet dsStructureRules = commisionReceivableBo.GetAdviserCommissionStructureRules(advisorVo.advisorId, 
+                                                ddProduct.SelectedValue.ToLower(), ddCategory.SelectedValue.ToLower(), 
+                                                ddSubCategory.SelectedValue.ToLower(), int.Parse(ddIssuer.SelectedValue), ddStatus.SelectedValue.ToLower());
+            gvCommMgmt.DataSource = dsStructureRules.Tables[0];
+            //if (dsStructureRules.Tables[0].Rows.Count <= gvCommMgmt.PageSize)
+            //    gvCommMgmt.AllowPaging = false;
+            gvCommMgmt.DataBind();
+            Cache.Insert(userVo.UserId.ToString() + "CommissionStructureRule", dsStructureRules.Tables[0]);
+        }
+
+        protected void gvCommMgmt_OnNeedDataSource(object sender, GridNeedDataSourceEventArgs e)
+        {
+            DataSet dsCommissionStructureRule = new DataSet();
+            if (Cache[userVo.UserId.ToString() + "CommissionStructureRule"] != null)
+            {
+                dsCommissionStructureRule = (DataSet)Cache[userVo.UserId.ToString() + "CommissionStructureRule"];
+                gvCommMgmt.DataSource = dsCommissionStructureRule.Tables[0];
+            }
+        }
+
+        protected void btnGo_Click(object sender, EventArgs e)
+        {
+            BindStructureRuleGrid();
+            //gvCommMgmt.Visible = true;
+            pnlGrid.Visible = true;
+        }
+
+        protected void ibtExportSummary_OnClick(object sender, ImageClickEventArgs e)
+        {
+            DataTable dtCommMgmt = new DataTable();
+            dtCommMgmt = (DataTable)Cache[userVo.UserId.ToString() + "CommissionStructureRule"];
+            if (dtCommMgmt == null)
+                return;
+            else if (dtCommMgmt.Rows.Count < 1)
+                return;
+            gvCommMgmt.DataSource = dtCommMgmt;
+            gvCommMgmt.ExportSettings.OpenInNewWindow = true;
+            gvCommMgmt.ExportSettings.IgnorePaging = true;
+            gvCommMgmt.ExportSettings.HideStructureColumns = true;
+            gvCommMgmt.ExportSettings.ExportOnlyData = true;
+            gvCommMgmt.ExportSettings.FileName = "CommissionStructureRule";
+            gvCommMgmt.ExportSettings.Excel.Format = GridExcelExportFormat.ExcelML;
+            gvCommMgmt.MasterTableView.ExportToExcel();
+            //BindStructureRuleGrid();
+        }
+
+        protected void ddAction_OnSelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+        protected void gvCommMgmt_PageIndexChanged(object sender, GridPageChangedEventArgs e)
+        {
+            gvCommMgmt.CurrentPageIndex = e.NewPageIndex;
+            BindStructureRuleGrid();
+        }
+
     }
 }
