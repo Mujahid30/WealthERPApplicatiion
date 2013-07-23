e34a6cbb2ed99923383b6e981bf1927ff7e8396b
AMP\Kirteeshree <AMP\Kirteeshree>
2013-07-23 12:55:53 +0530

    RE: SBI- Associate  Code generation

--- WealthERPSolution/UI/WealthERP/Associates/AddBranchRMAgentAssociation.ascx
+++ WealthERPSolution/UI/WealthERP/Associates/AddBranchRMAgentAssociation.ascx
@@ -1,1 +1,88 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AddBranchRMAgentAssociation.ascx.cs" Inherits="WealthERP.Associates.AddBranchRMAgentAssociation" %>
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AddBranchRMAgentAssociation.ascx.cs"
+    Inherits="WealthERP.Associates.AddBranchRMAgentAssociation" %>
+<telerik:RadScriptManager ID="scptMgr" runat="server">
+</telerik:RadScriptManager>
+<table width="100%">
+    <tr>
+        <td colspan="3" style="width: 100%;">
+            <div class="divPageHeading">
+                <table cellspacing="0" width="100%">
+                    <tr>
+                        <td align="left">
+                            Add Associates/Agent Code
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+    </tr>
+</table>
+<%--<table width="100%">
+    <tr>
+        <td colspan="6">
+            <div class="divSectionHeading" style="vertical-align: text-bottom">
+                Step-3
+            </div>
+        </td>
+    </tr>
+</table>--%>
+<table width="60%">
+    <tr>
+        <td align="leftField">
+            &nbsp;
+        </td>
+        <%--        <td class="rightField">
+            <asp:RadioButton ID="rbtBM" Class="cmbField" runat="server" GroupName="BMRMAgent"   AutoPostBack="true"
+                Checked="True" Text="BM" oncheckedchanged="rbtBM_CheckedChanged"  /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="rbtRM" Class="cmbField" runat="server" GroupName="BMRMAgent"  AutoPostBack="true"
+                Text="RM" oncheckedchanged="rbtRM_CheckedChanged" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="rbtnAgent" Class="cmbField" runat="server" GroupName="BMRMAgent"  AutoPostBack="true"
+                Text="Agent" oncheckedchanged="rbtnAgent_CheckedChanged" />
+        </td>--%>
+    </tr>
+    <tr>
+        <td class="leftField">
+            <asp:Label ID="Label1" runat="server" CssClass="FieldName" Text="Generate code for:"></asp:Label>
+        </td>
+        <td class="rightField">
+            <asp:DropDownList ID="ddlUserType" runat="server" CssClass="cmbField" 
+                onselectedindexchanged="ddlUserType_SelectedIndexChanged">
+            <asp:ListItem Text="Select" Value="Select" Selected="True"></asp:ListItem>
+            <asp:ListItem Text="BM" Value="BM" ></asp:ListItem>
+            <asp:ListItem Text="RM" Value="RM" ></asp:ListItem>
+            <asp:ListItem Text="Associates" Value="Associates" ></asp:ListItem>
+            </asp:DropDownList>
+        </td>
+        <td class="leftField">
+            <asp:Label ID="lblSelectType" runat="server" CssClass="FieldName" Text="Select:"></asp:Label>
+        </td>
+        <td class="rightField">
+            <asp:DropDownList ID="ddlSelectType" runat="server" CssClass="cmbField">
+            </asp:DropDownList>
+        </td>
+    </tr>
+        <tr>
+        <td class="leftField">
+            <asp:Label ID="lblNoOfCode" CssClass="FieldName" runat="server" Text="No. of code to be generated:"></asp:Label>
+        </td>
+        <td class="rightField">
+            <asp:TextBox ID="txtNoOfCode" runat="server" CssClass="txtField" Text="1"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td class="leftField">
+            <asp:Label ID="lblAgentCode" CssClass="FieldName" runat="server" Text="Agent Code:"></asp:Label>
+        </td>
+        <td class="rightField">
+            <asp:TextBox ID="txtAgentCode" runat="server" CssClass="txtField"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td class="leftField">
+            &nbsp;
+        </td>
+        <td class="rightField">
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" CssClass="PCGButton" OnClick="btnSubmit_Click" />
+        </td>
+    </tr>
+</table>
--- WealthERPSolution/UI/WealthERP/Associates/AddBranchRMAgentAssociation.ascx.cs
+++ WealthERPSolution/UI/WealthERP/Associates/AddBranchRMAgentAssociation.ascx.cs
@@ -4,14 +4,220 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+using WealthERP.Base;
+using BoUploads;
+using VoUser;
+using BoCommon;
+using VOAssociates;
+using BOAssociates;
+using BoAdvisorProfiling;
 
 namespace WealthERP.Associates
 {
     public partial class AddBranchRMAgentAssociation : System.Web.UI.UserControl
     {
+        AdvisorVo advisorVo = new AdvisorVo();
+        AssociatesVO associatesVo = new AssociatesVO();
+        AssociatesBo associatesBo = new AssociatesBo();
+        UserVo userVo = new UserVo();
+        RMVo rmVo = new RMVo();
+        String userType;
+
+        string agentCode=string.Empty;
+        int agentId = 0;
+        int associationId;
         protected void Page_Load(object sender, EventArgs e)
         {
+            advisorVo = (AdvisorVo)Session["advisorVo"];
+            rmVo = (RMVo)Session[SessionContents.RmVo];
+            if (Session[SessionContents.CurrentUserRole].ToString().ToLower() == "ops")
+                userType = "advisor";
+            associatesVo = (AssociatesVO)Session["associatesVo"];
+            if(!IsPostBack)
+            {
+                if (Request.QueryString["AgentId"] != null)
+                {
+                    agentCode = Request.QueryString["AgentId"];
+                    txtAgentCode.Text = agentCode;
+                }
+                else
+                {
+                    agentId = associatesBo.GetAgentId();
+                    agentId = agentId + 1;
+                    agentCode = agentId.ToString();
+                    txtAgentCode.Text = agentCode;
+                }
+                if (Request.QueryString["AssociationId"] != null)
+                {
+                    BindAgentList();
+                    associationId = int.Parse(Request.QueryString["AssociationId"]);
+                    ddlUserType.SelectedValue = "Associates";
+                    ddlSelectType.SelectedValue = associationId.ToString();
+                }
+            }
+        }
 
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool result = false;
+            if (Request.QueryString["AgentId"] != null)
+            {
+                agentId = int.Parse(Request.QueryString["AgentId"]);
+                associatesVo.AAC_AdviserAgentId = agentId;
+            }
+            else
+                agentId = associatesVo.AAC_AdviserAgentId;
+            if (ddlUserType.SelectedIndex != 0)
+            {
+                if (ddlUserType.SelectedValue == "BM")
+                {
+                    if (ddlSelectType.SelectedIndex!=0)
+                         associatesVo.BranchId = int.Parse(ddlSelectType.SelectedValue);
+                }
+                else if (ddlUserType.SelectedValue == "RM")
+                {
+                    if (ddlSelectType.SelectedIndex != 0)
+                        associatesVo.RMId = int.Parse(ddlSelectType.SelectedValue);
+                }
+                else if (ddlUserType.SelectedValue == "Associates")
+                {
+                    if (ddlSelectType.SelectedIndex != 0)
+                    {
+                        associatesVo.AdviserAssociateId = int.Parse(ddlSelectType.SelectedValue);
+                        associatesVo.ContactPersonName = ddlSelectType.SelectedItem.Text;
+                    }
+                }
+
+            }
+            //if (rbtBM.Checked == true)
+            //{
+            //    associatesVo.AAC_UserType = "BM";
+            //    if (ddlSelectType.SelectedIndex!=0)
+            //        associatesVo.BranchId = int.Parse(ddlSelectType.SelectedValue);
+            //}
+            //else if (rbtRM.Checked == true)
+            //{
+            //    associatesVo.AAC_UserType = "RM";
+            //    if (ddlSelectType.SelectedIndex != 0)
+            //        associatesVo.RMId = int.Parse(ddlSelectType.SelectedValue);
+            //}
+            //else if (rbtnAgent.Checked == true)
+            //{
+            //    associatesVo.AAC_UserType = "Agent";
+            //    if (ddlSelectType.SelectedIndex != 0)
+            //        associatesVo.AdviserAssociateId = int.Parse(ddlSelectType.SelectedValue);
+            //}
+            associatesVo.AAC_AgentCode = txtAgentCode.Text;
+            associatesVo.AAC_CreatedBy = userVo.UserId;
+            associatesVo.AAC_ModifiedBy = userVo.UserId;
+            result = associatesBo.CreateAdviserAgentCode(associatesVo,agentId);
+            if(result==true)
+                ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "ViewAgentCode", "loadcontrol('ViewAgentCode','login');", true);
+
+        }
+
+        private void BindAgentList()
+        {
+            DataSet ds;
+            DataTable dt;
+            dt = associatesBo.GetAssociatesList(advisorVo.advisorId);
+            if (dt.Rows.Count>0)
+            {
+                ddlSelectType.DataSource = dt;
+                ddlSelectType.DataValueField = dt.Columns["AA_AdviserAssociateId"].ToString();
+                ddlSelectType.DataTextField = dt.Columns["AA_ContactPersonName"].ToString();
+                ddlSelectType.DataBind();
+            }
+            ddlSelectType.Items.Insert(0, new System.Web.UI.WebControls.ListItem("Select Agent", "Select Agent"));
+
+        }
+        private void BindBranchDropDown()
+        {
+
+            RMVo rmVo = new RMVo();
+            rmVo = (RMVo)Session[SessionContents.RmVo];
+            int bmID = rmVo.RMId;
+            try
+            {
+                UploadCommonBo uploadsCommonDao = new UploadCommonBo();
+                DataSet ds = uploadsCommonDao.GetAdviserBranchList(advisorVo.advisorId, "adviser");
+                if (ds != null)
+                {
+                    ddlSelectType.DataSource = ds;
+                    ddlSelectType.DataValueField = ds.Tables[0].Columns["AB_BranchId"].ToString();
+                    ddlSelectType.DataTextField = ds.Tables[0].Columns["AB_BranchName"].ToString();
+                    ddlSelectType.DataBind();
+                }
+                ddlSelectType.Items.Insert(0, new System.Web.UI.WebControls.ListItem("Select Branch", "Select Branch"));
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "RMAMCSchemewiseMIS.ascx:BindBranchDropDown()");
+
+                object[] objects = new object[4];
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+        private void BindRMDropDown()
+        {
+            try
+            {
+                AdvisorStaffBo advisorStaffBo = new AdvisorStaffBo();
+                DataTable dt = advisorStaffBo.GetAdviserRM(advisorVo.advisorId);
+                if (dt.Rows.Count > 0)
+                {
+                    ddlSelectType.DataSource = dt;
+                    ddlSelectType.DataValueField = dt.Columns["AR_RMId"].ToString();
+                    ddlSelectType.DataTextField = dt.Columns["RMName"].ToString();
+                    ddlSelectType.DataBind();
+                }
+                ddlSelectType.Items.Insert(0, new System.Web.UI.WebControls.ListItem("Select RM", "Select RM"));
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+
+                FunctionInfo.Add("Method", "RMAMCSchemewiseMIS.ascx:BindRMDropDown()");
+
+                object[] objects = new object[0];
+
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+        }
+
+        protected void ddlUserType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlUserType.SelectedIndex != 0)
+            {
+                if(ddlUserType.SelectedValue=="BM")
+                    BindBranchDropDown();
+                else if (ddlUserType.SelectedValue == "RM")
+                    BindRMDropDown();
+                else if (ddlUserType.SelectedValue == "Associates")
+                    BindAgentList();
+            }
         }
     }
 }
