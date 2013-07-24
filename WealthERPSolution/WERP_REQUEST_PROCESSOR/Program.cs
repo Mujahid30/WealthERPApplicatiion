e306313b98977f15d2efbd45b2ce18e161bd2cbe
AMP\Administrator:4 <AMP\Administrator:4>
2013-07-24 19:16:30 +0530

    RE:BulkMail-Added new project for bulk mail request processor

---
+++ WealthERPSolution/WERP_REQUEST_PROCESSOR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_REQUEST_PROCESSOR
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ WealthERPSolution/WERP_REQUEST_PROCESSOR/RequestCreatorBo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WERP_REQUEST_PROCESSOR
+{
+    public class RequestCreatorBo
+    {
+
+    }
+}
