225a4c479471380e11c4d4ff38444f9bcce915e2
AMP\Administrator:4 <AMP\Administrator:4>
2013-07-26 18:38:53 +0530

    revert

--- WealthERPSolution/UI/WealthERP/Advisor/CustomerFolioMerge.ascx.designer.cs
+++ WealthERPSolution/UI/WealthERP/Advisor/CustomerFolioMerge.ascx.designer.cs
@@ -479,5 +479,7 @@ namespace WealthERP.Advisor {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HiddenField hdnadviserId;
+        protected global::System.Web.UI.WebControls.HiddenField hdnAgentId;
+        protected global::System.Web.UI.WebControls.HiddenField hdnIsassociate;
     }
 }
