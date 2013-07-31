2fd0414140ee9cbb0bd89a922cd79cb6a5765b32
AMP\baivab <AMP\baivab>
2013-07-31 15:57:55 +0530

    RE:7.9 added log for balance creation

--- WealthERPSolution/WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION/MFTransactionBalanceProcessBo.cs
+++ WealthERPSolution/WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION/MFTransactionBalanceProcessBo.cs
@@ -19,9 +19,9 @@ namespace WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION
         BoValuation.MFEngineBo.ValuationLabel valuationFor = BoValuation.MFEngineBo.ValuationLabel.Advisor;
         MFEngineBo mfEngineBo = new MFEngineBo();
         CustomerPortfolioBo customerPortfolioBo = new CustomerPortfolioBo();
-        
         public void CreateMFTransactionBalanceForAllAdviser(int isForAllAdviser, int adviserId)
         {
+
             if (isForAllAdviser == 1)
             {
                 adviserVoList = adviserMaintenanceBo.GetAdviserList();
@@ -30,7 +30,10 @@ namespace WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION
 
                     try
                     {
+                        int logId = 0;
+                        logId = CreateAdviserEODLog("MF_Balance", DateTime.Now, adviserVoList[i].advisorId);
                         mfEngineBo.MFBalanceCreation(adviserVoList[i].advisorId, 0, valuationFor);
+                        UpdateAdviserEODLog("MF_Balance", 1, logId);
 
                     }
                     catch
@@ -40,15 +43,48 @@ namespace WERP_DAILY_MF_TRANSACTION_BALANCE_VALUATION
                     }
 
                 }
+
             }
             else
             {
-
+                int logId = 0;
+                logId = CreateAdviserEODLog("MF_Balance", DateTime.Now, adviserId);
                 mfEngineBo.MFBalanceCreation(adviserId, 0, valuationFor);
+                UpdateAdviserEODLog("MF_Balance", 1, logId);
             }
-                
+
 
         }
+        private int CreateAdviserEODLog(string assetType, DateTime dt, int adviserId)
+        {
+            int LogId = 0;
 
+            AdviserDailyLOGVo adviserDaliyLOGVo = new AdviserDailyLOGVo();
+
+            adviserDaliyLOGVo.AdviserId = adviserId;
+            adviserDaliyLOGVo.CreatedBy = 1000;
+            adviserDaliyLOGVo.StartTime = DateTime.Now;
+            adviserDaliyLOGVo.ProcessDate = dt;
+            adviserDaliyLOGVo.AssetGroup = assetType;
+            LogId = customerPortfolioBo.CreateAdviserEODLog(adviserDaliyLOGVo);
+
+            return LogId;
+        }
+
+
+        protected void UpdateAdviserEODLog(string group, int IsComplete, int LogId)
+        {
+            AdviserDailyLOGVo adviserDaliyLOGVo = new AdviserDailyLOGVo();
+
+            adviserDaliyLOGVo.IsEquityCleanUpComplete = 0;
+            adviserDaliyLOGVo.IsValuationComplete = IsComplete;
+            adviserDaliyLOGVo.ModifiedBy = 1000;
+            adviserDaliyLOGVo.EODLogId = LogId;
+            adviserDaliyLOGVo.AssetGroup = group;
+            adviserDaliyLOGVo.EndTime = DateTime.Now;
+            customerPortfolioBo.UpdateAdviserEODLog(adviserDaliyLOGVo);
+
+
+        }
     }
 }
