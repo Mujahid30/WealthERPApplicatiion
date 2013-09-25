3be197dd7078c46b1d442cd7f8792d8ccbf092f9
AMP\Administrator:4 <AMP\Administrator:4>
2013-09-25 13:46:27 +0530

    RE:WERP-Parameterized done

--- WealthERPSolution/WERP_MF_Historical_ValuationQueue/Program.cs
+++ WealthERPSolution/WERP_MF_Historical_ValuationQueue/Program.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 
 namespace WERP_MF_Historical_ValuationQueue
 {
-   public class Program
+    public class Program
     {
+        static string isManual = ConfigurationSettings.AppSettings["IsManual"].ToString();
+        static int adviserId;
+        static int isCurrent;
+        static DateTime valuationDate;
         static void Main(string[] args)
         {
             ProcessMFHistoricalValuationQueue();
@@ -15,7 +20,19 @@ namespace WERP_MF_Historical_ValuationQueue
         public static void ProcessMFHistoricalValuationQueue()
         {
             MFHistoricalValuationQueue mfHistoricalValuationQueue = new MFHistoricalValuationQueue();
-            mfHistoricalValuationQueue.ProcessMFAccountInstantValuation();
+            if (isManual == "1")
+            {
+                adviserId = Convert.ToInt32(ConfigurationSettings.AppSettings["AdviserId"].ToString());
+                isCurrent = Convert.ToInt32(ConfigurationSettings.AppSettings["IsCurrent"].ToString());
+                valuationDate = Convert.ToDateTime(ConfigurationSettings.AppSettings["ValuationDate"].ToString());
+                mfHistoricalValuationQueue.CreateMFHistoricalNetposition(valuationDate, adviserId, isCurrent);
+
+            }
+            else
+            {
+                mfHistoricalValuationQueue.ProcessMFAccountInstantValuation();
+
+            }
         }
     }
 }
