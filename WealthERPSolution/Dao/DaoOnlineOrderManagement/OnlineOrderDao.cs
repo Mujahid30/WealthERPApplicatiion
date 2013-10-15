7a5fad0a9b6c24d51bc3b7f19a2281a8b29b83fc
AMP\Administrator:4 <AMP\Administrator:4>
2013-10-16 03:09:23 +0530

    RE:SBI-RMS API Integration for Online Order done Testing Pending

--- WealthERPSolution/Dao/DaoOnlineOrderManagement/OnlineOrderDao.cs
+++ WealthERPSolution/Dao/DaoOnlineOrderManagement/OnlineOrderDao.cs
@@ -2,10 +2,89 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Collections;
+using System.Data.Sql;
+using VoOnlineOrderManagemnet;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+
 
 namespace DaoOnlineOrderManagement
 {
-     public class OnlineOrderDao
+    public class OnlineOrderDao
     {
+        public void UpdateOrderRMSAccountDebitRequestTime(int orderId)
+        {
+
+            Database db;
+            DbCommand updateRMSDebitRequestTimeCmd;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                updateRMSDebitRequestTimeCmd = db.GetStoredProcCommand("SPROC_ONL_UpdateOrderRMSDebitRequestTime");
+                db.AddInParameter(updateRMSDebitRequestTimeCmd, "@OrderId", DbType.Int32, orderId);
+                db.ExecuteDataSet(updateRMSDebitRequestTimeCmd);
+
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "OnlineOrderDao.cs:GUpdateOrderRMSAccountDebitRequestTime(int orderId)");
+                object[] objects = new object[10];
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+
+        }
+
+        public void UpdateOrderRMSAccountDebitDetails(int orderId,int isDebited, string rmsTransactionId, string rmsResponseMessage)
+        {
+
+            Database db;
+            DbCommand updateRMSAccountDebitedCmd;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                updateRMSAccountDebitedCmd = db.GetStoredProcCommand("SPROC_ONL_UpdateOrderRMSAccountDebitStatus");
+                db.AddInParameter(updateRMSAccountDebitedCmd, "@OrderId", DbType.Int32, orderId);
+                db.AddInParameter(updateRMSAccountDebitedCmd, "@IsDebited", DbType.Int16, isDebited);
+                if (!string.IsNullOrEmpty(rmsTransactionId))
+                    db.AddInParameter(updateRMSAccountDebitedCmd, "@RMSTransactionId", DbType.String, rmsTransactionId);
+                if (!string.IsNullOrEmpty(rmsResponseMessage))
+                    db.AddInParameter(updateRMSAccountDebitedCmd, "@RMSResponseMessage", DbType.String, rmsResponseMessage);
+                db.ExecuteDataSet(updateRMSAccountDebitedCmd);
+
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "OnlineOrderDao.cs:UpdateOrderRMSAccountDebitDetails(int orderId,string rmsTransactionId)");
+                object[] objects = new object[2];
+                objects[0] = orderId;
+                objects[1] = rmsTransactionId;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+
+        }
+
+
     }
 }
