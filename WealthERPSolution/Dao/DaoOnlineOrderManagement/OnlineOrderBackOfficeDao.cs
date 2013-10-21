2efd9a9e537f9fcd89a85855cb9f0db1147b12ee
AMP\gobinda <AMP\gobinda>
2013-10-21 20:30:00 +0530

    re sb iextraxt

--- WealthERPSolution/Dao/DaoOnlineOrderManagement/OnlineOrderBackOfficeDao.cs
+++ WealthERPSolution/Dao/DaoOnlineOrderManagement/OnlineOrderBackOfficeDao.cs
@@ -2,10 +2,82 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Collections;
+using System.Data.Sql;
+using VoOnlineOrderManagemnet;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using System.Collections.Specialized;
+
 
 namespace DaoOnlineOrderManagement
 {
     public class OnlineOrderBackOfficeDao
     {
+        public DataSet GetExtractType()
+        {
+            DataSet dsExtractType;
+            Database db;
+            DbCommand GetGetMfOrderExtractCmd;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                GetGetMfOrderExtractCmd = db.GetStoredProcCommand("SPROC_GetExtractType");
+
+                dsExtractType = db.ExecuteDataSet(GetGetMfOrderExtractCmd);
+
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "OperationDao.cs:GetMfOrderExtract()");
+                object[] objects = new object[10];
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return dsExtractType;
+        }
+
+        public DataSet GetExtractTypeDataForFileCreation(DateTime orderDate,int AdviserId)
+        {
+            DataSet dsExtractType;
+            Database db;
+            DbCommand GetGetMfOrderExtractCmd;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                GetGetMfOrderExtractCmd = db.GetStoredProcCommand("SPROC_GetExtractTypeDataForFileCreation");
+                if (orderDate != DateTime.MinValue)
+                    db.AddInParameter(GetGetMfOrderExtractCmd, "@orderDate", DbType.DateTime, orderDate);
+                db.AddInParameter(GetGetMfOrderExtractCmd, "@adviserId", DbType.Int32, AdviserId);
+                dsExtractType = db.ExecuteDataSet(GetGetMfOrderExtractCmd);
+
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "OperationDao.cs:GetMfOrderExtract()");
+                object[] objects = new object[10];
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return dsExtractType;
+        }
     }
 }
