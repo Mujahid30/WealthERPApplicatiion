dd3c36866f6457cd55f614a0e119e6191d2f7bd0
AMP\kbajpai <AMP\kbajpai>
2013-10-23 02:59:01 +0530

    RE : SBI OrderExtract added

--- WealthERPSolution/Dao/DaoOnlineOrderManagement/OnlineOrderBackOfficeDao.cs
+++ WealthERPSolution/Dao/DaoOnlineOrderManagement/OnlineOrderBackOfficeDao.cs
@@ -80,5 +80,75 @@ namespace DaoOnlineOrderManagement
             }
             return dsExtractType;
         }
+
+        public DataSet GetMfOrderExtract(DateTime ExecutionDate, int AdviserId, string TransactionType, string RtaIdentifier)
+        {
+            DataSet dsGetMfOrderExtract;
+            Database db;
+            DbCommand cmd;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                cmd = db.GetStoredProcCommand("SPROC_GetAdviserMfOrderExtract");
+                db.AddInParameter(cmd, "@WTBD_ExecutionDate", DbType.DateTime, ExecutionDate);
+                db.AddInParameter(cmd, "@A_AdviserId", DbType.Int32, AdviserId);
+                db.AddInParameter(cmd, "@XES_SourceCode", DbType.String, RtaIdentifier);
+                if (string.IsNullOrEmpty(TransactionType) == false && TransactionType.ToUpper() != "ALL") { db.AddInParameter(cmd, "@AMFE_TrxnType", DbType.String, TransactionType); }
+
+                dsGetMfOrderExtract = db.ExecuteDataSet(cmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "OnlineOrderBackOfficeDao.cs:GetMfOrderExtract(DateTime ExecutionDate, int AdviserId, string TransactionType, string RtaIdentifier)");
+                object[] objects = new object[4];
+                objects[0] = ExecutionDate;
+                objects[1] = AdviserId;
+                objects[2] = TransactionType;
+                objects[3] = RtaIdentifier;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return dsGetMfOrderExtract;
+        }
+
+        public DataSet GetOrderExtractHeaderMapping(string RtaIdentifier)
+        {
+            DataSet dsHeaderMapping;
+            Database db;
+            DbCommand cmd;
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("wealtherp");
+                cmd = db.GetStoredProcCommand("SPROC_OrderExtractHeaderMapping");
+                db.AddInParameter(cmd, "@XES_SourceCode", DbType.String, RtaIdentifier);
+
+                dsHeaderMapping = db.ExecuteDataSet(cmd);
+            }
+            catch (BaseApplicationException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                BaseApplicationException exBase = new BaseApplicationException(Ex.Message, Ex);
+                NameValueCollection FunctionInfo = new NameValueCollection();
+                FunctionInfo.Add("Method", "OperationDao.cs:GetOrderExtractHeaderMapping(string RtaIdentifier)");
+                object[] objects = new object[1];
+                objects[0] = RtaIdentifier;
+                FunctionInfo = exBase.AddObject(FunctionInfo, objects);
+                exBase.AdditionalInformation = FunctionInfo;
+                ExceptionManager.Publish(exBase);
+                throw exBase;
+            }
+            return dsHeaderMapping;
+        }
     }
 }
