a67799eb356de9b527ecd45df74d4b7f0a4e1f39
AMP\psruthi <AMP\psruthi>
2013-11-14 13:52:31 +0530

    RE:SBI-Trade business date implementation function added

--- WealthERPSolution/Vo/VoOnlineOrderManagemnet/OnlineOrderBackOfficeVo.cs
+++ WealthERPSolution/Vo/VoOnlineOrderManagemnet/OnlineOrderBackOfficeVo.cs
@@ -15,7 +15,7 @@ namespace VoOnlineOrderManagemnet
         public int MaxLength { get; set; }
         public bool IsNullable { get; set; }
 
-        public int LookupID { get; set; }        
+        public int LookupID { get; set; }
         public string ExternalName { get; set; }
         public int CategoryID { get; set; }
         public string WerpCode { get; set; }
@@ -23,69 +23,78 @@ namespace VoOnlineOrderManagemnet
         public int MapID { get; set; }
 
 
-       public int AMCCode { get; set; }
-       public int SchemePlanCode { get; set; }
-	   public string SchemePlanName { get; set; }
-	   public string AssetSubSubCategory { get; set; }
-	   public string AssetSubCategoryCode { get; set; }
-	   public string AssetCategoryCode { get; set; }
-	   public string Product  { get; set; }
-	   public string Status { get; set; }
-	   public int   IsOnline { get; set; }
-	   public int   IsDirect { get; set; }
-	   public double FaceValue { get; set; }
-       public string SchemeType { get; set; }
-       public string SchemeOption { get; set; }
-       public string DividendFrequency { get; set; }
-       public string  BankName { get; set; }
-       public string AccountNumber  { get; set; }
-       public string    Branch { get; set; }
-       public int  IsNFO { get; set; }
-       public DateTime  NFOStartDate  { get; set; }
-       public DateTime    NFOEndDate { get; set; }
-       public int  LockInPeriod { get; set; }
-       public TimeSpan CutOffTime { get; set; }
-       public double  EntryLoadPercentag { get; set; }
-       public string   EntryLoadRemark { get; set; }
-       public double   ExitLoadPercentage { get; set; }
-       public string ExitLoadRemark { get; set; }
-       public int    IsPurchaseAvailable { get; set; }
-       public int   IsRedeemAvailable { get; set; }
-       public int   IsSIPAvailable { get; set; }
-       public int  IsSWPAvailable { get; set; }
-       public int IsSwitchAvailable { get; set; }
-       public int  IsSTPAvailable { get; set; }
-       public double InitialPurchaseAmount { get; set; }
-       public double InitialMultipleAmount  { get; set; }
-       public double AdditionalPruchaseAmount { get; set; } 
-       public double AdditionalMultipleAmount { get; set; }
-       public double MinRedemptionAmount { get; set; }
-       public double RedemptionMultipleAmount  { get; set; }
-       public int   MinRedemptionUnits { get; set; }
-       public int   RedemptionMultiplesUnits { get; set; }
-       public double MinSwitchAmount { get; set; }
-       public double SwitchMultipleAmount { get; set; }
-       public int    MinSwitchUnits { get; set; }
-       public int    SwitchMultiplesUnits { get; set; }
-       public string  GenerationFrequency { get; set; }
-       public string  SourceCode  { get; set; }
-       public string  CustomerSubTypeCode { get; set; }
-       public string  SecurityCode { get; set; }
-       public double  PASPD_MaxInvestment { get; set; }
-       public string    WERPBM_BankCode  { get; set; }
-       public string    ExternalCode { get; set; }
-       public string ExternalType { get; set; }
-       public string Dividendfreq { get; set; }
-       public string startdate { get; set; }
-       public string frequency {get; set;}
-       public int MinDues { get; set; }
-       public int MaxDues { get; set; }
-       public double MinAmount { get; set; }
-       public double MultipleAmount { get; set; }
-       public string systematiccode { get; set; }
-      
+        public int AMCCode { get; set; }
+        public int SchemePlanCode { get; set; }
+        public string SchemePlanName { get; set; }
+        public string AssetSubSubCategory { get; set; }
+        public string AssetSubCategoryCode { get; set; }
+        public string AssetCategoryCode { get; set; }
+        public string Product { get; set; }
+        public string Status { get; set; }
+        public int IsOnline { get; set; }
+        public int IsDirect { get; set; }
+        public double FaceValue { get; set; }
+        public string SchemeType { get; set; }
+        public string SchemeOption { get; set; }
+        public string DividendFrequency { get; set; }
+        public string BankName { get; set; }
+        public string AccountNumber { get; set; }
+        public string Branch { get; set; }
+        public int IsNFO { get; set; }
+        public DateTime NFOStartDate { get; set; }
+        public DateTime NFOEndDate { get; set; }
+        public int LockInPeriod { get; set; }
+        public TimeSpan CutOffTime { get; set; }
+        public double EntryLoadPercentag { get; set; }
+        public string EntryLoadRemark { get; set; }
+        public double ExitLoadPercentage { get; set; }
+        public string ExitLoadRemark { get; set; }
+        public int IsPurchaseAvailable { get; set; }
+        public int IsRedeemAvailable { get; set; }
+        public int IsSIPAvailable { get; set; }
+        public int IsSWPAvailable { get; set; }
+        public int IsSwitchAvailable { get; set; }
+        public int IsSTPAvailable { get; set; }
+        public double InitialPurchaseAmount { get; set; }
+        public double InitialMultipleAmount { get; set; }
+        public double AdditionalPruchaseAmount { get; set; }
+        public double AdditionalMultipleAmount { get; set; }
+        public double MinRedemptionAmount { get; set; }
+        public double RedemptionMultipleAmount { get; set; }
+        public int MinRedemptionUnits { get; set; }
+        public int RedemptionMultiplesUnits { get; set; }
+        public double MinSwitchAmount { get; set; }
+        public double SwitchMultipleAmount { get; set; }
+        public int MinSwitchUnits { get; set; }
+        public int SwitchMultiplesUnits { get; set; }
+        public string GenerationFrequency { get; set; }
+        public string SourceCode { get; set; }
+        public string CustomerSubTypeCode { get; set; }
+        public string SecurityCode { get; set; }
+        public double PASPD_MaxInvestment { get; set; }
+        public string WERPBM_BankCode { get; set; }
+        public string ExternalCode { get; set; }
+        public string ExternalType { get; set; }
+        public string Dividendfreq { get; set; }
+        public string startdate { get; set; }
+        public string frequency { get; set; }
+        public int MinDues { get; set; }
+        public int MaxDues { get; set; }
+        public double MinAmount { get; set; }
+        public double MultipleAmount { get; set; }
+        public string systematiccode { get; set; }
 
+       
+    }
 
+    public class TradeBusinessDateVo
+    {
+        public int TradeBusinessId { get; set; }
+        public DateTime TradeBusinessDate { get; set; }
+        public DateTime TradeBusinessExecutionDate { get; set; }
+        public int IsTradeBusinessDateHoliday { get; set; }
+        public int IsTradeBusinessDateWeekend { get; set; }
 
+ 
     }
 }
