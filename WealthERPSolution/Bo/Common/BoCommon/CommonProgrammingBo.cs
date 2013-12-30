c4ea7f8349f74b5d3c6b2fab25c766258fa20ed3
AMP\psahoo:5 <AMP\psahoo:5>
2013-12-30 13:05:20 +0530

    RE:SBI-Accounting Extract File Deliminator change done

--- WealthERPSolution/Bo/Common/BoCommon/CommonProgrammingBo.cs
+++ WealthERPSolution/Bo/Common/BoCommon/CommonProgrammingBo.cs
@@ -5,59 +5,59 @@ using System.Text;
 
 namespace BoCommon
 {
-   public class CommonProgrammingBo
+    public class CommonProgrammingBo
     {
         string filename;
         string delimeter;
 
-       public bool IsNumeric(string s)
-       {
-           double Result;
-           return double.TryParse(s, out Result);
-       }
+        public bool IsNumeric(string s)
+        {
+            double Result;
+            return double.TryParse(s, out Result);
+        }
 
 
-       public string SetFileNameAndDelimeter(int FileID,DateTime dtExtractDate)
-       {
-           string strExtractDate = Convert.ToDateTime(dtExtractDate).ToShortDateString();
-           string[] strSplitExtractDate = strExtractDate.Split('/');
+        public string SetFileNameAndDelimeter(int FileID, DateTime dtExtractDate)
+        {
+            string strExtractDate = Convert.ToDateTime(dtExtractDate).ToShortDateString();
+            string[] strSplitExtractDate = strExtractDate.Split('/');
 
-           string DD = strSplitExtractDate[0].ToString();
-           string MM = strSplitExtractDate[1].ToString();
-           string YYYY = strSplitExtractDate[2].ToString();
+            string DD = strSplitExtractDate[0].ToString();
+            string MM = strSplitExtractDate[1].ToString();
+            string YYYY = strSplitExtractDate[2].ToString();
 
-           if (FileID == 37)
-           {
-               filename = "sbiemf" + DD + MM + ".txt";
-               delimeter = "#";
-           }
-           else if (FileID == 38)
-           {
-               filename = "sbipay" + DD + MM + ".txt";
-               delimeter = "   ";
-           }
-           else if (FileID == 39)
-           {
-               filename = "HDFCPAY" + DD + MM + ".txt";
-               delimeter = " ";
-           }
-           else if (FileID == 40)
-           {
-               filename = "eMF-InProcess" + MM + DD + YYYY + ".txt";
-               delimeter = "|";
-           }
-           else if (FileID == 41)
-           {
-               filename = "eMF-Executed" + MM + DD + YYYY + ".txt";
-               delimeter = "|";
-           }
-           else if (FileID == 42)
-           {
-               filename = "SSL104" + DD + MM + ".txt";
-               delimeter = ",";
-           }
+            if (FileID == 37)
+            {
+                filename = "sbiemf" + DD + MM + ".txt";
+                delimeter = "#";
+            }
+            else if (FileID == 38)
+            {
+                filename = "sbipay" + DD + MM + ".txt";
+                delimeter = "\t";
+            }
+            else if (FileID == 39)
+            {
+                filename = "HDFCPAY" + DD + MM + ".txt";
+                delimeter = "\t";
+            }
+            else if (FileID == 40)
+            {
+                filename = "eMF-InProcess" + MM + DD + YYYY + ".txt";
+                delimeter = "|";
+            }
+            else if (FileID == 41)
+            {
+                filename = "eMF-Executed" + MM + DD + YYYY + ".txt";
+                delimeter = "|";
+            }
+            else if (FileID == 42)
+            {
+                filename = "SSL104" + DD + MM + ".txt";
+                delimeter = ",";
+            }
 
-           return filename + "~" + delimeter;
-       }
+            return filename + "~" + delimeter;
+        }
     }
 }
