a8ee011895d5bd1a5d2f8fb357488affaeee188b
AMP\Rkumar <AMP\Rkumar>
2014-07-12 19:46:42 +0530

    @RE:PRINTFILE CHANGES

--- WealthERPSolution/Bo/BoOnlineOrderManagement/OnlineNCDBackOfficeBo.cs
+++ WealthERPSolution/Bo/BoOnlineOrderManagement/OnlineNCDBackOfficeBo.cs
@@ -999,7 +999,7 @@ namespace BoOnlineOrderManagement
 
                 int desiredSize = (AID_SeriesCount * 2) + 15;
 
-                while ((dtExtract.Columns.Count - 15) >= desiredSize)
+                while ((dtExtract.Columns.Count - 19) >= desiredSize)
                 {
                     dtExtract.Columns.RemoveAt(desiredSize);
                 }
