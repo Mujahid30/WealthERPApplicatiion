dc7031d3d914091921e01cc5961fec98678389b6
AMP\Rkumar <AMP\Rkumar>
2014-11-07 16:50:18 +0530

    @RE:SBI- change control

--- WealthERPSolution/UI/WealthERP/CustomerPortfolio/RMMultipleTransactionView.ascx.designer.cs
+++ WealthERPSolution/UI/WealthERP/CustomerPortfolio/RMMultipleTransactionView.ascx.designer.cs
@@ -66,7 +66,7 @@ namespace WealthERP.CustomerPortfolio {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
-        protected global::System.Web.UI.WebControls.Button lblAgentCode;
+        protected global::System.Web.UI.WebControls.Label lblAgentCode;
         /// <summary>
         /// lbBack control.
         /// </summary>
