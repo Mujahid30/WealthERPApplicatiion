3b1a66cb9eec20addd5135649aaf597cf8155c06
AMP\Rkumar <AMP\Rkumar>
2015-01-29 18:51:16 +0530

    @RE:SBI-order book

--- WealthERPSolution/Dao/DaoOnlineOrderManagement/OnlineMFOrderDao.cs
+++ WealthERPSolution/Dao/DaoOnlineOrderManagement/OnlineMFOrderDao.cs
@@ -75,6 +75,7 @@ namespace DaoOnlineOrderManagement
                     db.AddInParameter(GetOrderBookMISCmd, "@Status", DbType.String, DBNull.Value);
                 db.AddInParameter(GetOrderBookMISCmd, "@Fromdate", DbType.DateTime, dtFrom);
                 db.AddInParameter(GetOrderBookMISCmd, "@ToDate", DbType.DateTime, dtTo);
+                db.AddInParameter(GetOrderBookMISCmd, "@StatusType", DbType.String, orderType);
                 dsOrderBookMIS = db.ExecuteDataSet(GetOrderBookMISCmd);
 
             }
