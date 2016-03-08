02d60f787bbd4dd73e2172dbc2040405e6de42c5
AMP\Rpanda <AMP\Rpanda>
2016-03-08 18:46:48 +0530

    @re:SBI email and sms

--- WealthERPSolution/UI/WealthERP/InvestorOnline.aspx
+++ WealthERPSolution/UI/WealthERP/InvestorOnline.aspx
@@ -1,16 +1,101 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InvestorOnline.aspx.cs" Inherits="WealthERP.InvestorOnline" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InvestorOnline.aspx.cs"
+    Inherits="WealthERP.InvestorOnline" ValidateRequest="false" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" >
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <link href="../Base/CSS/demo.css" rel="stylesheet" type="text/css" />
+    <link href="../Base/CSS/jquery-te-1.4.0.css" rel="stylesheet" type="text/css" />
+    
+    
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-    
+   
+    <div style="width: 900px;">
+        <div style="width: 600px; float: left;">
+            <div id="divEmail" runat="server">
+               
+                    <asp:RadioButton ID="Subject" runat="server" Checked="true" AutoPostBack="true" GroupName="Email" Text="Email Subject" OnCheckedChanged="RadioBtn_OnCheckedChanged" />
+                    <asp:RadioButton ID="Body" runat="server" AutoPostBack="true" GroupName="Email" Text="Email Body" OnCheckedChanged="RadioBtn_OnCheckedChanged" />
+                <div id="divEmailSubject" runat="server" visible="true">
+                    <div style="width: 700px;">
+                        <asp:TextBox ID="txtEmailSubject"  TextMode="MultiLine" runat="server" CssClass="textEditor"
+                            onblur="getEmailSubject()"></asp:TextBox>
+                    </div>
+                    <asp:Button ID="btnEmailSubjectPreview" runat="server" OnClick="previewButton_Onclick"
+                        Text="Preview" />
+                    <asp:Button ID="btnEmailSubjectSave" runat="server" OnClick="SaveButton_Onclick"
+                        Text="Save" />
+                    <div style="border:solid 2px Blue; margin:20px;padding:20px;">
+                        <asp:Label ID="lblSampleEmailSubject" runat="server"></asp:Label>
+                    </div>
+                </div>
+                <div id="divEmailBody" runat="server" visible="false">
+                    <div style="width: 700px;">
+                        <asp:TextBox ID="txtEmailBody" TextMode="MultiLine" runat="server" CssClass="textEditor"
+                            onblur="getEmailBody()"></asp:TextBox>
+                    </div>
+                    <asp:Button ID="btnEmailBodyPreview" runat="server" OnClick="previewButton_Onclick"
+                        Text="Preview" />
+                    <asp:Button ID="btnEmailBodySave" runat="server" OnClick="SaveButton_Onclick" Text="Save" />
+                    <div style="border:solid 2px Blue; margin:20px;padding:20px;">
+                        <asp:Label ID="lblSampleEmailBody" runat="server"></asp:Label>
+                    </div>
+                </div>
+            </div>
+            <div id="divSMS" runat="server">
+                <div style="width: 400px;">
+                    <asp:TextBox ID="txtSMSBody" TextMode="MultiLine" runat="server" CssClass="textEditor"
+                        onblur="getSMSBody()"></asp:TextBox>
+                </div>
+                <asp:Button ID="btnSMSBodyPreview" runat="server" OnClick="previewButton_Onclick"
+                    Text="Preview" />
+                <asp:Button ID="btnSMSBodySave" runat="server" OnClick="SaveButton_Onclick" Text="Save" />
+                <div style="border:solid 2px Blue; margin:20px;padding:20px;">
+                    <asp:Label ID="lblSampleSMSBody" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div style="width: 300px; float: left; margin-top:50px;">
+        <div style="float:right;">
+            <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+            </div>
+        </div>
     </div>
+    <asp:HiddenField ID="hdnEmailSubject" runat="server" />
+    <asp:HiddenField ID="hdnEmailBody" runat="server" />
+    <asp:HiddenField ID="hdnSMSBody" runat="server" />
+    <asp:HiddenField ID="hdnCurrentTextEditor" runat="server" />
+    
+<script src="../Scripts/jquery-1.10.2.min.js" type="text/javascript"></script>
+
+<script src="../Scripts/jquery-te-1.4.0.min.js" type="text/javascript"></script>
+
+ <script language="javascript" type="text/javascript">
+     $('.textEditor').jqte();
+     function getEmailSubject() {
+         document.getElementById('<%=hdnEmailSubject.ClientID %>').value = document.getElementById('<%=txtEmailSubject.ClientID %>').value;
+         
+     }
+     function getEmailBody() {
+         document.getElementById('<%=hdnEmailBody.ClientID %>').value = document.getElementById('<%=txtEmailBody.ClientID %>').value;
+     }
+     function getSMSBody() {
+         document.getElementById('<%=hdnSMSBody.ClientID %>').value = document.getElementById('<%=txtSMSBody.ClientID %>').value;
+     }
+     function SetParameter(id) {
+         var x = document.getElementById('<%=hdnCurrentTextEditor.ClientID %>').value;
+         var y = document.getElementById(x).value + id;
+         //        document.getElementById('<%=txtEmailSubject.ClientID %>').value = document.getElementById(x).value + id;
+         document.getElementById(x).value = y;
+         document.getElementById(x).setfocus();
+     }
+    
+</script>
     </form>
 </body>
+
+
 </html>
--- WealthERPSolution/UI/WealthERP/InvestorOnline.aspx.cs
+++ WealthERPSolution/UI/WealthERP/InvestorOnline.aspx.cs
@@ -4,13 +4,218 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VoUser;
+using WealthERP.Base;
+using System.Data;
+using Telerik.Web.UI;
+using BoOnlineOrderManagement;
+using System.Configuration;
+using System.IO;
+using BoCommon;
+using BoProductMaster;
+using System.Web.UI.HtmlControls;
 
 namespace WealthERP
 {
     public partial class InvestorOnline : System.Web.UI.Page
     {
+        UserVo userVo = new UserVo();
+        AdvisorVo adviserVo = new AdvisorVo();
+        OnlineOrderBackOfficeBo onlineOrderBackOfficeBo = new OnlineOrderBackOfficeBo();
+        OnlineCommonBackOfficeBo OnlineCommonBackOfficeBo = new OnlineCommonBackOfficeBo();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            SessionBo.CheckSession();
+            userVo = (UserVo)Session[SessionContents.UserVo];
+            adviserVo = (AdvisorVo)Session["advisorVo"];
+            if (!IsPostBack)
+            {
+                if (!string.IsNullOrEmpty(Request.QueryString["setupId"]))
+                {
+                    DataSet ds = onlineOrderBackOfficeBo.GetNotificationParameterwithEmailSMSDetails(Convert.ToInt32(Request.QueryString["setupId"].Trim()));
+                    if (Request.QueryString["FormatType"] == "Email")
+                    {
+                        divEmail.Visible = true;
+                        divSMS.Visible = false;
+                        hdnCurrentTextEditor.Value = "txtEmailSubject";
+                    }
+                    else if (Request.QueryString["FormatType"] == "SMS")
+                    {
+                        divEmail.Visible = false;
+                        divSMS.Visible = true;
+                        hdnCurrentTextEditor.Value = "txtSMSBody";
+                    }
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        if (Cache["NotificationParameter" + userVo.UserId.ToString()] == null)
+                        {
+                            Cache.Insert("NotificationParameter" + userVo.UserId.ToString(), ds.Tables[0]);
+                        }
+                        else
+                        {
+                            Cache.Remove("NotificationParameter" + userVo.UserId.ToString());
+                            Cache.Insert("NotificationParameter" + userVo.UserId.ToString(), ds.Tables[0]);
+                        }
+                        //BindLiteral(ds.Tables[0]);
+
+                    }
+                    if (ds.Tables[1].Rows.Count > 0)
+                    {
+                        if (Cache["EmailDetails" + userVo.UserId.ToString()] == null)
+                        {
+                            Cache.Insert("EmailDetails" + userVo.UserId.ToString(), ds.Tables[1]);
+                        }
+                        else
+                        {
+                            Cache.Remove("EmailDetails" + userVo.UserId.ToString());
+                            Cache.Insert("EmailDetails" + userVo.UserId.ToString(), ds.Tables[1]);
+                        }
+
+                        txtEmailSubject.Text = ds.Tables[1].Rows[0]["CTNEF_EmailSubjectFormat"].ToString();
+                        txtEmailBody.Text = ds.Tables[1].Rows[0]["CTNEF_EmailBodyFormat"].ToString();
+                        hdnEmailBody.Value = ds.Tables[1].Rows[0]["CTNEF_EmailBodyFormat"].ToString();
+                        hdnEmailSubject.Value = ds.Tables[1].Rows[0]["CTNEF_EmailSubjectFormat"].ToString();
+                    }
+                    if (ds.Tables[2].Rows.Count > 0)
+                    {
+                        if (Cache["SMSDetails" + userVo.UserId.ToString()] == null)
+                        {
+                            Cache.Insert("SMSDetails" + userVo.UserId.ToString(), ds.Tables[2]);
+                        }
+                        else
+                        {
+                            Cache.Remove("SMSDetails" + userVo.UserId.ToString());
+                            Cache.Insert("SMSDetails" + userVo.UserId.ToString(), ds.Tables[2]);
+                        }
+                        txtSMSBody.Text = ds.Tables[2].Rows[0]["CTNSF_SMSBodyFormat"].ToString();
+                        hdnSMSBody.Value = ds.Tables[2].Rows[0]["CTNSF_SMSBodyFormat"].ToString();
+                    }
+
+                }
+            }
+            DataTable dt=new DataTable();
+            BindLiteral(dt);
+        }
+        protected void BindLiteral(DataTable parameter)
+        {
+            PlaceHolder1.Controls.Clear();
+            parameter = (DataTable)Cache["NotificationParameter" + userVo.UserId.ToString()];
+            PlaceHolder1.Controls.Add(new LiteralControl(@"<table ID=divparameter width=100" + "%"));
+            foreach (DataRow dr in parameter.Rows)
+            {
+                PlaceHolder1.Controls.Add(new LiteralControl(@"<tr class=" + "trparameter>"));
+                PlaceHolder1.Controls.Add(new LiteralControl("<td colspan=\"15\">"));
+                LinkButton lbl = new LinkButton();
+                lbl.ID = dr["NP_ParameterCode"].ToString();
+                lbl.Text = dr["NP_Parameter"].ToString();
+                lbl.CssClass = "lblparameter";
+                lbl.OnClientClick= "SetParameter('" + dr["NP_ParameterCode"].ToString()+"')";
+                
+                PlaceHolder1.Controls.Add(lbl);
+                PlaceHolder1.Controls.Add(new LiteralControl("</td></tr>"));
+
+            }
+            PlaceHolder1.Controls.Add(new LiteralControl("</table>"));
+        }
+        protected string SampleText(string formatText)
+        {
+            DataTable dt;
+            dt = (DataTable)Cache["NotificationParameter" + userVo.UserId.ToString()];
+            if (dt != null)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                 formatText=   formatText.Replace(dr["NP_ParameterCode"].ToString(), dr["NP_SampleText"].ToString());
+                    
+                }
+            }
+            return formatText;
+        }
+        protected string GetParameters(string formatText)
+        {
+            DataTable dt;
+            dt = (DataTable)Cache["NotificationParameter" + userVo.UserId.ToString()];
+            string parameterCodes = string.Empty;
+            if (dt != null)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    if (formatText.Contains(dr["NP_ParameterCode"].ToString()))
+                    {
+                        parameterCodes = parameterCodes + dr["NP_ParameterCode"].ToString() + ",";
+                    }
+                }
+            }
+            return parameterCodes;
+
+        }
+        protected void previewButton_Onclick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.ID == "btnEmailSubjectPreview")
+            {
+                lblSampleEmailSubject.Text = SampleText(txtEmailSubject.Text);
+            }
+            else if (btn.ID == "btnEmailBodyPreview")
+            {
+                lblSampleEmailBody.Text = SampleText(txtEmailBody.Text);
+            }
+            else if (btn.ID == "btnSMSBodyPreview")
+            {
+                lblSampleSMSBody.Text = SampleText(txtSMSBody.Text);
+            }
+        }
+        protected void SaveButton_Onclick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            DataTable dtEmail;
+            DataTable dtSms;
+            dtEmail = (DataTable)Cache["EmailDetails" + userVo.UserId.ToString()];
+            dtSms = (DataTable)Cache["SMSDetails" + userVo.UserId.ToString()];
+            int emailId = 0;
+            int smsId = 0;
+            int notificationId = Convert.ToInt32(Request.QueryString["setupId"]);
+            if (dtEmail != null && dtEmail.Rows.Count > 0)
+            {
+
+                emailId = Convert.ToInt32(dtEmail.Rows[0]["CTNEF_Id"].ToString());
+            }
+            if (dtSms!=null && dtSms.Rows.Count > 0)
+            {
+
+                emailId = Convert.ToInt32(dtSms.Rows[0]["CTNSF_Id"].ToString());
+            }
+            if (btn.ID == "btnEmailSubjectSave")
+            {
+                onlineOrderBackOfficeBo.InsertUpdateNotificationFormat(userVo.UserId, notificationId, "EmailSubject", GetParameters(txtEmailSubject.Text), txtEmailSubject.Text, emailId);
+            }
+            else if (btn.ID == "btnEmailBodySave")
+            {
+                onlineOrderBackOfficeBo.InsertUpdateNotificationFormat(userVo.UserId, notificationId, "EmailBody", GetParameters(txtEmailBody.Text), txtEmailBody.Text, emailId);
+            }
+            else if (btn.ID == "btnSMSBodySave")
+            {
+                onlineOrderBackOfficeBo.InsertUpdateNotificationFormat(userVo.UserId, notificationId, "SMS", GetParameters(txtSMSBody.Text), txtSMSBody.Text, smsId);
+
+            }
+
+        }
+        protected void RadioBtn_OnCheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rbtn = (RadioButton)sender;
+            if (rbtn.ID == "Subject")
+            {
+                divEmailBody.Visible = false;
+                divEmailSubject.Visible = true;
+                hdnCurrentTextEditor.Value = "txtEmailSubject";
+            }
+            else if (rbtn.ID == "Body")
+            {
+                divEmailBody.Visible = true;
+                divEmailSubject.Visible = false;
+                hdnCurrentTextEditor.Value = "txtEmailBody";
+            }
 
         }
     }
