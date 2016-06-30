575618a19eff2df178aa2540f8582cf41b48ed3f
AMP\Rkumar <AMP\Rkumar>
2016-06-30 11:51:27 +0530

    @SBI-Equity_Market_Data

---
+++ WealthERPSolution/WERPMarketAPIData/EquityMarketDateAPI/EquityMarketDateAPI/EquityAPIBO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace EquityMarketDateAPI
+{
+  public  class EquityAPIBO
+    {
+      EquityAPIDAO EquityAPIDAO = new EquityAPIDAO();
+      public int CreateUpdateEquityMarketData(DataTable dt)
+      {
+          int result = 0;
+          result = EquityAPIDAO.CreateUpdateEquityMarketData(dt);
+          return result;
+      }
+      public int CreateUpdateEquityMasterData(DataTable dt)
+      {
+          int result = 0;
+          result = EquityAPIDAO.CreateUpdateEquityMasterData(dt);
+          return result;
+      }
+    }
+}
---
+++ WealthERPSolution/WERPMarketAPIData/EquityMarketDateAPI/EquityMarketDateAPI/EquityAPIDAO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.IO;
+using System.Configuration;
+namespace EquityMarketDateAPI
+{
+   public class EquityAPIDAO
+    {
+       public int CreateUpdateEquityMarketData(DataTable dt)
+       {
+           int result = 0;
+           {
+               string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+               SqlConnection sqlCon = new SqlConnection(connectionString);
+               sqlCon.Open();
+               SqlCommand cmdProc = new SqlCommand("SPROC_EquityMarketData", sqlCon);
+               cmdProc.CommandType = CommandType.StoredProcedure;
+               cmdProc.Parameters.AddWithValue("@EquityMarketData", dt);
+               result = cmdProc.ExecuteNonQuery();
+           }
+           return result;
+       }
+       public int CreateUpdateEquityMasterData(DataTable dt)
+       {
+           int result = 0;
+           {
+               string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+               SqlConnection sqlCon = new SqlConnection(connectionString);
+               sqlCon.Open();
+               SqlCommand cmdProc = new SqlCommand("SPROC_CreateUpdateEquityMaster", sqlCon);
+               cmdProc.CommandType = CommandType.StoredProcedure;
+               cmdProc.Parameters.AddWithValue("@EquityMaster", dt);
+               result = cmdProc.ExecuteNonQuery();
+           }
+           return result;
+       }
+    }
+}
