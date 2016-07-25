31b8fbf736cb4ecb162cee93943f877950d0297b
AMP\Rpanda <AMP\Rpanda>
2016-07-25 16:59:36 +0530

    @RE:-DPC Flag Added As per Transaction Type

--- WealthERPSolution/Bo/BoOnlineOrderManagement/OnlineMFOrderBo.cs
+++ WealthERPSolution/Bo/BoOnlineOrderManagement/OnlineMFOrderBo.cs
@@ -669,6 +669,7 @@ namespace BoOnlineOrderManagement
                     if (onlinemforderVo.DividendType != "0" && onlinemforderVo.DividendType != "")
                         onlinemforderVo.BSESchemeCode = OnlineMFOrderDao.BSESchemeCode(onlinemforderVo.SchemePlanCode, onlinemforderVo.DividendType);
                     string allRedeem = "N";
+                    string DPCFlag = "Y";
                     string Amount = string.Empty;
                     string Unit = string.Empty;
                     if (DematAcctype == "CDSL" || DematAcctype == "")
@@ -701,6 +702,7 @@ namespace BoOnlineOrderManagement
                             Unit = onlinemforderVo.Redeemunits.ToString();
                         PurchaseType = "FRESH";
                         purchase = "R";
+                        DPCFlag = "N";
                     }
                     if (onlinemforderVo.TransactionType != "SEL")
                     {
@@ -712,11 +714,11 @@ namespace BoOnlineOrderManagement
                     }
                     if (isRMSDebited)
                     {
-                        int transCode = OnlineMFOrderDao.BSEorderEntryParam("NEW", UserID, ClientCode, onlinemforderVo.BSESchemeCode, purchase, PurchaseType, DematAcctype, Amount, Unit, allRedeem, "", "Y", "", "", "E116327", "Y", "N", "Y", "", rmsId);
+                        int transCode = OnlineMFOrderDao.BSEorderEntryParam("NEW", UserID, ClientCode, onlinemforderVo.BSESchemeCode, purchase, PurchaseType, DematAcctype, Amount, Unit, allRedeem, "", "Y", "", "", "E116327", "Y", "N", DPCFlag, "", rmsId);
                         string uniqueRefNo;
                         Random ran = new Random();
                         uniqueRefNo = transCode.ToString() + ran.Next().ToString();
-                        string orderEntryresponse = webOrderEntryClient.orderEntryParam("NEW", uniqueRefNo, "", bseuserID, bseMemberId, ClientCode, onlinemforderVo.BSESchemeCode, purchase, PurchaseType, DematAcctype, Amount, Unit, allRedeem, "", "", "Y", "", "", "E116327", "Y", "N", "Y", "", bsePassArray[1], passkey, "", "", "");
+                        string orderEntryresponse = webOrderEntryClient.orderEntryParam("NEW", uniqueRefNo, "", bseuserID, bseMemberId, ClientCode, onlinemforderVo.BSESchemeCode, purchase, PurchaseType, DematAcctype, Amount, Unit, allRedeem, "", "", "Y", "", "", "E116327", "Y", "N", DPCFlag, "", bsePassArray[1], passkey, "", "", "");
                         string[] bseorderEntryresponseArray = orderEntryresponse.Split('|');
                         OnlineMFOrderDao.BSEorderResponseParam(transCode, UserID, Convert.ToInt64(bseorderEntryresponseArray[2]), ClientCode, bseorderEntryresponseArray[6], bseorderEntryresponseArray[7], rmsId, uniqueRefNo);
                         if (Convert.ToInt32(bseorderEntryresponseArray[7]) == 1)
